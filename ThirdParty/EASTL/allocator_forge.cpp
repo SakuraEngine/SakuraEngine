ae60de4da3c9f88f776858b0da7f89823cb0b821
SaeruHikari <39457738+SaeruHikari@users.noreply.github.com>
2021-11-09 17:56:25 +0800

    Init Commit

---
+++ ThirdParty/EASTL/allocator_forge.cpp
@@ -0,0 +1,66 @@
+#include "internal/config.h"
+#include "allocator_forge.h"
+#include <stdlib.h>
+
+#define core_malloc malloc
+#ifdef _WINDOWS
+#define core_memalign _aligned_malloc
+#else
+#define core_memalign aligned_alloc
+#endif
+#define core_free free
+
+#if EASTL_ALLOCATOR_FORGE
+	namespace eastl
+	{
+		void* allocator_forge::allocate(size_t n, int /*flags*/)
+		{ 
+			return core_malloc(n);
+		}
+
+		void* allocator_forge::allocate(size_t n, size_t alignment, size_t alignmentOffset, int /*flags*/)
+		{
+		    if ((alignmentOffset % alignment) == 0) // We check for (offset % alignmnent == 0) instead of (offset == 0) because any block which is
+													// aligned on e.g. 64 also is aligned at an offset of 64 by definition.
+			    return core_memalign(alignment, n);
+
+		    return NULL;
+		}
+
+		void allocator_forge::deallocate(void* p, size_t /*n*/)
+		{ 
+			core_free(p);
+		}
+
+		/// gDefaultAllocator
+		/// Default global allocator_forge instance. 
+		EASTL_API allocator_forge  gDefaultAllocatorForge;
+		EASTL_API allocator_forge* gpDefaultAllocatorForge = &gDefaultAllocatorForge;
+
+		EASTL_API allocator_forge* GetDefaultAllocatorForge()
+		{
+			return gpDefaultAllocatorForge;
+		}
+
+		EASTL_API allocator_forge* SetDefaultAllocatorForge(allocator_forge* pAllocator)
+		{
+			allocator_forge* const pPrevAllocator = gpDefaultAllocatorForge;
+			gpDefaultAllocatorForge = pAllocator;
+			return pPrevAllocator;
+		}
+
+	} // namespace eastl
+
+
+#endif // EASTL_USER_DEFINED_ALLOCATOR
+
+
+
+
+
+
+
+
+
+
+
