d18ad6bfd394302f69d9e38cc2048921ad9c0ad2
SaeruHikari <39457738+SaeruHikari@users.noreply.github.com>
2021-11-18 13:23:30 +0800

    将驱动扩展的初始化结果反映到Instance中

--- include/cgpu/drivers/cgpu_nvapi.h
+++ include/cgpu/drivers/cgpu_nvapi.h
@@ -8,14 +8,7 @@ extern "C" {
 #endif
 #include "cgpu/api.h"
 
-typedef enum ECGpuNvAPI_Status
-{
-	CGPU_NVAPI_OK = 0,          //!< Success. Request is completed.
-    CGPU_NVAPI_NONE = 1,
-    CGPU_NVAPI_ERROR = -1,      //!< Generic error
-} ECGpuNvAPI_Status;
-
-ECGpuNvAPI_Status cgpu_nvapi_init();
+ECGpuNvAPI_Status cgpu_nvapi_init(struct CGpuInstance* Inst);
 void cgpu_nvapi_exit();
 
 
