39c5b4209d0f8c5213f2ad2e8e5139bdadfc1b47
SaeruHikari <39457738+SaeruHikari@users.noreply.github.com>
2021-11-19 12:23:53 +0800

    VkUtil_SelectInstanceExtensions

--- src/gpu/cgpu/vulkan/vma.cpp
+++ src/gpu/cgpu/vulkan/vma.cpp
@@ -4,6 +4,11 @@
 #endif
 
 #define VMA_IMPLEMENTATION
+#define WIN32_MEAN_AND_LEAN
+#define VMA_RECORDING_ENABLED 0
+#define VMA_USE_STL_UNORDERED_MAP 0 // unused
+#define VMA_USE_STL_LIST 0          // off
+#define VMA_USE_STL_VECTOR 0        // off
 #include "cgpu/backend/vulkan/vk_mem_alloc.h"
 
 #ifdef __clang__
