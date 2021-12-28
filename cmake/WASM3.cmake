9df547160f0150f99295629393318fdb4709e04c
SaeruHikari <39457738+SaeruHikari@users.noreply.github.com>
2021-12-28 20:11:33 +0800

    为macos目标添加m3 lib

--- cmake/WASM3.cmake
+++ cmake/WASM3.cmake
@@ -4,10 +4,17 @@ endif()
 
 if(USE_WASM3)
 
-
 if(TARGET_WIN)
     extract_file2(${CMAKE_CURRENT_SOURCE_DIR}/SDKs/m3_d-win.zip ${SAKURA_BIN_DIR}/Debug)
     extract_file2(${CMAKE_CURRENT_SOURCE_DIR}/SDKs/m3-win.zip ${SAKURA_BIN_DIR}/Release)
+elseif(TARGET_MAC)
+    if(${CMAKE_SYSTEM_PROCESSOR} MATCHES "arm64")
+        extract_file2(${CMAKE_CURRENT_SOURCE_DIR}/SDKs/m3_d-macos-arm.zip ${SAKURA_BIN_DIR}/Debug)
+        extract_file2(${CMAKE_CURRENT_SOURCE_DIR}/SDKs/m3-macos-arm.zip ${SAKURA_BIN_DIR}/Release)
+    else()
+        extract_file2(${CMAKE_CURRENT_SOURCE_DIR}/SDKs/m3_d-macos-amd64.zip ${SAKURA_BIN_DIR}/Debug)
+        extract_file2(${CMAKE_CURRENT_SOURCE_DIR}/SDKs/m3-macos-amd64.zip ${SAKURA_BIN_DIR}/Release)
+    endif()
 endif(TARGET_WIN)
 
 set(WASM3_LIBRARIES m3 uv_a uvwasi_a)
