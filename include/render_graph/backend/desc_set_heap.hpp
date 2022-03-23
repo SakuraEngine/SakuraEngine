3b604f8687ddd9b1159e7f1331ce98f41dc39cfc
SaeruHikari <39457738+SaeruHikari@users.noreply.github.com>
2022-03-24 01:14:28 +0800

    为单帧执行器实现一个完全单向的DescriptorSet堆

---
+++ include/render_graph/backend/desc_set_heap.hpp
@@ -0,0 +1,45 @@
+#pragma once
+#include <gsl/span>
+#include "render_graph/frontend/render_graph.hpp"
+
+namespace sakura
+{
+namespace render_graph
+{
+// thread-unsafe descriptor set heap
+// it's supposed to be resized only once at compile
+class DescSetHeap
+{
+public:
+    inline void expand(size_t set_count = 1)
+    {
+        for (uint32_t i = 0; i < root_sig->table_count; i++)
+        {
+            CGpuDescriptorSetDescriptor desc = {};
+            desc.root_signature = root_sig;
+            desc.set_index = i;
+            auto new_set = cgpu_create_descriptor_set(root_sig->device, &desc);
+            heap.emplace_back(new_set);
+        }
+    }
+    inline const gsl::span<CGpuDescriptorSetId> pop()
+    {
+        if (cursor >= heap.size()) expand();
+        auto res = gsl::span<CGpuDescriptorSetId>(heap.data() + cursor, root_sig->table_count);
+        cursor += root_sig->table_count;
+        return res;
+    }
+    inline void reset() { cursor = 0; }
+    void destroy()
+    {
+        for (auto desc_set : heap)
+            cgpu_free_descriptor_set(desc_set);
+    }
+
+protected:
+    std::atomic_uint32_t cursor = 0;
+    CGpuRootSignatureId root_sig;
+    eastl::vector<CGpuDescriptorSetId> heap;
+};
+} // namespace render_graph
+} // namespace sakura
