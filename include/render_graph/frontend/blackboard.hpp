f171975d54957a5f092c062de84a2fdb31fa3163
SaeruHikari <39457738+SaeruHikari@users.noreply.github.com>
2022-03-24 01:12:50 +0800

    整理代码，引入一个Subresource边

--- include/render_graph/frontend/blackboard.hpp
+++ include/render_graph/frontend/blackboard.hpp
@@ -8,9 +8,9 @@ namespace render_graph
 {
 class Blackboard
 {
+public:
     friend class RenderGraph;
 
-public:
 protected:
     eastl::string_map<class PassNode*> named_passes;
     eastl::string_map<class TextureNode*> named_textures;
