5bd4ceab99b07d8ed61818754e7a3b5c60619f16
SaeruHikari <39457738+SaeruHikari@users.noreply.github.com>
2022-04-19 17:54:44 +0800

    windows上引入tracy

--- cmake/Tracy.cmake
+++ cmake/Tracy.cmake
@@ -1,5 +1,5 @@
 if(TARGET_WIN)
-    extract_file2(${CMAKE_CURRENT_SOURCE_DIR}/SDKs/tracyclient-win.zip 
+    extract_file2(${CMAKE_CURRENT_SOURCE_DIR}/SDKs/tracyclient_d-win.zip 
         ${SAKURA_BIN_DIR}/Debug
     )
     extract_file2(${CMAKE_CURRENT_SOURCE_DIR}/SDKs/tracyclient-win.zip 
