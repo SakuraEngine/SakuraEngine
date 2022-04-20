13540eba9ce0e3da8f8f8eb354e3b8fb1399e85a
SaeruHikari <39457738+SaeruHikari@users.noreply.github.com>
2022-04-21 00:34:19 +0800

    更新VMA至3.0.0

--- include/render_graph/frontend/base_types.hpp
+++ include/render_graph/frontend/base_types.hpp
@@ -18,7 +18,6 @@ enum class EObjectType : uint8_t
 
 enum class ERelationshipType : uint8_t
 {
-    SubResource,
     TextureRead,      // SRV
     TextureWrite,     // RTV/DSV
     TextureReadWrite, // UAV
