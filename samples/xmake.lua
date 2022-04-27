b2dffd3cb62ae168f9a9be3e14ac721312cde6c1
gaymenhub <672619410@qq.com>
2022-04-27 19:29:45 +0800

    ecs 使用 mimalloc

--- samples/xmake.lua
+++ samples/xmake.lua
@@ -15,3 +15,12 @@ target("cgpu-texture")
     add_deps("SkrRT")
     add_files("cgpu-texture/*.c")
     add_files("cgpu-texture/**.hlsl")
+
+target("cgpu-3d")
+    add_rules("utils.dxc", {
+        spv_outdir = "/../resources/shaders/cgpu-3d",
+        dxil_outdir = "/../resources/shaders/cgpu-3d"})
+    set_kind("binary")
+    add_deps("SkrRT")
+    add_files("cgpu-3d/**.cpp")
+    add_files("cgpu-3d/**.hlsl")
--- src/ecs/pool.cpp
+++ src/ecs/pool.cpp
@@ -2,75 +2,80 @@
 #include "constants.hpp"
 #include <vector>
 #include <numeric>
+#include "utils.hpp"
 
 namespace dual
 {
-    pool_t::pool_t(size_t blockSize, size_t blockCount)
-        : blockSize(blockSize), blocks(blockCount)
-    {}
-
-    pool_t::~pool_t()
-    {
-        void* block;
-        while(blocks.try_dequeue(block))
-            delete (char*)block;
-    }
-
-    void* pool_t::allocate()
-    {
-        void* block; 
-        if(blocks.try_dequeue(block))
-            return block;
-        return new char[blockSize];
-    }
-
-    void pool_t::free(void* block)
-    {
-        if(blocks.try_enqueue(block))
-            return;
-        delete (char*)block;
-    }
-
-    fixed_pool_t::fixed_pool_t(size_t blockSize, size_t blockCount)
-        : blockSize(blockSize), blockCount(blockCount), blocks(blockCount)
-    {
-        buffer = new char[blockSize * blockCount];
-        std::vector<size_t> indicies;
-        indicies.resize(blockCount);
-        std::iota(indicies.begin(), indicies.end(), 0);
-        for(size_t i=0; i<blockCount; ++i)
-            blocks.try_enqueue_bulk(indicies.data(), blockCount);
-    }
-
-    fixed_pool_t::~fixed_pool_t()
-    {
-        delete buffer;
-    }
-
-    void* fixed_pool_t::allocate()
-    {
-        size_t block; 
-        if(blocks.try_dequeue(block))
-            return buffer+block*blockSize;
-        return nullptr;
-    }
-
-    void fixed_pool_t::free(void* block)
-    {
-        if(block < buffer || block > buffer + blockSize * blockCount)
-            return;
-        if(blocks.try_enqueue(((char*)block - buffer)/blockSize))
-            return;
-    }
-
-    void fixed_pool_t::reset()
-    {
-        moodycamel::ConcurrentQueue<size_t> temp(blockCount);
-        blocks.swap(temp);
-        std::vector<size_t> indicies;
-        indicies.resize(blockCount);
-        std::iota(indicies.begin(), indicies.end(), 0);
-        for(size_t i=0; i<blockCount; ++i)
-            blocks.try_enqueue_bulk(indicies.data(), blockCount);
-    }
+pool_t::pool_t(size_t blockSize, size_t blockCount)
+    : blockSize(blockSize)
+    , blocks(blockCount)
+{
 }
+
+pool_t::~pool_t()
+{
+    void* block;
+    while (blocks.try_dequeue(block))
+        delete (char*)block;
+}
+
+void* pool_t::allocate()
+{
+    void* block;
+    if (blocks.try_dequeue(block))
+        return block;
+    return dual_malloc(blockSize);
+}
+
+void pool_t::free(void* block)
+{
+    if (blocks.try_enqueue(block))
+        return;
+    dual_free(block);
+}
+
+fixed_pool_t::fixed_pool_t(size_t blockSize, size_t blockCount)
+    : blockSize(blockSize)
+    , blockCount(blockCount)
+    , blocks(blockCount)
+{
+    buffer = new char[blockSize * blockCount];
+    std::vector<size_t> indicies;
+    indicies.resize(blockCount);
+    std::iota(indicies.begin(), indicies.end(), 0);
+    for (size_t i = 0; i < blockCount; ++i)
+        blocks.try_enqueue_bulk(indicies.data(), blockCount);
+}
+
+fixed_pool_t::~fixed_pool_t()
+{
+    delete buffer;
+}
+
+void* fixed_pool_t::allocate()
+{
+    size_t block;
+    if (blocks.try_dequeue(block))
+        return buffer + block * blockSize;
+    return nullptr;
+}
+
+void fixed_pool_t::free(void* block)
+{
+    if (block < buffer || block > buffer + blockSize * blockCount)
+        return;
+    if (blocks.try_enqueue(((char*)block - buffer) / blockSize))
+        return;
+}
+
+void fixed_pool_t::reset()
+{
+    moodycamel::ConcurrentQueue<size_t> temp(blockCount);
+    blocks.swap(temp);
+    std::vector<size_t> indicies;
+    indicies.resize(blockCount);
+    std::iota(indicies.begin(), indicies.end(), 0);
+    for (size_t i = 0; i < blockCount; ++i)
+        blocks.try_enqueue_bulk(indicies.data(), blockCount);
+}
+} // namespace dual
