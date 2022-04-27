17a285bfe7e97b5cdbd7da396babb1165c760b74
SaeruHikari <39457738+SaeruHikari@users.noreply.github.com>
2022-04-27 12:51:07 +0800

    prepare resources with xmake

--- samples/xmake.lua
+++ samples/xmake.lua
@@ -1,9 +1,17 @@
 target("rg-deferred")
+    add_rules("utils.dxc", {
+        spv_outdir = "/../resources/shaders/rg-deferred",
+        dxil_outdir = "/../resources/shaders/rg-deferred"})
     set_kind("binary")
     add_deps("SkrRT")
     add_files("rg-deferred/*.cpp")
+    add_files("rg-deferred/**.hlsl")
     
 target("cgpu-texture")
+    add_rules("utils.dxc", {
+        spv_outdir = "/../resources/shaders/cgpu-texture",
+        dxil_outdir = "/../resources/shaders/cgpu-texture"})
     set_kind("binary")
     add_deps("SkrRT")
     add_files("cgpu-texture/*.c")
+    add_files("cgpu-texture/**.hlsl")
