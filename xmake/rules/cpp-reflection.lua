09567067d926aad80e970910a8f0e1585f8c9242
SaeruHikari <39457738+SaeruHikari@users.noreply.github.com>
2022-05-02 20:30:37 +0800

    初步加入代码生成逻辑

--- xmake/rules/cpp-reflection.lua
+++ xmake/rules/cpp-reflection.lua
@@ -1,5 +1,5 @@
 rule("c++.reflection")
-    after_load(function (target)
+    after_load(function (target, opt)
         import("gen_refl")
         local headerfiles = {}
         local files = target:extraconf("rules", "c++.reflection", "files")
@@ -11,9 +11,17 @@ rule("c++.reflection")
         end
         gen_refl(target, headerfiles)
     end)
-    before_build(function (target, opt)
+    on_config(function (target, opt)
         import("gen_refl")
         local rootdir = target:extraconf("rules", "c++.reflection", "rootdir")
         local abs_rootdir = path.absolute(path.join(target:scriptdir(), rootdir))
         gen_refl.generate_refl_files(target, abs_rootdir, opt)
+        -- add to sourcebatch
+        local sourcebatches = target:sourcebatches()
+        local gendir = path.join(target:autogendir({root = true}), target:plat(), "reflection/generated")
+        target:add("includedirs", gendir)
+        local cppfiles = os.files(path.join(gendir, "/**.cpp"))
+        for _, file in ipairs(cppfiles) do
+            target:add("files", file)
+        end
     end)
