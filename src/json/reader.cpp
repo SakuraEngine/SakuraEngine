97001d2917240f820339e1a353b72bb52145406b
SaeruHikari <39457738+SaeruHikari@users.noreply.github.com>
2022-05-04 18:05:06 +0800

    修复noexception下的构建

--- src/json/reader.cpp
+++ src/json/reader.cpp
@@ -5,21 +5,21 @@ namespace skr::json
 {
 // TODO: error handling
 template <>
-void Read(simdjson::ondemand::value&& json, bool& b) { b = json.get_bool(); }
+void Read(simdjson::ondemand::value&& json, bool& b) { b = json.get_bool().value(); }
 template <>
-void Read(simdjson::ondemand::value&& json, int32_t& b) { b = json.get_int64(); }
+void Read(simdjson::ondemand::value&& json, int32_t& b) { b = json.get_int64().value_unsafe(); }
 template <>
-void Read(simdjson::ondemand::value&& json, uint32_t& b) { b = json.get_uint64(); }
+void Read(simdjson::ondemand::value&& json, uint32_t& b) { b = json.get_uint64().value_unsafe(); }
 template <>
-void Read(simdjson::ondemand::value&& json, int64_t& b) { b = json.get_int64(); }
+void Read(simdjson::ondemand::value&& json, int64_t& b) { b = json.get_int64().value_unsafe(); }
 template <>
-void Read(simdjson::ondemand::value&& json, uint64_t& b) { b = json.get_uint64(); }
+void Read(simdjson::ondemand::value&& json, uint64_t& b) { b = json.get_uint64().value_unsafe(); }
 template <>
-void Read(simdjson::ondemand::value&& json, double& b) { b = json.get_double(); }
+void Read(simdjson::ondemand::value&& json, double& b) { b = json.get_double().value_unsafe(); }
 template <>
 void Read(simdjson::ondemand::value&& json, eastl::string& str)
 {
-    std::string_view view = json.get_string();
+    std::string_view view = json.get_string().value_unsafe();
     str = eastl::string(view.data(), view.length());
 }
 } // namespace skr::json
