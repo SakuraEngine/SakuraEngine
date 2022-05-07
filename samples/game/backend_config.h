eabfb4cc80b63891484fd818889ea56737475405
BenzzZX <672619410@qq.com>
2022-05-08 04:04:32 +0800

    enum 改为 fixed enum

--- samples/game/backend_config.h
+++ samples/game/backend_config.h
@@ -9,7 +9,7 @@ typedef enum reflect attr(
 "guid" : "b4b7f387-d8c2-465c-9b3a-6d83a3d198b1",
 "serialize" : true
 )
-ECGPUBackEnd : uint8_t{ Vulkan, DX12, Metal } ECGPUBackEnd;
+ECGPUBackEnd : uint32_t{ Vulkan, DX12, Metal } ECGPUBackEnd;
 
 typedef struct reflect attr(
 "guid" : "b537f7b1-6d2d-44f6-b313-bcb559d3f490",
