8be1fa0d8349e28f9dee1bc6a5f395954717ba20
SaeruHikari <39457738+SaeruHikari@users.noreply.github.com>
2022-05-10 21:50:47 +0800

    remove sync io support

---
+++ tools/core/include/assets/hlsl-shader.h
@@ -0,0 +1,11 @@
+#pragma once
+#include "platform/configure.h"
+#include "platform/guid.h"
+#include "cgpu/api.h"
+
+struct reflect attr (
+    "guid" : "5405827d-78dc-4b10-bc12-a071580acccd"
+)
+HLSLShader{
+
+};
