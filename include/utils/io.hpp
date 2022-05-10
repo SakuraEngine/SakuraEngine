79e4d7b408388bc3da95668d9eecdc09d77f4ae0
SaeruHikari <39457738+SaeruHikari@users.noreply.github.com>
2022-05-10 21:50:47 +0800

    remove sync io support

--- include/utils/io.hpp
+++ include/utils/io.hpp
@@ -13,7 +13,6 @@ typedef struct RUNTIME_API RAM {
     static void request(skr_vfs_t* vfs,
     const char8_t* path, uint8_t* bytes,
     uint64_t offset, uint64_t size, skr_async_io_request_t* request);
-    static eastl::unique_ptr<ftl::TaskScheduler> ramTaskScheduler;
 } RAM;
 } // namespace io
 } // namespace skr
