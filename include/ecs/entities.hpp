c629a8c555275ae5a8a21d2a3070347c97da826e
BenzzZX <672619410@qq.com>
2022-05-15 03:28:46 +0800

    把 ecs 的 id 分配器拿出来复用

---
+++ include/ecs/entities.hpp
@@ -0,0 +1,26 @@
+#pragma once
+#include <vector>
+#include "dual.h"
+namespace dual
+{
+struct entity_registry_t {
+    struct entry_t {
+        dual_chunk_t* chunk;
+        uint32_t indexInChunk : 24;
+        uint32_t version : 8;
+    };
+    std::vector<entry_t> entries;
+    std::vector<EIndex> freeEntries;
+    uint32_t size = 0;
+
+    void reset();
+    void shrink();
+    void new_entities(dual_entity_t* dst, EIndex count);
+    void free_entities(const dual_entity_t* dst, EIndex count);
+    void fill_entities(const dual_chunk_view_t& view);
+    void fill_entities(const dual_chunk_view_t& view, const dual_entity_t* src);
+    void free_entities(const dual_chunk_view_t& view);
+    void move_entities(const dual_chunk_view_t& view, const dual_chunk_t* src, EIndex srcIndex);
+    void move_entities(const dual_chunk_view_t& view, EIndex srcIndex);
+};
+} // namespace dual
