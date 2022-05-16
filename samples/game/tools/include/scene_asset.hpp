5ef65019a1ec8fa40303ddd3f4e8fcdcd30cdd01
gaymenhub <672619410@qq.com>
2022-05-16 21:47:14 +0800

    game scene

---
+++ samples/game/tools/include/scene_asset.hpp
@@ -0,0 +1,44 @@
+#pragma once
+#include "gametool_configure.h"
+#include "platform/guid.h"
+#include "asset/importer.hpp"
+#include "asset/cooker.hpp"
+#include "platform/configure.h"
+#include "utils/hashmap.hpp"
+
+namespace game reflect
+{
+namespace asset reflect
+{
+using namespace skd::asset;
+struct reflect attr(
+"guid" : "4F0E4239-A07F-4F48-B54F-FBF406C60DC3",
+"serialize" : true,
+"importer" : "EFBA637E-E7E5-4B64-BA26-90AEEE9E3E1A"
+)
+TOOL_API SSceneImporter final : public SImporter
+{
+    eastl::string sceneRoot;
+    // mapping from asset path to resource
+    // by default importer will resolve the path to find resource if redirector is not exist
+    skr::flat_hash_map<eastl::string, skr_guid_t> redirectors;
+    using SImporter::SImporter;
+    void* Import(skr::io::RAMService*, const SAssetRecord* record) override;
+};
+
+struct reflect attr(
+"cooker" : "EFBA637E-E7E5-4B64-BA26-90AEEE9E3E1A"
+)
+TOOL_API SSceneCooker final : public SCooker
+{
+    bool Cook(SCookContext * ctx) override;
+    uint32_t Version() override;
+};
+
+struct TOOL_API SSceneImporterFactory final : public SImporterFactory {
+    bool CanImport(const SAssetRecord* record) override;
+    skr_guid_t GetResourceType() override;
+    SImporter* CreateImporter(const SAssetRecord* record) override;
+};
+} // namespace reflect
+} // namespace reflect
---
+++ samples/game/tools/src/scene_asset.cpp
@@ -0,0 +1,10 @@
+#include "scene_asset.hpp"
+
+namespace game::asset
+{
+void* SSceneImporter::Import(skr::io::RAMService*, const SAssetRecord* record)
+{
+
+    return nullptr;
+}
+} // namespace game::asset
