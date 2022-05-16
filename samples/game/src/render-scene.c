1edcdcc66c846a3e4fb41a4a71b95bbecda0a8a7
SaeruHikari <39457738+SaeruHikari@users.noreply.github.com>
2022-05-16 12:20:09 +0800

    修复windows上的构建问题

--- samples/game/src/render-scene.c
+++ samples/game/src/render-scene.c
@@ -1,6 +1,6 @@
 #include "render-scene.h"
 
-GAMERT_API gfx_material_id_t ecsr_register_gfx_material(const gfx_material_t*) SKR_NOEXCEPT
+GAMERT_API gfx_material_id_t ecsr_register_gfx_material(const gfx_material_t* mat) SKR_NOEXCEPT
 {
     return 0;
 }
