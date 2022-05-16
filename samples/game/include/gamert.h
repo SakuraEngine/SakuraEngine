a0a5c8a45339be9b0625c4bba62726fe47714870
SaeruHikari <39457738+SaeruHikari@users.noreply.github.com>
2022-05-16 11:59:59 +0800

    init main dual storage when GameRTModule initialize

--- samples/game/include/gamert.h
+++ samples/game/include/gamert.h
@@ -1,16 +1,17 @@
 #pragma once
 #include "gamert_configure.h"
 #include "platform/vfs.h"
-
+#include "utils/log.h"
+#ifdef __cplusplus
 #include "module/module_manager.hpp"
 #include "render_graph/frontend/render_graph.hpp"
-#include "utils/log.h"
 
+struct dual_storage_t;
 namespace skr::resource
 {
 struct SLocalResourceRegistry;
 }
-class SGameRTModule : public skr::IDynamicModule
+class GAMERT_API SGameRTModule : public skr::IDynamicModule
 {
     virtual void on_load() override;
     virtual void main_module_exec() override;
@@ -19,6 +20,7 @@ class SGameRTModule : public skr::IDynamicModule
 public:
     skr_vfs_t* resource_vfs = nullptr;
     skr::resource::SLocalResourceRegistry* registry;
+    struct dual_storage_t* ecs_world;
 };
 
 namespace skg
@@ -27,3 +29,11 @@ struct GameContext {
 };
 GAMERT_API bool GameLoop(GameContext& ctx);
 } // namespace skg
+
+extern "C"
+{
+#endif
+GAMERT_API struct dual_storage_t* gamert_get_ecs_world();
+#ifdef __cplusplus
+}
+#endif
--- samples/game/src/gamert.cpp
+++ samples/game/src/gamert.cpp
@@ -4,32 +4,7 @@
 #include "platform/memory.h"
 #include "resource/resource_system.h"
 #include "resource/local_resource_registry.h"
-
-void SGameRTModule::on_load()
-{
-    SKR_LOG_INFO("game runtime loaded!");
-    auto resourceRoot = (ghc::filesystem::current_path() / "../resources").u8string();
-    skr_vfs_desc_t vfs_desc = {};
-    vfs_desc.mount_type = SKR_MOUNT_TYPE_CONTENT;
-    vfs_desc.override_mount_dir = resourceRoot.c_str();
-    resource_vfs = skr_create_vfs(&vfs_desc);
-
-    registry = SkrNew<skr::resource::SLocalResourceRegistry>(resource_vfs);
-    skr::resource::GetResourceSystem()->Initialize(registry);
-}
-
-void SGameRTModule::main_module_exec()
-{
-    SKR_LOG_INFO("game runtime executed as main module!");
-}
-
-void SGameRTModule::on_unload()
-{
-    skr::resource::GetResourceSystem()->Shutdown();
-    SkrDelete(registry);
-    skr_free_vfs(resource_vfs);
-    SKR_LOG_INFO("game runtime unloaded!");
-}
+#include "ecs/dual.h"
 
 IMPLEMENT_DYNAMIC_MODULE(SGameRTModule, GameRT);
 SKR_MODULE_METADATA(u8R"(
@@ -47,3 +22,39 @@ SKR_MODULE_METADATA(u8R"(
 }
 )",
 GameRT)
+
+void SGameRTModule::on_load()
+{
+    SKR_LOG_INFO("game runtime loaded!");
+    auto resourceRoot = (ghc::filesystem::current_path() / "../resources").u8string();
+    skr_vfs_desc_t vfs_desc = {};
+    vfs_desc.mount_type = SKR_MOUNT_TYPE_CONTENT;
+    vfs_desc.override_mount_dir = resourceRoot.c_str();
+    resource_vfs = skr_create_vfs(&vfs_desc);
+
+    registry = SkrNew<skr::resource::SLocalResourceRegistry>(resource_vfs);
+    skr::resource::GetResourceSystem()->Initialize(registry);
+
+    ecs_world = dualS_create();
+}
+
+void SGameRTModule::main_module_exec()
+{
+    SKR_LOG_INFO("game runtime executed as main module!");
+}
+
+void SGameRTModule::on_unload()
+{
+    skr::resource::GetResourceSystem()->Shutdown();
+    SkrDelete(registry);
+    skr_free_vfs(resource_vfs);
+
+    dualS_release(ecs_world);
+    SKR_LOG_INFO("game runtime unloaded!");
+}
+
+dual_storage_t* gamert_get_ecs_world()
+{
+    static auto _module = (SGameRTModule*)skr_get_module_manager()->get_module("GameRT");
+    return _module->ecs_world;
+}
