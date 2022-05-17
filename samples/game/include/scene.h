00697f420b1b32c89e975026bf3834c596b611e1
BenzzZX <672619410@qq.com>
2022-05-18 03:23:43 +0800

    hierachy component & transform component

--- samples/game/include/scene.h
+++ samples/game/include/scene.h
@@ -1,5 +1,8 @@
 #pragma once
 #include "ecs/dual.h"
+#if !defined(__meta__) && defined(__cplusplus)
+    #include "GameRT/scene.dual.generated.hpp"
+#endif
 
 struct reflect attr(
     "guid" : "EFBA637E-E7E5-4B64-BA26-90AEEE9E3E1A"
@@ -9,3 +12,25 @@ game_scene_t
     dual_storage_t* world;
 };
 typedef struct game_scene_t game_scene_t;
+
+struct reflect attr(
+    "guid" : "82CDDC11-3D94-4552-8FD4-237A053F35C0",
+    "component" : 
+    {
+        "buffer" : 4
+    }
+) skr_child_t
+{
+    dual_entity_t entity;
+};
+typedef struct skr_child_t skr_child_t;
+
+struct reflect attr(
+    "guid" : "2CAA41D2-54A4-46FB-BE43-68B545F313BF",
+    "component" : true
+)
+skr_parent_t
+{
+    dual_entity_t entity;
+};
+typedef struct skr_parent_t skr_parent_t;
