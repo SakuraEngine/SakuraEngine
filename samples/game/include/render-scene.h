06ec83d266075a115536c1be638cb335e94b4e84
SaeruHikari <39457738+SaeruHikari@users.noreply.github.com>
2022-05-17 14:47:37 +0800

    ent id异常

--- samples/game/include/render-scene.h
+++ samples/game/include/render-scene.h
@@ -84,13 +84,22 @@ typedef struct skr_gfx_mat_param_t {
     skr_gfx_mat_param_freq frequency;
 } skr_gfx_mat_param_t;
 
+// gfx-shaders
 GAMERT_API gfx_shader_set_id_t ecsr_register_gfx_shader_set(const gfx_shader_set_t*) SKR_NOEXCEPT;
 GAMERT_API gfx_shader_set_t* ecsr_query_gfx_shader_set(gfx_shader_set_id_t) SKR_NOEXCEPT;
+GAMERT_API bool ecsr_unregister_gfx_shader_set(gfx_shader_set_id_t) SKR_NOEXCEPT;
+
+// gfx-mats
 GAMERT_API gfx_material_id_t ecsr_register_gfx_material(const gfx_material_t*) SKR_NOEXCEPT;
+GAMERT_API gfx_material_t* ecsr_query_gfx_material(gfx_material_id_t) SKR_NOEXCEPT;
 GAMERT_API bool ecsr_unregister_gfx_material(gfx_material_id_t) SKR_NOEXCEPT;
+
+// gfx-parameters
+GAMERT_API bool ecsr_register_gfx_mat_paramter(gfx_material_id_t mat, const skr_gfx_mat_param_t*) SKR_NOEXCEPT; 
+
+// cs-mats
 GAMERT_API processor_material_t ecsr_register_processor_material(const processor_material_t*) SKR_NOEXCEPT;
 GAMERT_API bool ecsr_unregister_processor_material(processor_material_t) SKR_NOEXCEPT;
-GAMERT_API bool ecsr_register_gfx_mat_paramter(gfx_material_id_t mat, const skr_gfx_mat_param_t*) SKR_NOEXCEPT; 
 
 #ifdef __cplusplus
 }
--- samples/game/src/render-scene.c
+++ samples/game/src/render-scene.c
@@ -8,41 +8,15 @@ static dual_type_index_t processor_shader_set_type;
 static dual_type_index_t gfx_material_type;
 static dual_type_index_t processor_material_type;
 
-typedef struct gfx_shader_set_query_exist_t {
-    gfx_shader_set_id_t exist;
-    size_t hash;
-    dual_storage_t* storage;
-} gfx_shader_set_query_exist_t;
-typedef struct gfx_shader_set_construct_t {
-    const gfx_shader_set_t* value;
-} gfx_shader_set_construct_t;
-void gfx_shader_set_query_exist_callback(void* u, dual_chunk_view_t* view)
+void __gfx_shader_set_construct_callback(void* u, dual_chunk_view_t* view)
 {
-    gfx_shader_set_query_exist_t* query = (gfx_shader_set_query_exist_t*)u;
-    gfx_shader_set_t* sets = (gfx_shader_set_t*)dualV_get_owned_rw(view, gfx_shader_set_type);
-    const dual_entity_t* ents = dualV_get_entities(view);
-    for(uint32_t i = 0; i < view->count; i++)
-    {
-        const size_t hash = skr_hash(&sets[i], sizeof(gfx_shader_set_t), (size_t)query->storage);
-        if(query->hash == hash)
-        {
-            query->exist = ents[i];
-            return;
-        }
-    }
-}
-void gfx_shader_set_construct_callback(void* u, dual_chunk_view_t* view)
-{
-    gfx_shader_set_construct_t* construct = u;
-    *(gfx_shader_set_t*)dualV_get_owned_rw(view, gfx_shader_set_type) = *construct->value;
+    const gfx_shader_set_t* set = u;
+    *(gfx_shader_set_t*)dualV_get_owned_rw(view, gfx_shader_set_type) = *set;
 }
 gfx_shader_set_id_t ecsr_register_gfx_shader_set(const gfx_shader_set_t* set) SKR_NOEXCEPT
 {
     dual_storage_t* storage = gamert_get_ecs_world();
-    gfx_shader_set_construct_t construct = {
-        .value = set
-    };
-    dual_entity_type_t mat_type = {
+    dual_entity_type_t alloc_type = {
         .type.data = &gfx_shader_set_type,
         .type.length = 1,
     };
@@ -50,8 +24,9 @@ gfx_shader_set_id_t ecsr_register_gfx_shader_set(const gfx_shader_set_t* set) SK
         .data = &gfx_shader_set_type,
         .length = 1
     };
-    return dualX_hashset_insert(storage, &key_set, &mat_type, &gfx_shader_set_construct_callback, &construct);
+    return dualX_hashset_insert(storage, &key_set, &alloc_type, &__gfx_shader_set_construct_callback, (void*)set);
 }
+
 gfx_shader_set_t* ecsr_query_gfx_shader_set(gfx_shader_set_id_t id) SKR_NOEXCEPT
 {
     dual_storage_t* storage = gamert_get_ecs_world();
@@ -60,11 +35,51 @@ gfx_shader_set_t* ecsr_query_gfx_shader_set(gfx_shader_set_id_t id) SKR_NOEXCEPT
     return (gfx_shader_set_t*)dualV_get_owned_rw(&view, gfx_shader_set_type);
 }
 
+bool ecsr_unregister_gfx_shader_set(gfx_shader_set_id_t ent) SKR_NOEXCEPT
+{
+    dual_storage_t* storage = gamert_get_ecs_world();
+    dual_chunk_view_t cv = {0};
+    dualS_access(storage, ent, &cv);
+    dualS_destroy(storage, &cv);
+    return true;
+}
+
+void __gfx_material_construct_callback(void* u, dual_chunk_view_t* view)
+{
+    const gfx_material_t* mat = u;
+    *(gfx_material_t*)dualV_get_owned_rw(view, gfx_material_type) = *mat;
+}
 gfx_material_id_t ecsr_register_gfx_material(const gfx_material_t* mat) SKR_NOEXCEPT
 {
     dual_storage_t* storage = gamert_get_ecs_world();
-    (void)storage;
-    return 0;
+    dual_entity_type_t alloc_type = {
+        .type.data = &gfx_material_type,
+        .type.length = 1,
+        .meta.data = &mat->m_gfx,
+        .meta.length = 1
+    };
+    dual_type_set_t key_set = {
+        .data = &gfx_material_type,
+        .length = 1
+    };
+    return dualX_hashset_insert(storage, &key_set, &alloc_type, &__gfx_material_construct_callback, (void*)mat);
+}
+
+gfx_material_t* ecsr_query_gfx_material(gfx_material_id_t id) SKR_NOEXCEPT
+{
+    dual_storage_t* storage = gamert_get_ecs_world();
+    dual_chunk_view_t view = {0};
+    dualS_access(storage, id, &view);
+    return (gfx_material_t*)dualV_get_owned_rw(&view, gfx_material_type);
+}
+
+bool ecsr_unregister_gfx_material(gfx_material_id_t ent) SKR_NOEXCEPT
+{
+    dual_storage_t* storage = gamert_get_ecs_world();
+    dual_chunk_view_t cv = {0};
+    dualS_access(storage, ent, &cv);
+    dualS_destroy(storage, &cv);
+    return true;
 }
 
 void ecsr_register_types()
