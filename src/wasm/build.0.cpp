3f1ac011f763d36529d2d8955d3fc88c19872d72
SaeruHikari <39457738+SaeruHikari@users.noreply.github.com>
2022-05-17 15:14:38 +0800

    暂时屏蔽wasm构建

--- src/wasm/build.0.cpp
+++ src/wasm/build.0.cpp
@@ -1,6 +1,10 @@
+#ifdef SKR_USE_WASM
+
 #if !defined(__EMSCRIPTEN__) && !defined(__wasi__)
     #include "common/swa.cpp"
     #ifdef USE_M3
         #include "wasm3/swa_wasm3.cpp"
     #endif
+#endif
+
 #endif
