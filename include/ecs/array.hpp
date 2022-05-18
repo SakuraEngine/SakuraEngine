51ffadcc1c20f0509e14847ef9c446150590d8ca
BenzzZX <672619410@qq.com>
2022-05-18 19:09:10 +0800

    修复 array component 的参数

--- include/ecs/array.hpp
+++ include/ecs/array.hpp
@@ -9,7 +9,7 @@ struct dual_array_component_t : llvm_vecsmall::SmallVectorBase {
 namespace dual
 {
 template <class T, size_t N>
-using array_component_T = llvm_vecsmall::SmallVector<T, N / sizeof(T)>;
+using array_component_T = llvm_vecsmall::SmallVector<T, N>;
 
 using link_array_t = array_component_T<dual_entity_t, kLinkComponentSize>;
 } // namespace dual
