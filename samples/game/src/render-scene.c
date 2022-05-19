b0466b14aeb79fede996816c7f77e42863655108
SaeruHikari <39457738+SaeruHikari@users.noreply.github.com>
2022-05-19 12:51:02 +0800

    ecsr_renderable_primitive_type

--- samples/game/src/render-scene.c
+++ samples/game/src/render-scene.c
@@ -58,7 +58,8 @@ const char8_t* entry = "material";
 void __gfx_material_construct_callback(void* u, dual_chunk_view_t* view)
 {
     const gfx_material_t* mat = u;
-    *(gfx_material_t*)dualV_get_owned_rw(view, gfx_material_type) = *mat;
+    gfx_material_t* new_mat = (gfx_material_t*)dualV_get_owned_rw(view, gfx_material_type);
+    *new_mat = *mat;
     CGPURootSignatureId* pRS = dualV_get_owned_rw(view, gfx_root_sig_type);
     gfx_shader_set_t* set = ecsr_query_gfx_shader_set(mat->m_gfx);
     uint32_t sindex = 0;
@@ -144,12 +145,6 @@ bool ecsr_unregister_gfx_material(gfx_material_id_t ent) SKR_NOEXCEPT
     return true;
 }
 
-bool ecsr_renderable_primitive_type(const skr_scene_primitive_desc_t* desc, 
-dual_type_index_t* ctypes, uint32_t ctype_count, dual_entity_t* emetas, uint32_t meta_count)
-{
-    return false;   
-}
-
 /*
 void ecsr_draw_with_gfx_mat_inst(dual_entity_type_t type, gfx_material_inst_id_t mat_id) SKR_NOEXCEPT
 {
@@ -214,15 +209,6 @@ void ecsr_register_types()
         processor_material_type = dualT_register_type(&desc);
     }
     // objects on scene prims
-    {
-        dual_type_description_t desc = {
-            .name = "gfx_material_inst",
-            .size = sizeof(gfx_material_inst_t),
-            .guid = gfx_material_inst_guid,
-            .alignment = _Alignof(gfx_material_inst_t)
-        };
-        index_buffer_type = dualT_register_type(&desc);
-    }
     {
         dual_type_description_t desc = {
             .name = "transform",
