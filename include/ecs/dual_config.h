e0176c16fcd88814c96fac5f1e737844aa78fb71
gaymenhub <672619410@qq.com>
2022-05-20 19:13:45 +0800

    修复 small vector grow 的 bug

--- include/ecs/dual_config.h
+++ include/ecs/dual_config.h
@@ -38,29 +38,12 @@ typedef struct dual_entity_debug_proxy_t {
 
 #include "platform/memory.h"
 
-#ifdef _CRTDBG_MAP_ALLOC
-    #include <crtdbg.h>
-FORCEINLINE static void* _aligned_calloc(size_t nelem, size_t elsize, size_t alignment)
-{
-    void* memory = _aligned_malloc(nelem * elsize, alignment);
-    if (memory != NULL) memset(memory, 0, nelem * elsize);
-    return memory;
-}
-    #define dual_malloc malloc
-    #define dual_malloc_aligned _aligned_malloc
-    #define dual_calloc calloc
-    #define dual_calloc_aligned _aligned_calloc
-    #define dual_memalign _aligned_malloc
-    #define dual_free free
-    #define dual_free_aligned _aligned_free
-    #define DEBUG_NEW_SOURCE_LINE (_NORMAL_BLOCK, __FILE__, __LINE__)
-#else
-    #define dual_malloc sakura_malloc
-    #define dual_malloc_aligned sakura_malloc_aligned
-    #define dual_calloc sakura_calloc
-    #define dual_calloc_aligned sakura_calloc_aligned
-    #define dual_memalign sakura_malloc_aligned
-    #define dual_free sakura_free
-    #define dual_free_aligned sakura_free
-    #define DEBUG_NEW_SOURCE_LINE
-#endif
+
+#define dual_malloc sakura_malloc
+#define dual_malloc_aligned sakura_malloc_aligned
+#define dual_calloc sakura_calloc
+#define dual_calloc_aligned sakura_calloc_aligned
+#define dual_memalign sakura_malloc_aligned
+#define dual_free sakura_free
+#define dual_free_aligned sakura_free
+#define dual_realloc sakura_realloc
