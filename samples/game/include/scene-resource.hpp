e7eb64098f0f4ad9009dabd6c69100da861be8ce
BenzzZX <672619410@qq.com>
2022-06-04 20:25:54 +0800

    scene 的加载 （WIP）

---
+++ samples/game/include/scene-resource.hpp
@@ -0,0 +1,14 @@
+#pragma once
+#include "gamert_configure.h"
+#include "resource/resource_factory.h"
+namespace skg::resource
+{
+    using namespace skr::resource;
+    struct GAMERT_API SSceneFactory : SResourceFactory
+    {
+        skr_type_id_t GetResourceType() override;
+
+        ESkrLoadStatus Load(skr_resource_record_t* record) override;
+        ESkrInstallStatus Install(skr_resource_record_t* record) override;
+    };  
+}
