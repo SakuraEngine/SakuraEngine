5d13b92cadb00ef67651b9b16c7b1c306871c219
hikahuang <39457738+SaeruHikari@users.noreply.github.com>
2022-07-05 16:10:43 +0800

    更正一些构建错误

--- samples/cgpu/hot-triangle/compile_32.bat
+++ samples/cgpu/hot-triangle/compile_32.bat
@@ -1,3 +1,3 @@
-@%~dp0../../SDKs/tools/windows/clang -c -O3 --target=wasm32 -I %~dp0../../include/ -I %~dp0../../SDKs/wasm-include/ %~dp0triangle_module.wa.c -o %cd%/../triangle_module32.o -Wno-macro-redefined
-@%~dp0../../SDKs/tools/windows/wasm-ld --no-entry --strip-all --allow-undefined --export-all %cd%/../triangle_module32.o -o %cd%/../Debug/triangle_module32.wasm
-@%~dp0../../SDKs/tools/windows/wasm-ld --no-entry --strip-all --allow-undefined --export-all %cd%/../triangle_module32.o -o %cd%/../Release/triangle_module32.wasm
+@%~dp0../../../SDKs/tools/windows/clang -c -O3 --target=wasm32 -I %~dp0../../../include/ -I %~dp0../../../SDKs/wasm-include/ %~dp0triangle_module.wa.c -o %cd%/../triangle_module32.o -Wno-macro-redefined
+@%~dp0../../../SDKs/tools/windows/wasm-ld --no-entry --strip-all --allow-undefined --export-all %cd%/../triangle_module32.o -o %cd%/../Debug/triangle_module32.wasm
+@%~dp0../../../SDKs/tools/windows/wasm-ld --no-entry --strip-all --allow-undefined --export-all %cd%/../triangle_module32.o -o %cd%/../Release/triangle_module32.wasm
