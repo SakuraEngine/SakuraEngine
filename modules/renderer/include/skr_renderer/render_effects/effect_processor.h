15b58208222ceea2329b403ef75f2b0aadbe7c29
hikahuang <39457738+SaeruHikari@users.noreply.github.com>
2022-07-06 18:01:49 +0800

    更正写错的接口

--- modules/renderer/include/skr_renderer/render_effects/effect_processor.h
+++ modules/renderer/include/skr_renderer/render_effects/effect_processor.h
@@ -37,7 +37,7 @@ typedef void (*SProcRenderEffectAddData)(SGameEntity entity, const char* effect_
 typedef void (*SProcRenderEffectRemoveData)(SGameEntity entity, const char* effect_name, dual_type_index_t type);
 
 // Drawcall interfaces for effect processor
-typedef uint32_t (*SProcRenderEffectProduceDrawcall)(SGameSceneStorage game_storage, SRenderStorage effect_storage);
+typedef uint32_t (*SProcRenderEffectProduceDrawcall)(SGameSceneStorage* game_storage, SRenderStorage* effect_storage);
 typedef void (*SProcRenderEffectPeekDrawcall)(skr_primitive_draw_t* drawcalls, uint32_t peek_count);
 
 typedef struct VtblRenderEffectProcessor {
@@ -53,7 +53,7 @@ typedef struct VtblRenderEffectProcessor {
 typedef struct IRenderEffectProcessor {
     virtual void on_attach(SGameEntity entity, SRenderEffectEntity effect) = 0;
     virtual void on_detach(SGameEntity entity, SRenderEffectEntity effect) = 0;
-    virtual uint32_t produce_drawcall(SGameSceneStorage game_storage, SRenderStorage effect_storage) = 0;
+    virtual uint32_t produce_drawcall(SGameSceneStorage* game_storage, SRenderStorage* effect_storage) = 0;
     virtual void peek_drawcall(skr_primitive_draw_t* drawcalls, uint32_t peek_count) = 0;
 } IRenderEffectProcessor;
 
