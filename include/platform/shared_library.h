855d5bdc000619f4adde52a5d338ea5784783dad
SaeruHikari <39457738+SaeruHikari@users.noreply.github.com>
2022-07-11 23:02:58 +0800

    独立 shared_library 实现文件

--- include/platform/shared_library.h
+++ include/platform/shared_library.h
@@ -9,25 +9,18 @@
  */
 #pragma once
 #include "configure.h"
-
-#if defined(SKR_OS_UNIX)
-    #include <dlfcn.h>
-using NativeLibHandle = void*;
-#elif defined(SKR_OS_WINDOWS)
-    #include <windows.h>
-    #include <ghc/filesystem.hpp>
-using NativeLibHandle = HMODULE;
-#endif
 #include <EASTL/string.h>
 
 namespace skr
 {
+using NativeLibHandle = void*;
+
 /**
  * @description: Provides cross-platform low-level
  * access to shared library
  * @author: SaeruHikari
  */
-class SharedLibrary
+class RUNTIME_API SharedLibrary
 {
 public:
     SharedLibrary() = default;
@@ -51,45 +44,24 @@ public:
      * @return: result of the operation
      * @author: SaeruHikari
      */
-    bool load(const char* path)
-    {
-        if (isLoaded() && !unload())
-            return false;
-        return loadImpl(path);
-    }
-
+    bool load(const char* path);
     /**
      * @description: checks if the library is loaded.
      * @author: SaeruHikari
      */
-    bool isLoaded() const
-    {
-        return _handle != nullptr;
-    }
+    bool isLoaded() const;
     /**
      * @description: unload the shared library.
      * @author: SaeruHikari
      */
-    bool unload()
-    {
-        return isLoaded() && unloadImpl();
-    }
-
+    bool unload();
     /**
      * @description: Checks if the library has the symbol
      * specified by @a symbolName
      * @param symbolName
      * @author: SaeruHikari
      */
-    bool hasSymbol(const char* symbolName)
-    {
-        eastl::string error = _lastError;
-        getImpl(symbolName);
-        bool has = _lastError.empty();
-        _lastError = error;
-        return has;
-    }
-
+    bool hasSymbol(const char* symbolName);
     /**
      * @description: Returns the symbol specified by @a symbolName
      * need to ensuring the symbol type.
@@ -101,7 +73,6 @@ public:
     {
         return *(reinterpret_cast<SymT*>(reinterpret_cast<uintptr_t>(getImpl(symbolName))));
     }
-
     /**
      * @description:  Get the address of a symbol.
      *  Returns nullptr if the library doesn't have the symbol.
@@ -109,158 +80,37 @@ public:
      * @return The address
      * @author: SaeruHikari
      */
-    void* getRawAddress(const char* symbolName)
-    {
-        return getImpl(symbolName);
-    }
-
+    void* getRawAddress(const char* symbolName);
     /**
      * @description: Checks if the last call raise an error.
      * @author: SaeruHikari
      */
-    bool hasError() const
-    {
-        return !_lastError.empty();
-    }
-
+    bool hasError() const;
     /**
      * @description: Get the last error string.
      * @author: SaeruHikari
      */
-    eastl::string errorString() const
-    {
-        return _lastError;
-    }
-
+    eastl::string errorString() const;
     /**
      * @description: Get the native handle of lib on the system.
      * @author: SaeruHikari
      */
-    NativeLibHandle handle() const
-    {
-        return _handle;
-    }
+    NativeLibHandle handle() const;
 
 private:
     eastl::string _lastError;
     NativeLibHandle _handle = nullptr;
     // Linux implementation
 #if defined(SKR_OS_UNIX)
-    bool loadImpl(const char* path)
-    {
-        _lastError.clear();
-        _handle = dlopen(path, RTLD_LAZY | RTLD_LOCAL);
-        if (!_handle)
-        {
-            _lastError = dlerror();
-            return false;
-        }
-        return true;
-    }
-
-    bool unloadImpl()
-    {
-        _lastError.clear();
-        // dlclose returns 0 on success
-        if (dlclose(_handle) != 0)
-        {
-            _lastError = dlerror();
-            return false;
-        }
-        _handle = nullptr;
-        return true;
-    }
-
-    void* getImpl(const char* symbolName)
-    {
-        _lastError.clear();
-        dlerror();
-        void* symbol = dlsym(_handle, symbolName);
-        const char* error = dlerror();
-        if (error)
-        {
-            // An error occured
-            _lastError = error;
-            return nullptr;
-        }
-        return symbol;
-    }
+    bool loadImpl(const char* path);
+    bool unloadImpl();
+    void* getImpl(const char* symbolName);
 #elif defined(SKR_OS_WINDOWS) // Windows implementation
-    void tchar_to_utf8(const TCHAR* str, char* str8)
-    {
-    #ifdef _UNICODE
-        auto size = WideCharToMultiByte(CP_UTF8, 0, str, (int)wcslen(str), NULL, 0, NULL, NULL);
-        WideCharToMultiByte(CP_UTF8, 0, str, (int)wcslen(str), str8, size, NULL, NULL);
-        str8[size] = '\0';
-    #else
-        return strcpy(str8, str);
-    #endif
-    }
-
     // Return a string explaining the last error
-    eastl::string getWindowsError()
-    {
-        DWORD lastError = GetLastError();
-        TCHAR buffer[256];
-        char8_t u8str[256];
-        if (lastError != 0)
-        {
-            FormatMessage(FORMAT_MESSAGE_FROM_SYSTEM,
-            nullptr,
-            lastError,
-            MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
-            buffer,
-            256 - 1,
-            nullptr);
-            tchar_to_utf8(buffer, u8str);
-            return eastl::string(u8str);
-        }
-        return eastl::string();
-    }
-
-    bool loadImpl(const char* path)
-    {
-        _lastError.clear();
-        if (path == nullptr)
-        {
-            _handle = GetModuleHandle(nullptr);
-        }
-        else
-        {
-            auto wpath = ghc::filesystem::path(path);
-            _handle = LoadLibrary(wpath.c_str());
-        }
-        if (!_handle)
-        {
-            _lastError = getWindowsError();
-            return false;
-        }
-        return true;
-    }
-
-    bool unloadImpl()
-    {
-        _lastError.clear();
-        if (!FreeLibrary(_handle))
-        {
-            _lastError = getWindowsError();
-            return false;
-        }
-        _handle = nullptr;
-        return true;
-    }
-
-    void* getImpl(const char* symbolName)
-    {
-        _lastError.clear();
-        void* addr = (void*)GetProcAddress(_handle, symbolName);
-        if (!addr)
-        {
-            _lastError = getWindowsError();
-            return nullptr;
-        }
-        return addr;
-    }
+    eastl::string getWindowsError();
+    bool loadImpl(const char* path);
+    bool unloadImpl();
+    void* getImpl(const char* symbolName);
 #endif
 };
 
---
+++ src/platform/shared_library.cpp
@@ -0,0 +1,173 @@
+#include "platform/shared_library.h"
+#include <minwindef.h>
+
+#if defined(SKR_OS_UNIX)
+    #include <dlfcn.h>
+#elif defined(SKR_OS_WINDOWS)
+    #include <windows.h>
+    #include <ghc/filesystem.hpp>
+#endif
+
+
+bool skr::SharedLibrary::load(const char* path)
+{
+    if (isLoaded() && !unload())
+        return false;
+    return loadImpl(path);
+}
+
+bool skr::SharedLibrary::isLoaded() const
+{
+    return _handle != nullptr;
+}
+
+bool skr::SharedLibrary::unload()
+{
+    return isLoaded() && unloadImpl();
+}
+
+bool skr::SharedLibrary::hasSymbol(const char* symbolName)
+{
+    eastl::string error = _lastError;
+    getImpl(symbolName);
+    bool has = _lastError.empty();
+    _lastError = error;
+    return has;
+}
+
+void* skr::SharedLibrary::getRawAddress(const char* symbolName)
+{
+    return getImpl(symbolName);
+}
+
+bool skr::SharedLibrary::hasError() const
+{
+    return !_lastError.empty();
+}
+
+eastl::string skr::SharedLibrary::errorString() const
+{
+    return _lastError;
+}
+
+skr::NativeLibHandle skr::SharedLibrary::handle() const
+{
+    return _handle;
+}
+
+#if defined(SKR_OS_UNIX)
+    bool skr::SharedLibrary::loadImpl(const char* path)
+    {
+        _lastError.clear();
+        _handle = dlopen(path, RTLD_LAZY | RTLD_LOCAL);
+        if (!_handle)
+        {
+            _lastError = dlerror();
+            return false;
+        }
+        return true;
+    }
+
+    bool skr::SharedLibrary::unloadImpl()
+    {
+        _lastError.clear();
+        // dlclose returns 0 on success
+        if (dlclose(_handle) != 0)
+        {
+            _lastError = dlerror();
+            return false;
+        }
+        _handle = nullptr;
+        return true;
+    }
+
+    void* skr::SharedLibrary::getImpl(const char* symbolName)
+    {
+        _lastError.clear();
+        dlerror();
+        void* symbol = dlsym(_handle, symbolName);
+        const char* error = dlerror();
+        if (error)
+        {
+            // An error occured
+            _lastError = error;
+            return nullptr;
+        }
+        return symbol;
+    }
+#elif defined(SKR_OS_WINDOWS) // Windows implementation
+    // Return a string explaining the last error
+    eastl::string skr::SharedLibrary::getWindowsError()
+    {
+        auto tchar_to_utf8 = +[](const TCHAR* str, char* str8)
+        {
+        #ifdef _UNICODE
+            auto size = WideCharToMultiByte(CP_UTF8, 0, str, (int)wcslen(str), NULL, 0, NULL, NULL);
+            WideCharToMultiByte(CP_UTF8, 0, str, (int)wcslen(str), str8, size, NULL, NULL);
+            str8[size] = '\0';
+        #else
+            return strcpy(str8, str);
+        #endif
+        };
+        DWORD lastError = GetLastError();
+        TCHAR buffer[256];
+        char8_t u8str[256];
+        if (lastError != 0)
+        {
+            FormatMessage(FORMAT_MESSAGE_FROM_SYSTEM,
+            nullptr,
+            lastError,
+            MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+            buffer,
+            256 - 1,
+            nullptr);
+            tchar_to_utf8(buffer, u8str);
+            return eastl::string(u8str);
+        }
+        return eastl::string();
+    }
+
+    bool skr::SharedLibrary::loadImpl(const char* path)
+    {
+        _lastError.clear();
+        if (path == nullptr)
+        {
+            _handle = GetModuleHandle(nullptr);
+        }
+        else
+        {
+            auto wpath = ghc::filesystem::path(path);
+            _handle = LoadLibrary(wpath.c_str());
+        }
+        if (!_handle)
+        {
+            _lastError = getWindowsError();
+            return false;
+        }
+        return true;
+    }
+
+    bool skr::SharedLibrary::unloadImpl()
+    {
+        _lastError.clear();
+        if (!FreeLibrary((HMODULE)_handle))
+        {
+            _lastError = getWindowsError();
+            return false;
+        }
+        _handle = nullptr;
+        return true;
+    }
+
+    void* skr::SharedLibrary::getImpl(const char* symbolName)
+    {
+        _lastError.clear();
+        void* addr = (void*)GetProcAddress((HMODULE)_handle, symbolName);
+        if (!addr)
+        {
+            _lastError = getWindowsError();
+            return nullptr;
+        }
+        return addr;
+    }
+#endif
