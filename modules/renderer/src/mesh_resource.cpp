4ac4e32269bb165ebe1b3aac84a34a75e76549fd
hikahuang <39457738+SaeruHikari@users.noreply.github.com>
2022-07-18 17:22:22 +0800

    pass ramIO status while loading gltf

--- modules/renderer/src/mesh_resource.cpp
+++ modules/renderer/src/mesh_resource.cpp
@@ -41,7 +41,6 @@ void skr_mesh_resource_create_from_gltf(skr_io_ram_service_t* ioService, const c
                 {
                     skr_mesh_resource_id resource = (skr_mesh_resource_id)sakura_calloc(1, sizeof(skr_mesh_resource_t));
                     // TODO: fill runtime structure
-                    
                 }
             }
         }
@@ -51,5 +50,17 @@ void skr_mesh_resource_create_from_gltf(skr_io_ram_service_t* ioService, const c
         skr_atomic32_store_relaxed(&cbData->gltfRequest->gltf_status, SKR_ASYNC_IO_STATUS_OK);
     };
     ramIO.callback_datas[SKR_ASYNC_IO_STATUS_OK] = (void*)&callbackData;
+    // pass status
+    ramIO.callbacks[SKR_ASYNC_IO_STATUS_ENQUEUED] = +[](skr_async_io_request_t* request, void* data) noexcept {
+        auto cbData = (CallbackData*)data;
+        skr_atomic32_store_relaxed(&cbData->gltfRequest->gltf_status, SKR_ASYNC_IO_STATUS_ENQUEUED);
+    };
+    ramIO.callback_datas[SKR_ASYNC_IO_STATUS_ENQUEUED] = (void*)&callbackData;
+    ramIO.callbacks[SKR_ASYNC_IO_STATUS_RAM_LOADING] = +[](skr_async_io_request_t* request, void* data) noexcept {
+        auto cbData = (CallbackData*)data;
+        skr_atomic32_store_relaxed(&cbData->gltfRequest->gltf_status, SKR_ASYNC_IO_STATUS_RAM_LOADING);
+    };
+    ramIO.callback_datas[SKR_ASYNC_IO_STATUS_RAM_LOADING] = (void*)&callbackData;
+    
     ioService->request(request->vfs_override, &ramIO, &request->ioRequest);
 }
