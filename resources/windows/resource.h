3fb226729824bd056b3f05bff81fb43b4a1d0ced
SaeruHikari <39457738+SaeruHikari@users.noreply.github.com>
2022-07-29 01:14:50 +0800

    Update resources

--- resources/windows/resource.h
+++ resources/windows/resource.h
@@ -6,11 +6,11 @@
 #define IDS_APP_TITLE			103
 
 #define IDR_MAINFRAME			128
-#define IDD_TESTW_DIALOG	102
+#define IDD_TESTW_DIALOG	    102
 #define IDD_ABOUTBOX			103
 #define IDM_ABOUT				104
 #define IDM_EXIT				105
-#define IDI_TESTW			107
+#define IDI_TESTW			    107
 #define IDI_SMALL				108
 #define IDC_TESTW			109
 #define IDC_MYICON				2
