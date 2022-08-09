fe92545a22d1cb533fe4433e23ee4edb378676e1
hikahuang <39457738+SaeruHikari@users.noreply.github.com>
2022-08-09 17:21:17 +0800

    添加测试资源，修复macos的构建问题

--- modules/live2d/xmake.lua
+++ modules/live2d/xmake.lua
@@ -7,7 +7,7 @@ target("SkrLive2D")
     })
     add_deps("SkrRenderer")
     add_includedirs("include", "CubismNativeCore/include", {public=true})
-    add_includedirs("src/Framework", {public=false })
+    add_includedirs("src/Framework", {public=false})
     add_files("src/**.cpp")
     if (is_os("windows")) then 
         add_linkdirs("CubismNativeCore/lib/windows/x86_64/142")
@@ -16,4 +16,8 @@ target("SkrLive2D")
         else
             add_links("Live2DCubismCore_MDd")
         end
+    end
+    if (is_os("macosx")) then 
+        add_linkdirs("CubismNativeCore/lib/macos/x86_64")
+        add_links("Live2DCubismCore")
     end
