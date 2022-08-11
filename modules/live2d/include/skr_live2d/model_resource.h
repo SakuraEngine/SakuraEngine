8d834f6e6330d12d3a1f6f02eb02459573fd4d67
hikahuang <39457738+SaeruHikari@users.noreply.github.com>
2022-08-11 19:14:57 +0800

    Live2D Model Async Load

---
+++ modules/live2d/include/skr_live2d/model_resource.h
@@ -0,0 +1,69 @@
+#pragma once
+#include "SkrLive2D/skr_live2d.configure.h"
+
+#ifdef __cplusplus
+namespace Live2D { namespace Cubism { namespace Framework {
+    class CubismModelSettingJson;
+    class csmUserModel;
+    class csmString;
+    class ACubismMotion;
+
+    class csmMotionMap;
+    class csmBreathVector;
+    class csmIdVector;
+}}}
+typedef class Live2D::Cubism::Framework::CubismModelSettingJson* CubismModelSettingJsonId;
+typedef class Live2D::Cubism::Framework::csmUserModel* csmUserModelId;
+typedef class Live2D::Cubism::Framework::csmMotionMap* csmMotionMapId;
+typedef class Live2D::Cubism::Framework::csmBreathVector* csmBreathVectorId;
+typedef class Live2D::Cubism::Framework::csmIdVector* csmIdVectorId;
+#else
+typedef struct CubismModelSettingJson* CubismModelSettingJsonId;
+typedef struct csmUserModel* csmUserModelId;
+typedef struct csmMotionMap* csmMotionMapId;
+typedef struct csmBreathVector* csmBreathVectorId;
+typedef struct csmIdVector* csmIdVectorId;
+#endif
+
+// Made up with: 
+// [Setting], [Model], [Expression], [Motions], Physics
+// Pose, EyeBlink, [Breath], UserData, [EyeBlinkIds], [LipSyncIds]
+struct skr_live2d_model_resource_t {
+    CubismModelSettingJsonId model_setting;
+    csmUserModelId model;
+    csmMotionMapId expression_map;
+    csmMotionMapId motion_map;
+    csmBreathVectorId breath_vector;
+    csmIdVectorId eye_blink_ids;
+    csmIdVectorId lip_sync_ids;
+};
+typedef struct skr_live2d_model_resource_t skr_live2d_model_resource_t;
+typedef struct skr_live2d_model_resource_t* skr_live2d_model_resource_id;
+
+#include "utils/io.h"
+
+typedef void (*skr_async_live2d_io_callback_t)(struct skr_live2d_ram_io_request_t* request, void* data);
+typedef struct skr_live2d_ram_io_request_t {
+    struct skr_vfs_t* vfs_override;
+    skr_async_io_request_t settingsRequest;
+    SAtomic32 liv2d_status;
+    skr_live2d_model_resource_id model_resource;
+#ifdef __cplusplus
+    bool is_ready() const SKR_NOEXCEPT
+    {
+        return get_status() == SKR_ASYNC_IO_STATUS_OK;
+    }
+    SkrAsyncIOStatus get_status() const SKR_NOEXCEPT
+    {
+        return (SkrAsyncIOStatus)skr_atomic32_load_acquire(&liv2d_status);
+    }
+#endif
+} skr_live2d_ram_io_request_t;
+
+#ifndef SKR_SERIALIZE_GURAD
+SKR_LIVE2D_EXTERN_C SKR_LIVE2D_API void 
+skr_live2d_model_create_from_json(skr_io_ram_service_t* ioService, const char* path, skr_live2d_ram_io_request_t* request);
+#endif
+
+SKR_LIVE2D_EXTERN_C SKR_LIVE2D_API void 
+skr_live2d_model_free(skr_live2d_model_resource_id live2d_resource);
--- samples/application/live2d-viewer/src/viewer_module.cpp
+++ samples/application/live2d-viewer/src/viewer_module.cpp
@@ -3,11 +3,21 @@
 #include "imgui/skr_imgui_rg.h"
 #include "imgui/imgui.h"
 
+#include "ghc/filesystem.hpp"
+#include "platform/vfs.h"
+#include "utils/io.hpp"
+
+#include "utils/make_zeroed.hpp"
+#include "skr_live2d/model_resource.h"
+
 class SLive2DViewerModule : public skr::IDynamicModule
 {
     virtual void on_load(int argc, char** argv) override;
     virtual int main_module_exec(int argc, char** argv) override;
     virtual void on_unload() override;
+
+    skr_vfs_t* resource_vfs = nullptr;
+    skr::io::RAMService* ram_service = nullptr;
 };
 
 IMPLEMENT_DYNAMIC_MODULE(SLive2DViewerModule, Live2DViewer);
@@ -33,16 +43,43 @@ Live2DViewer)
 void SLive2DViewerModule::on_load(int argc, char** argv)
 {
     SKR_LOG_INFO("live2d viewer loaded!");
+
+    auto resourceRoot = (ghc::filesystem::current_path() / "../resources").u8string();
+    skr_vfs_desc_t vfs_desc = {};
+    vfs_desc.mount_type = SKR_MOUNT_TYPE_CONTENT;
+    vfs_desc.override_mount_dir = resourceRoot.c_str();
+    resource_vfs = skr_create_vfs(&vfs_desc);
+
+    auto ioServiceDesc = make_zeroed<skr_ram_io_service_desc_t>();
+    ioServiceDesc.name = "Live2DViewerRAMIOService";
+    ioServiceDesc.sleep_mode = SKR_IO_SERVICE_SLEEP_MODE_SLEEP;
+    ioServiceDesc.sleep_time = 1000 / 60;
+    ioServiceDesc.lockless = true;
+    ioServiceDesc.sort_method = SKR_IO_SERVICE_SORT_METHOD_PARTIAL;
+    ram_service = skr::io::RAMService::create(&ioServiceDesc);
 }
 
 void SLive2DViewerModule::on_unload()
 {
     SKR_LOG_INFO("live2d viewer unloaded!");
+
+    skr::io::RAMService::destroy(ram_service);
+    skr_free_vfs(resource_vfs);
 }
 
 int SLive2DViewerModule::main_module_exec(int argc, char** argv)
 {
     SKR_LOG_INFO("live2d viewer executed!");
 
+    auto request = make_zeroed<skr_live2d_ram_io_request_t>();
+    skr_io_ram_service_t* ioService = ram_service;
+    request.vfs_override = resource_vfs;
+    skr_live2d_model_create_from_json(ioService, "Live2DViewer/Haru/Haru.model3.json", &request);
+
+    while(!request.is_ready())
+    {
+
+    }
+    skr_live2d_model_free(request.model_resource);
     return 0;
 }
