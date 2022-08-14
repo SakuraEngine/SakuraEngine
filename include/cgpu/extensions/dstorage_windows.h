507c91821a76d4d66fe2916330d900ef11f5e848
SaeruHikari <39457738+SaeruHikari@users.noreply.github.com>
2022-08-15 01:44:44 +0800

    初步完成 WindowsDStorageService & 改良 VRAMService 的 DStorage 性能

--- include/cgpu/extensions/dstorage_windows.h
+++ include/cgpu/extensions/dstorage_windows.h
@@ -6,8 +6,35 @@ typedef struct skr_win_dstorage_decompress_service_t* skr_win_dstorage_decompres
 
 #define SKR_WIN_DSTORAGE_COMPRESSION_TYPE_ADATIVE CGPU_DSTORAGE_COMPRESSION_CUSTOM
 
+typedef enum EWinDStorageDecompressionFlag {
+    WIN_DSTORAGE_DECOMPRESSION_FLAG_NONE = 0x00,
+    WIN_DSTORAGE_DECOMPRESSION_FLAG_DEST_IN_UPLOAD_HEAP = 0x01,
+
+    WIN_DSTORAGE_DECOMPRESSION_FLAG_MAX_ENUM_BIT = 0xFFFFFFFF
+} EWinDStorageDecompressionFlag;
+typedef uint32_t WinDStorageDecompressionFlags;
+
+typedef struct skr_win_dstorage_decompress_request_t {
+    uint64_t id;
+    CGPUDStorageCompression compression;
+    WinDStorageDecompressionFlags flags;
+    uint64_t src_size;
+    void const* src_buffer;
+    uint64_t dst_size;
+    void* dst_buffer;
+} skr_win_dstorage_decompress_request_t;
+
+#ifndef _HRESULT_DEFINED
+#define _HRESULT_DEFINED
+typedef long HRESULT;
+#endif
+typedef HRESULT (*skr_win_dstorage_decompress_callback_t)(skr_win_dstorage_decompress_request_t* request, void* user_data);
+
 RUNTIME_EXTERN_C RUNTIME_API
-skr_win_dstorage_decompress_service_id cgpu_win_create_decompress_service(CGPUDStorageQueueId dsqueue);
+skr_win_dstorage_decompress_service_id cgpu_win_create_decompress_service();
+
+RUNTIME_EXTERN_C RUNTIME_API
+bool cgpu_win_decompress_service_register_callback(skr_win_dstorage_decompress_service_id service, CGPUDStorageCompression, skr_win_dstorage_decompress_callback_t, void*);
 
 RUNTIME_EXTERN_C RUNTIME_API
 void cgpu_win_free_decompress_service(skr_win_dstorage_decompress_service_id service);
--- src/cgpu/d3d12/cgpu_d3d12_dstorage.cpp
+++ src/cgpu/d3d12/cgpu_d3d12_dstorage.cpp
@@ -9,6 +9,8 @@
 #include "dstorage.h"
 #include "EASTL/vector_map.h"
 
+#include "tracy/Tracy.hpp"
+
 struct CGPUDStorageQueueD3D12 : public CGPUDStorageQueue {
     IDStorageQueue* pQueue;
     IDStorageFactory* pFactory;
@@ -153,51 +155,79 @@ void cgpu_free_dstorage_queue_d3d12(CGPUDStorageQueueId queue)
     SkrDelete(Q);
 }
 
-
+#include <EASTL/vector_map.h>
 #include "utils/log.h"
+#include "utils/make_zeroed.hpp"
 #include "platform/memory.h"
 #include "platform/thread.h"
 #include "cgpu/extensions/dstorage_windows.h"
 #include "cgpu/io.hpp"
 
-static void CALLBACK __decompressThreadTask_DirectStorage(
+static void CALLBACK __decompressThreadPoolTask_DirectStorage(
     TP_CALLBACK_INSTANCE*,
     void*,
     TP_WAIT* wait,
     TP_WAIT_RESULT);
 
+static void CALLBACK __decompressThreadTask_DirectStorage(void*);
+
 struct skr_win_dstorage_decompress_service_t
 {
+    const bool use_thread_pool = false;
     skr_win_dstorage_decompress_service_t(IDStorageCustomDecompressionQueue* queue)
         : decompress_queue(queue)
     {
+        skr_atomic32_store_release(&thread_running, 1);
         event_handle = queue->GetEvent();
-        thread_pool_wait = CreateThreadpoolWait(__decompressThreadTask_DirectStorage, this, nullptr);
-        SetThreadpoolWait(thread_pool_wait, event_handle, nullptr);
+        if (use_thread_pool)
+        {
+            thread_pool_wait = CreateThreadpoolWait(__decompressThreadPoolTask_DirectStorage, this, nullptr);
+            SetThreadpoolWait(thread_pool_wait, event_handle, nullptr);
+        }
+        else 
+        {
+            thread_desc.pData = this;
+            thread_desc.pFunc = &__decompressThreadTask_DirectStorage;
+            skr_init_thread(&thread_desc, &thread_handle);
+        }
     }
-    ~skr_win_dstorage_decompress_service_t() SKR_NOEXCEPT
+    ~skr_win_dstorage_decompress_service_t  () SKR_NOEXCEPT
     {
-        CloseThreadpoolWait(thread_pool_wait);
-        decompress_queue->Release();
+        skr_atomic32_store_release(&thread_running, 0);
+        SetEvent(event_handle);
+        if (use_thread_pool)
+        {
+            CloseThreadpoolWait(thread_pool_wait);
+        }
+        else
+        {
+            skr_join_thread(thread_handle);
+            skr_destroy_thread(thread_handle);
+        }
         CloseHandle(event_handle);
+        decompress_queue->Release();
     }
-    // thread items
+
+    // queue items
+    struct DecompressionResolver {
+        skr_win_dstorage_decompress_callback_t callback = nullptr;
+        void* user_data = nullptr;
+    };
     IDStorageCustomDecompressionQueue* decompress_queue = nullptr;
     HANDLE event_handle = nullptr;
+    eastl::vector_map<CGPUDStorageCompression, DecompressionResolver> resolvers;
+    // threadpool items
     TP_WAIT* thread_pool_wait = nullptr;
+    // thread items
+    SThreadDesc thread_desc;
+    SThreadHandle thread_handle = nullptr;
+    SAtomic32 thread_running;
 };
 
-static void CALLBACK __decompressThreadTask_DirectStorage(
-    TP_CALLBACK_INSTANCE*,
-    void* data,
-    TP_WAIT* wait,
-    TP_WAIT_RESULT)
+static void __decompressTask_DirectStorage(skr_win_dstorage_decompress_service_id service)
 {
-    auto service = (skr_win_dstorage_decompress_service_id)data;
-    auto oldPriority = GetThreadPriority(GetCurrentThread());
-    SetThreadPriority(GetCurrentThread(), THREAD_PRIORITY_HIGHEST);
-
-    while (true)
+    auto running = skr_atomic32_load_acquire(&service->thread_running);
+    while (running)
     {
         DSTORAGE_CUSTOM_DECOMPRESSION_REQUEST requests[64];
         uint32_t numRequests = 0;
@@ -208,11 +238,48 @@ static void CALLBACK __decompressThreadTask_DirectStorage(
         if (numRequests == 0)
             break;
 
-        for (uint32_t i = 0; i < numRequests; ++i)
         {
-            // ScheduleDecompression(requests[i]);
+            ZoneScopedN("DirectStorageDecompress");
+            for (uint32_t i = 0; i < numRequests; ++i)
+            {
+                auto resolver = service->resolvers.find(requests[i].CompressionFormat);
+                if (resolver != service->resolvers.end())
+                {
+                    auto skrRequest = make_zeroed<skr_win_dstorage_decompress_request_t>();
+                    skrRequest.id = requests[i].Id;
+                    skrRequest.compression = requests[i].CompressionFormat;
+                    skrRequest.flags = requests[i].Flags;
+                    skrRequest.src_size = requests[i].SrcSize;
+                    skrRequest.src_buffer = requests[i].SrcBuffer;
+                    skrRequest.dst_size = requests[i].DstSize;
+                    skrRequest.dst_buffer = requests[i].DstBuffer;
+                    resolver->second.callback(&skrRequest, resolver->second.user_data);
+                }
+                else
+                {
+                    SKR_LOG_WARN("Unable to find decompression resolver for format %d\n", requests[i].CompressionFormat);
+                    DSTORAGE_CUSTOM_DECOMPRESSION_RESULT failResult = {};
+                    failResult.Result = S_FALSE;
+                    failResult.Id = requests[i].Id;
+                    service->decompress_queue->SetRequestResults(1, &failResult);
+                }
+            }
         }
     }
+}
+
+static void CALLBACK __decompressThreadPoolTask_DirectStorage(
+    TP_CALLBACK_INSTANCE*,
+    void* data,
+    TP_WAIT* wait,
+    TP_WAIT_RESULT)
+{
+    ZoneScopedN("DirectStorageDecompress");
+    auto service = (skr_win_dstorage_decompress_service_id)data;
+    auto oldPriority = GetThreadPriority(GetCurrentThread());
+    SetThreadPriority(GetCurrentThread(), THREAD_PRIORITY_HIGHEST);
+
+    __decompressTask_DirectStorage(service);
     
     // Restore the original thread's priority back to its original setting.
     SetThreadPriority(GetCurrentThread(), oldPriority);
@@ -221,10 +288,28 @@ static void CALLBACK __decompressThreadTask_DirectStorage(
     SetThreadpoolWait(wait, service->event_handle, nullptr);
 }
 
-skr_win_dstorage_decompress_service_id cgpu_win_create_decompress_service(CGPUDStorageQueueId dsqueue)
+static void CALLBACK __decompressThreadTask_DirectStorage(void* data)
 {
-    CGPUDStorageQueueD3D12* Q = (CGPUDStorageQueueD3D12*)dsqueue;
-    IDStorageFactory* pFactory = Q->pFactory;
+    auto service = (skr_win_dstorage_decompress_service_id)data;
+
+    while (skr_atomic32_load_acquire(&service->thread_running))
+    {
+        {
+            ZoneScopedNC("DirectStorageDecompressWait", tracy::Color::Gray43);
+            WaitForSingleObject(service->event_handle, INFINITE);
+        }
+        __decompressTask_DirectStorage(service);
+    } 
+}
+
+skr_win_dstorage_decompress_service_id cgpu_win_create_decompress_service()
+{
+    IDStorageFactory* pFactory = nullptr;
+    if (!SUCCEEDED(DStorageGetFactory(IID_PPV_ARGS(&pFactory))))
+    {
+        SKR_LOG_ERROR("Failed to get DStorage factory!");
+        return nullptr;
+    }
     IDStorageCustomDecompressionQueue* pCompressionQueue = nullptr;
     if (!SUCCEEDED(pFactory->QueryInterface(IID_PPV_ARGS(&pCompressionQueue))))
     {
@@ -232,7 +317,19 @@ skr_win_dstorage_decompress_service_id cgpu_win_create_decompress_service(CGPUDS
     }
     auto service = SkrNew<skr_win_dstorage_decompress_service_t>(pCompressionQueue);
     SKR_LOG_DEBUG("Created decompress service");
+    pFactory->Release();
     return service;
+}
+
+bool cgpu_win_decompress_service_register_callback(skr_win_dstorage_decompress_service_id service, 
+    CGPUDStorageCompression compression, skr_win_dstorage_decompress_callback_t callback, void* user_data)
+{
+    const auto registered = (service->resolvers.find(compression) != service->resolvers.end());
+    SKR_ASSERT(!registered && "Callback already registered for this compression");
+    if (registered) return false;
+    SKR_ASSERT(callback && "Callback must be valid");
+    service->resolvers[compression] = { callback, user_data };
+    return true;
 }
 
 void cgpu_win_free_decompress_service(skr_win_dstorage_decompress_service_id service)
