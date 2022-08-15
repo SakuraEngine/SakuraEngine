fd8161be214bba7701cb2008b676bcb269875f18
gaymenhub <672619410@qq.com>
2022-08-15 17:53:02 +0800

    type builder 加上去重

--- src/ecs/type_builder.cpp
+++ src/ecs/type_builder.cpp
@@ -13,6 +13,7 @@ type_builder_t& type_builder_t::with(const dual_type_index_t* types, uint32_t in
 dual_type_set_t type_builder_t::build()
 {
     std::sort(indices.begin(), indices.end());
-    return { indices.data(), (SIndex)indices.size() };
+    auto end = std::unique(indices.begin(), indices.end());
+    return { indices.data(), (SIndex)(end - indices.begin()) };
 }
 } // namespace dual
