42032af8ae62b001363a93e690cde9eb56791a86
SaeruHikari <39457738+SaeruHikari@users.noreply.github.com>
2022-08-18 06:23:55 +0800

    引入 live2d shaders

---
+++ modules/live2d/shaders/live2d_masked_vertex_shader.vs_6_0.hlsl
@@ -0,0 +1,23 @@
+#include "live2d_cb.hlsli"
+
+struct VSIn
+{
+    float2 pos : POSITION;
+    float2 uv  : TEXCOORD0;
+};
+
+struct VSOut
+{
+    float4 pos : SV_POSITION;
+    float2 uv  : TEXCOORD0;
+    float4 clip_pos  : TEXCOORD1;
+};
+
+VSOut main(VSIn input)
+{
+    VSOut output = (VSOut)0;
+    output.pos = mul(float4(input.pos, 0.f, 1.f), push_constants.projection_matrix);
+    output.clip_pos = mul(float4(input.pos, 0.f, 1.f), push_constants.clip_matrix);
+    output.uv = input.uv;
+    return output;
+}
