1e775efe377defdf9eb85966a8cf484dfbcc58dc
SaeruHikari <39457738+SaeruHikari@users.noreply.github.com>
2022-08-22 22:07:50 +0800

    修复构建错误

--- modules/renderer/include/skr_renderer/render_texture.h
+++ modules/renderer/include/skr_renderer/render_texture.h
@@ -7,11 +7,11 @@ typedef struct skr_render_texture_request_t {
     CGPUQueueId queue_override;
     CGPUDStorageQueueId dstorage_queue_override;
     skr_async_io_request_t ram_request;
-    skr_async_io_request_t vram_request;
-    skr_vram_texture_request_t vram_texture_request;
+    skr_async_io_request_t vtexture_request;
+    skr_async_vtexture_destination_t vtexture_destination;
 } skr_render_texture_request_t;
 
 #ifndef SKR_SERIALIZE_GURAD
 SKR_RENDERER_EXTERN_C SKR_RENDERER_API void 
-skr_render_texture_create_from_png(skr_io_ram_service_t*, skr_io_vram_service_t*, const char* path, skr_render_texture_request_t* request);
+skr_render_texture_create_from_png(skr_io_ram_service_t*, skr_io_vram_service_t*, const char* path, skr_async_vtexture_destination_t* request);
 #endif
