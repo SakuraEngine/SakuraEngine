e91911905bf6fd5ee0f107478a591ba9b1a60628
hikahuang <39457738+SaeruHikari@users.noreply.github.com>
2022-09-09 17:18:02 +0800

    完善 nsight 扩展

--- include/cgpu/extensions/cgpu_nsight.h
+++ include/cgpu/extensions/cgpu_nsight.h
@@ -4,6 +4,18 @@
 typedef struct CGPUNSightTracker CGPUNSightTracker;
 typedef struct CGPUNSightTracker* CGPUNSightTrackerId;
 
+typedef void (*CGPUNSightProcAddGpuCrashDumpDescription)(uint32_t key, const char* value);
+typedef void (*CGPUNSightTrackerCrashDumpCallback)(const void* pGpuCrashDump, const uint32_t gpuCrashDumpSize, void* pUserData);
+typedef void (*CGPUNSightTrackerShaderDebugInfoCallback)(const void* pShaderDebugInfo, const uint32_t shaderDebugInfoSize, void* pUserData);
+typedef void (*CGPUNSightTrackerCrashDumpDescriptionCallback)(CGPUNSightProcAddGpuCrashDumpDescription addDescription, void* pUserData);
+
+typedef struct CGPUNSightTrackerDescriptor {
+    CGPUNSightTrackerCrashDumpCallback crash_dump_callback = nullptr;
+    CGPUNSightTrackerShaderDebugInfoCallback shader_debug_info_callback = nullptr;
+    CGPUNSightTrackerCrashDumpDescriptionCallback crash_dump_description_callback = nullptr;
+    void* user_data = nullptr;
+} CGPUNSightTrackerDescriptor;
+
 struct CGPUNSightTracker
 {
     CGPUInstanceId instance;
@@ -11,10 +23,6 @@ struct CGPUNSightTracker
     virtual ~CGPUNSightTracker() = default;
 #endif
 };
-
-typedef struct CGPUNSightTrackerDescriptor {
-    bool enable_shader_info;
-} CGPUNSightTrackerDescriptor;
 
 RUNTIME_EXTERN_C RUNTIME_API
 CGPUNSightTrackerId cgpu_create_nsight_tracker(CGPUInstanceId instance, const CGPUNSightTrackerDescriptor* descriptor);
