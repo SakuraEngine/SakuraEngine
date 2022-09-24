8467ebb042babb848115b2e405fe6d6379169329
SaeruHikari <39457738+SaeruHikari@users.noreply.github.com>
2022-09-24 16:38:22 +0800

    跨进程视口

--- editors/ed-core/include/editor_core/sqgl_shared_resource.h
+++ editors/ed-core/include/editor_core/sqgl_shared_resource.h
@@ -11,12 +11,15 @@ struct SKR_EDITOR_CORE_API SQGLFunctions
     static void Initialize(QOpenGLContext*) noexcept;
 
     static PFNGLCREATEMEMORYOBJECTSEXTPROC pGLCreateMemoryObjectsEXT;
+    static PFNGLDELETEMEMORYOBJECTSEXTPROC pGLDeleteMemoryObjectsEXT;
 #ifdef _WIN32
     static PFNGLIMPORTMEMORYWIN32NAMEEXTPROC pGLImportMemoryWin32NameEXT;
 #else
     static PFNGLIMPORTMEMORYFDEXTPROC pGLImportMemoryFdEXT;
 #endif
     static PFNGLCREATETEXTURESPROC pGLCreateTextures;
+    static PFNGLDELETETEXTURESEXTPROC pGLDeleteTextures;
+    
     static PFNGLTEXTURESTORAGEMEM2DEXTPROC pGLTextureStorageMem2DEXT;
 };
 }
