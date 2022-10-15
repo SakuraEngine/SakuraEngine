965a63b86d703ace8ff94f8fea2225f62235d1de
gaymenhub <672619410@qq.com>
2022-10-16 02:24:23 +0800

    marl 后端

--- modules/runtime/src/ecs/storage.cpp
+++ modules/runtime/src/ecs/storage.cpp
@@ -24,7 +24,7 @@ dual_storage_t::dual_storage_t()
 dual_storage_t::~dual_storage_t()
 {
     if(scheduler)
-        scheduler->sync_storage(this);
+        scheduler->remove_storage(this);
     scheduler = nullptr;
     for(auto q : queries)
         ::sakura_free((void*)q);
