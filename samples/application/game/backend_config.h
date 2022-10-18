466fc24b21dbe509e55eca6034a9fce911c126b5
BenzzZX <672619410@qq.com>
2022-10-18 10:25:45 +0800

    replace config resource generator with static_ctor generator

--- samples/application/game/backend_config.h
+++ samples/application/game/backend_config.h
@@ -1,10 +1,13 @@
 #pragma once
 #include "platform/configure.h"
 #include "utils/types.h"
+#include "resource/config_resource.h"
 #if !defined(__meta__) && defined(__cplusplus)
     #include "GameRT/backend_config.rtti.generated.hpp"
     #include "GameRT/json_reader.generated.h"
+    #include "GameRT/serialize.generated.h"
 #endif
+
 
 enum sreflect sattr(
 "guid" : "b4b7f387-d8c2-465c-9b3a-6d83a3d198b1",
@@ -21,11 +24,11 @@ typedef enum ECGPUBackEnd ECGPUBackEnd;
 struct sreflect sattr(
 "guid" : "b537f7b1-6d2d-44f6-b313-bcb559d3f490",
 "serialize" : ["json", "bin"],
-"config" : true,
 "rtti" : true
 )
 config_backend_t
 {
     enum ECGPUBackEnd backend;
-};
+}
+sregister_config(0);
 typedef struct config_backend_t config_backend_t;
