e03448ec4b12330f31c0f3df972aaf958229d92f
SaeruHikari <39457738+SaeruHikari@users.noreply.github.com>
2022-10-28 22:28:09 +0800

    移除 wasm 的 thirdparty xmake 脚本并将 m3 的依赖移动到 wasm 模块中

--- modules/wasm/xmake.lua
+++ modules/wasm/xmake.lua
@@ -1,8 +1,11 @@
 if (os.host() == "windows") then
+    wasm3_includes_dir = "$(projectdir)/thirdparty/wasm3"
     shared_module("SkrWASM", "SKR_WASM", engine_version)
         set_group("01.modules")
         public_dependency("SkrRT", engine_version)
         add_includedirs("include", {public=true})
         add_files("src/build.*.c", "src/build.*.cpp")
         add_rules("utils.install-libs", { libnames = { "m3" } })
+        add_links("m3", "uv_a", "uvwasi_a", {public = true})
+        add_includedirs(wasm3_includes_dir, {public = true})
 end
