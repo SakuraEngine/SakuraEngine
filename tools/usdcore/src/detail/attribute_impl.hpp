c96d26dbc1967a6720eb57783a57382e75a84f7f
SaeruHikari <39457738+SaeruHikari@users.noreply.github.com>
2022-10-28 22:46:34 +0800

    一批 USD Wrapper

---
+++ tools/usdcore/src/detail/attribute_impl.hpp
@@ -0,0 +1,41 @@
+#pragma once
+#include "helpers.hpp"
+#include "UsdCore/attribute.hpp"
+#include "pxr/usd/usd/attribute.h"
+
+namespace skd
+{
+struct SUSDAttributeImpl : public USDWrapperWithRC<SUSDAttribute>
+{
+    SUSDAttributeImpl(pxr::UsdAttribute attribute);
+    ~SUSDAttributeImpl();
+
+    virtual bool GetMetadata(const char* key, SUSDVtValueId outValue) const SKR_NOEXCEPT override;
+    virtual bool HasMetadata(const char* key) const SKR_NOEXCEPT override;
+    virtual bool SetMetadata(const char* key, const SUSDVtValueId& value) SKR_NOEXCEPT override;
+    virtual bool ClearMetadata(const char* key) SKR_NOEXCEPT override;
+
+    virtual eastl::string GetName() const SKR_NOEXCEPT override;
+    virtual eastl::string GetBaseName() const SKR_NOEXCEPT override;
+    virtual eastl::string GetTypeName() const SKR_NOEXCEPT override;
+
+    virtual bool GetTimeSamples(eastl::vector<double>& times) const SKR_NOEXCEPT override;
+
+    virtual bool HasValue() const SKR_NOEXCEPT override;
+    virtual bool HasFallbackValue() const SKR_NOEXCEPT override;
+
+    virtual bool ValueMightBeTimeVarying() const SKR_NOEXCEPT override;
+    
+    virtual bool Get(SUSDVtValueId outValue, eastl::optional<double> time = {}) const SKR_NOEXCEPT override;
+    virtual bool Set(const SUSDVtValueId& value, eastl::optional<double> time = {}) SKR_NOEXCEPT override;
+
+    virtual bool Clear() SKR_NOEXCEPT override;
+    virtual bool ClearAtTime(double time) SKR_NOEXCEPT override;
+    
+    virtual skr::SObjectPtr<SUSDSdfPath> GetPath() const SKR_NOEXCEPT override;
+    virtual skr::SObjectPtr<SUSDPrim> GetPrim() const SKR_NOEXCEPT override;
+
+protected:
+    pxr::UsdAttribute attribute;
+};
+}
