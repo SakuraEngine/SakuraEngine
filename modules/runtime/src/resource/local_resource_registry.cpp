3ae51eddc1f38bd23ed21d35eea3a09f3b67108f
DESKTOP-EQGMHA5\Benzz <BenzzZX@outlook.com>
2022-11-03 14:03:39 +0800

    修正反序列化

--- modules/runtime/src/resource/local_resource_registry.cpp
+++ modules/runtime/src/resource/local_resource_registry.cpp
@@ -13,6 +13,7 @@ SLocalResourceRegistry::SLocalResourceRegistry(skr_vfs_t* vfs)
 }
 bool SLocalResourceRegistry::RequestResourceFile(SResourceRequest* request)
 {
+    namespace bin = skr::binary;
     //简单实现，直接在 resource 路径下按 guid 找到文件读信息，没有单独的数据库
     auto guid = request->resourceRecord->header.guid;
     skr::filesystem::path path = fmt::format("{}.bin", guid);
@@ -22,9 +23,40 @@ bool SLocalResourceRegistry::RequestResourceFile(SResourceRequest* request)
     char buffer[sizeof(skr_resource_header_t)];
     skr_vfs_fread(file, buffer, 0, sizeof(skr_resource_header_t));
     SKR_DEFER({ skr_vfs_fclose(file); });
+    struct SpanReader
+    {
+        gsl::span<char> data;
+        size_t offset = 0;
+        int read(void* dst, size_t size)
+        {
+            if (offset + size > data.size())
+                return -1;
+            memcpy(dst, data.data() + offset, size);
+            offset += size;
+            return 0;
+        }
+    } reader = {buffer};
+    skr_binary_reader_t archive{reader};
+    skr_resource_header_t header;
+    uint32_t function = 1;
+    int ret = bin::Archive(&archive, function);
+    if (ret != 0)
+        return false;
+    //NOTE: this code is derived from resource header serialization
+    SKR_ASSERT(function == 1);
+    ret = bin::Archive(&archive, header.version);
+    if (ret != 0)
+        return false;
+    ret = bin::Archive(&archive, header.guid);
+    if (ret != 0)
+        return false;
+    SKR_ASSERT(header.guid == guid);
+    ret = bin::Archive(&archive, header.type);
+    if (ret != 0)
+        return false;
     // TODO: 把这些封装起来传过去少暴露一些细节？
-    request->resourceRecord->header.type = ((skr_resource_header_t*)buffer)->type;
-    request->resourceRecord->header.version = ((skr_resource_header_t*)buffer)->version;
+    request->resourceRecord->header.type = header.type;
+    request->resourceRecord->header.version = header.version;
     request->path = path;
     request->vfs = vfs;
     request->OnRequestFileFinished();
