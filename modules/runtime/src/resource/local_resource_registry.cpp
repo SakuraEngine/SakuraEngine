cd385c9e333915721c0bb07c22d71f42fcb603f1
SaeruHikari <39457738+SaeruHikari@users.noreply.github.com>
2022-11-03 17:35:21 +0800

    修复 local resource registry 的路径解析

--- modules/runtime/src/resource/local_resource_registry.cpp
+++ modules/runtime/src/resource/local_resource_registry.cpp
@@ -17,10 +17,10 @@ bool SLocalResourceRegistry::RequestResourceFile(SResourceRequest* request)
 {
     //简单实现，直接在 resource 路径下按 guid 找到文件读信息，没有单独的数据库
     auto guid = request->GetGuid();
-    skr::filesystem::path path = fmt::format("game/{}.rh", guid);
-    auto u8Path = path.u8string();
+    skr::filesystem::path headerPath = fmt::format("game/{}.rh", guid);
+    auto headerUri = headerPath.u8string();
     // TODO: 检查文件存在？
-    auto file = skr_vfs_fopen(vfs, u8Path.c_str(), SKR_FM_READ, SKR_FILE_CREATION_OPEN_EXISTING);
+    auto file = skr_vfs_fopen(vfs, headerUri.c_str(), SKR_FM_READ, SKR_FILE_CREATION_OPEN_EXISTING);
     if (!file) return false;
     char buffer[sizeof(skr_resource_header_t)];
     skr_vfs_fread(file, buffer, 0, sizeof(skr_resource_header_t));
@@ -42,7 +42,11 @@ bool SLocalResourceRegistry::RequestResourceFile(SResourceRequest* request)
     skr_resource_header_t header;
     if(header.ReadWithoutDeps(&archive) != 0)
         return false;
-    FillRequest(request, header, vfs, u8Path.c_str());
+
+    auto resourcePath = headerPath;
+    resourcePath.replace_extension(".bin");
+    auto resourceUri = resourcePath.u8string();
+    FillRequest(request, header, vfs, resourceUri.c_str());
     request->OnRequestFileFinished();
     return true;
 }
