580d4ef9ad63d44105b5d4e06a9db46df710e47b
SaeruHikari <39457738+SaeruHikari@users.noreply.github.com>
2022-11-04 00:15:15 +0800

    实现 direct storage resource install

--- modules/renderer/src/resources/texture_resource.cpp
+++ modules/renderer/src/resources/texture_resource.cpp
@@ -1,14 +1,24 @@
 #include "skr_renderer/resources/texture_resource.h"
+#include "cgpu/api.h"
+#include "cgpu/flags.h"
+#include "containers/detail/sptr.hpp"
+#include "platform/configure.h"
+#include "resource/resource_factory.h"
 #include "resource/resource_system.h"
+#include "type/type_id.hpp"
+#include "utils/format.hpp"
+#include "skr_renderer/render_device.h"
+#include "utils/io.h"
+#include "utils/log.h"
 #include "utils/make_zeroed.hpp"
-#include "platform/debug.h"
 #include "cgpu/io.hpp"
+#include "platform/debug.h"
 
 #ifdef _WIN32
-#include "cgpu/extensions/dstorage_windows.h"
+//#include "cgpu/extensions/dstorage_windows.h"
 #endif
 
-#include "tracy/Tracy.hpp"
+//#include "tracy/Tracy.hpp"
 
 /*
 void skr_render_texture_create_from_png(skr_render_texture_io_t* io, const char* texture_path, 
@@ -79,10 +89,21 @@ struct SKR_RENDERER_API STextureFactoryImpl : public STextureFactory
     ESkrInstallStatus InstallWithDStorage(skr_resource_record_t* record);
     ESkrInstallStatus InstallWithQueue(skr_resource_record_t* record);
 
+    struct DStorageRequest
+    {
+        ~DStorageRequest()
+        {
+            SKR_LOG_TRACE("DStorage for resource %s finished!", absPath.c_str());
+        }
+        std::string absPath;
+        skr_async_io_request_t vtexture_request;
+        skr_async_vtexture_destination_t texture_destination;
+    };
+
     Root root;
     skr::flat_hash_map<skr_texture_resource_id, skr_async_io_request_t> mRamRequests;
     skr::flat_hash_map<skr_texture_resource_id, skr_async_io_request_t> mVRamRequests;
-    skr::flat_hash_map<skr_texture_resource_id, skr_async_io_request_t> mDStorageRequests;
+    skr::flat_hash_map<skr_texture_resource_id, SPtr<DStorageRequest>> mDStorageRequests;
 };
 
 STextureFactory* STextureFactory::Create(const Root& root)
@@ -97,12 +118,13 @@ void STextureFactory::Destroy(STextureFactory* factory)
 
 skr_type_id_t STextureFactoryImpl::GetResourceType()
 {
-     return {};
+    const auto resource_type = skr::type::type_id<skr_texture_resource_t>::get();
+    return resource_type;
 }
 
 bool STextureFactoryImpl::AsyncIO()
 {
-     return true; 
+    return true; 
 }
 
 ESkrLoadStatus STextureFactoryImpl::Load(skr_resource_record_t* record)
@@ -139,19 +161,73 @@ ESkrLoadStatus STextureFactoryImpl::UpdateLoad(skr_resource_record_t* record)
 
 bool STextureFactoryImpl::Unload(skr_resource_record_t* record)
 { 
-    SkrDelete((skr_texture_resource_t*)record->resource);
+    auto texture_resource = (skr_texture_resource_t*)record->resource;
+    if (texture_resource->texture) cgpu_free_texture(texture_resource->texture);
+    SkrDelete(texture_resource);
     return true; 
 }
 
 ESkrInstallStatus STextureFactoryImpl::Install(skr_resource_record_t* record)
 {
-    auto guid = record->activeRequest->GetGuid();
-
-    return ESkrInstallStatus::SKR_INSTALL_STATUS_INPROGRESS;
+    if (auto render_device = root.render_device)
+    {
+        // direct storage
+        if (auto file_dstorage_queue = render_device->get_file_dstorage_queue())
+        {
+            return InstallWithDStorage(record);
+        }
+        else
+        {
+            SKR_UNIMPLEMENTED_FUNCTION();
+        }
+    }
+    else
+    {
+        SKR_UNIMPLEMENTED_FUNCTION();
+    }
+    return ESkrInstallStatus::SKR_INSTALL_STATUS_FAILED;
 }
 
 ESkrInstallStatus STextureFactoryImpl::InstallWithDStorage(skr_resource_record_t* record)
 {
+    auto texture_resource = (skr_texture_resource_t*)record->resource;
+    auto guid = record->activeRequest->GetGuid();
+    if (auto render_device = root.render_device)
+    {
+        // direct storage
+        if (auto file_dstorage_queue = render_device->get_file_dstorage_queue())
+        {
+            auto compressedBin = skr::format("{}.bc1", guid);
+            auto compressedPath = root.dstorage_root / compressedBin.c_str();
+            auto dRequest = SPtr<DStorageRequest>::Create();
+            auto found = mDStorageRequests.find(texture_resource);
+            SKR_ASSERT(found == mDStorageRequests.end());
+            mDStorageRequests.emplace(texture_resource, dRequest);
+            dRequest->absPath = compressedPath.u8string();
+
+            auto vram_texture_io = make_zeroed<skr_vram_texture_io_t>();
+            vram_texture_io.device = render_device->get_cgpu_device();
+            vram_texture_io.dstorage.path = dRequest->absPath.c_str();
+            vram_texture_io.dstorage.compression = CGPU_DSTORAGE_COMPRESSION_NONE;
+            vram_texture_io.dstorage.source_type = CGPU_DSTORAGE_SOURCE_FILE;
+            vram_texture_io.dstorage.queue = file_dstorage_queue;
+            vram_texture_io.dstorage.uncompressed_size = texture_resource->data_size;
+            
+            vram_texture_io.vtexture.resource_types = CGPU_RESOURCE_TYPE_NONE;
+            vram_texture_io.vtexture.texture_name = nullptr; // TODO: Name
+            vram_texture_io.vtexture.width = texture_resource->width;
+            vram_texture_io.vtexture.height = texture_resource->height;
+            vram_texture_io.vtexture.depth = texture_resource->depth;
+            vram_texture_io.vtexture.format = (ECGPUFormat)texture_resource->format;
+
+            vram_texture_io.callbacks[SKR_ASYNC_IO_STATUS_OK] = +[](skr_async_io_request_t* request, void* data){
+
+            };
+            vram_texture_io.callback_datas[SKR_ASYNC_IO_STATUS_OK] = nullptr;
+            
+            root.vram_service->request(&vram_texture_io, &dRequest->vtexture_request, &dRequest->texture_destination);
+        }        
+    }
     return ESkrInstallStatus::SKR_INSTALL_STATUS_INPROGRESS;
 }
 
@@ -167,7 +243,19 @@ bool STextureFactoryImpl::Uninstall(skr_resource_record_t* record)
 
 ESkrInstallStatus STextureFactoryImpl::UpdateInstall(skr_resource_record_t* record)
 {
-    return ESkrInstallStatus::SKR_INSTALL_STATUS_SUCCEED; 
+    auto texture_resource = (skr_texture_resource_t*)record->resource;
+    auto dRequest = mDStorageRequests.find(texture_resource);
+    if (dRequest != mDStorageRequests.end())
+    {
+        bool okay = dRequest->second->vtexture_request.is_ready();
+        auto status = okay ? ESkrInstallStatus::SKR_INSTALL_STATUS_SUCCEED : ESkrInstallStatus::SKR_INSTALL_STATUS_INPROGRESS;
+        if (okay)
+        {
+            mDStorageRequests.erase(texture_resource);
+        }
+        return status;
+    }
+    return ESkrInstallStatus::SKR_INSTALL_STATUS_INPROGRESS;
 }
 
 void STextureFactoryImpl::DestroyResource(skr_resource_record_t* record)
