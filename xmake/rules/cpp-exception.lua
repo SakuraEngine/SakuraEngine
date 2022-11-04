0f2a54ef8e1260863b11df45ce993e8e53e606cd
SaeruHikari <39457738+SaeruHikari@users.noreply.github.com>
2022-11-04 13:43:34 +0800

    去掉冗余的 xmake 脚本行

--- xmake/rules/cpp-exception.lua
+++ xmake/rules/cpp-exception.lua
@@ -2,7 +2,6 @@ rule("c++.exception")
     on_config(function (target)
         if (target:has_tool("cxx", "cl")) then
             target:add("cxflags", "/EHsc", {force = true})
-            target:add("cxxflags", "/EHsc", {force = true})
             target:add("defines", "_HAS_EXCEPTIONS=1", {force = true})
         elseif(target:has_tool("cxx", "cl") or target:has_tool("cxx", "clang-cl")) then
             target:add("cxflags", "-fexceptions", {force = true})
@@ -14,7 +13,6 @@ rule("c++.noexception")
     on_config(function (target)
         if (target:has_tool("cxx", "cl")) then
             target:add("cxflags", "/EHs-c-", {force = true})
-            target:add("cxflags", "/D_HAS_EXCEPTIONS=0", {force = true})
             target:add("defines", "_HAS_EXCEPTIONS=0", {force = true})
         elseif(target:has_tool("cxx", "cl") or target:has_tool("cxx", "clang-cl")) then
             target:add("cxflags", "-fno-exceptions", {force = true})
