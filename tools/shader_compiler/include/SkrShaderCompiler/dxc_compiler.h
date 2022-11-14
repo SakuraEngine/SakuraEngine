eb64caaf587ef49499c04f25b572765c58f72a31
SaeruHikari <39457738+SaeruHikari@users.noreply.github.com>
2022-11-14 22:30:16 +0800

    dxc compiler setup for shader compiler tool

--- tools/shader_compiler/include/SkrShaderCompiler/dxc_compiler.h
+++ tools/shader_compiler/include/SkrShaderCompiler/dxc_compiler.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "SkrShaderCompiler/module.configure.h"
 #include "platform/configure.h"
+#include "utils/log.h"
 
 #ifdef __cplusplus
 #include "platform/shared_library.hpp"
@@ -8,23 +9,53 @@
 #include "SkrShaderCompiler/dxc_compiler.generated.h"
 #endif 
 
-SKR_SHADER_COMPILER_EXTERN_C 
-void Util_ShaderCompilerEventOnLoad(const char*, eastl::function<void()> event);
+struct IDxcUtils;
+struct IDxcCompiler3;
+struct IDxcIncludeHandler;
+
+namespace skd sreflect
+{
+
+void Util_ShaderCompilerEventOnLoad(const char*, const eastl::function<void()>& event);
+void Util_ShaderCompilerEventOnUnload(const char*, const eastl::function<void()>& event);
+
+sreflect_struct("guid" : "fef60053-e3d6-4296-8aae-5c508896930b")
+SKR_SHADER_COMPILER_API SDXCCompiler
+{
+public:
+    SDXCCompiler(IDxcUtils* utils, IDxcCompiler3* compiler) SKR_NOEXCEPT;
+    ~SDXCCompiler() SKR_NOEXCEPT;
+
+    void SetIncludeHandler(IDxcIncludeHandler* includeHandler) SKR_NOEXCEPT;
+
+protected:
+    IDxcUtils* utils = nullptr;
+    IDxcCompiler3* compiler = nullptr;
+    IDxcIncludeHandler* includeHandler = nullptr;
+};
 
 sreflect_struct("guid" : "ae28a9e5-39cf-4eab-aa27-6103f42cbf2d")
-sattr("rtti" : true)
 SKR_SHADER_COMPILER_API SDXCLibrary
 {
-    sattr("no-rtti" : true)
+    friend struct DxcCreateInstanceT;
+public:
+    static SDXCLibrary* Get() SKR_NOEXCEPT;    
+    static eastl::function<void()> DXCLoader() SKR_NOEXCEPT;
+    static eastl::function<void()> DXILLoader() SKR_NOEXCEPT;
+    static eastl::function<void()> LibrariesUnloader() SKR_NOEXCEPT;
+
+    SDXCCompiler* CreateCompiler() SKR_NOEXCEPT;
+    void FreeCompiler(SDXCCompiler* compiler) SKR_NOEXCEPT;
+
+protected:
     skr::SharedLibrary dxc_library;
-    sattr("no-rtti" : true)
     skr::SharedLibrary dxil_library;
+    void* pDxcCreateInstance = nullptr; 
 }
-sstatic_ctor(Util_ShaderCompilerEventOnLoad("LoadDXC", []() -> void {
+// load dxc dll
+sstatic_ctor(skd::Util_ShaderCompilerEventOnLoad("LoadDXC", skd::SDXCLibrary::DXCLoader()))
+sstatic_ctor(skd::Util_ShaderCompilerEventOnLoad("LoadDXIL", skd::SDXCLibrary::DXILLoader()))
+sstatic_ctor(skd::Util_ShaderCompilerEventOnUnload("LoadDXIL", skd::SDXCLibrary::LibrariesUnloader()));
 
-}))
-sstatic_ctor(Util_ShaderCompilerEventOnLoad("LoadDXIL", []() -> void {
-
-}));
-
+} // end namespace skd
 #endif
--- tools/shader_compiler/src/shader_compiler.cpp
+++ tools/shader_compiler/src/shader_compiler.cpp
@@ -6,25 +6,38 @@ struct SKR_SHADER_COMPILER_API SShaderCompilerModule : public skr::IDynamicModul
 {
     virtual void on_load(int argc, char** argv) override
     {
-        for (auto&& [name, event] : on_load_events)
+        for (auto&& [name, load_event] : on_load_events)
         {
-            // SKR_LOG_DEBUG("ShaderCompilerModule event %s invoked", name.c_str());
-            event();
+            // SKR_LOG_DEBUG("ShaderCompilerModule load event %s invoked", name.c_str());
+            load_event();
         }
     }
 
     virtual void on_unload() override
     {
-
+        for (auto&& [name, unload_event] : on_unload_events)
+        {
+            // SKR_LOG_DEBUG("ShaderCompilerModule unload event %s invoked", name.c_str());
+            unload_event();
+        }
     }
     static eastl::vector<eastl::pair<eastl::string, eastl::function<void()>>> on_load_events;
+    static eastl::vector<eastl::pair<eastl::string, eastl::function<void()>>> on_unload_events;
 };
 
 IMPLEMENT_DYNAMIC_MODULE(SShaderCompilerModule, SkrShaderCompiler);
 eastl::vector<eastl::pair<eastl::string, eastl::function<void()>>> SShaderCompilerModule::on_load_events = {};
+eastl::vector<eastl::pair<eastl::string, eastl::function<void()>>> SShaderCompilerModule::on_unload_events = {};
 
-SKR_SHADER_COMPILER_EXTERN_C
-void Util_ShaderCompilerEventOnLoad(const char* name, eastl::function<void()> event)
+namespace skd
 {
-    SShaderCompilerModule::on_load_events.push_back(eastl::make_pair(name, event));
+void Util_ShaderCompilerEventOnLoad(const char* name, const eastl::function<void()>& event)
+{
+    SShaderCompilerModule::on_load_events.emplace_back(name, event);
 }
+
+void Util_ShaderCompilerEventOnUnload(const char* name, const eastl::function<void()>& event)
+{
+    SShaderCompilerModule::on_unload_events.emplace_back(name, event);
+}
+} // end namespace skd
