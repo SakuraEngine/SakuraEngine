b27f3c3d11da573555ff4e60fcebeb63582495ed
SaeruHikari <39457738+SaeruHikari@users.noreply.github.com>
2022-11-15 17:04:18 +0800

    优化 parallel_for 的 batch 算法

--- modules/runtime/include/utils/parallel_for.hpp
+++ modules/runtime/include/utils/parallel_for.hpp
@@ -9,7 +9,7 @@ template <class F, class Iter>
 void parallel_for(Iter begin, Iter end, size_t batch, F f)
 {
     typename std::iterator_traits<Iter>::difference_type n = std::distance(begin, end);
-    size_t batchCount = (n / batch) + (n % batch ? 1 : 0);
+    size_t batchCount = (n + batch - 1) / batch;
     task::counter_t counter;
     counter.add((uint32_t)batchCount);
     for (size_t i = 0; i < batchCount; ++i)
