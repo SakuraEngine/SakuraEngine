863e0b5ad1d7753078099e122950868260728a8f
SaeruHikari <39457738+SaeruHikari@users.noreply.github.com>
2022-11-22 15:54:30 +0800

    修复写错字的构建问题

--- tools/animation_tool/include/SkrAnimTool/animation_asset.h
+++ tools/animation_tool/include/SkrAnimTool/animation_asset.h
@@ -1,20 +1,21 @@
 #pragma once
 #include "SkrAnimTool/module.configure.h"
-
-#ifndef __meta__
-#include "SkrAnimTool/animation_asset.genereated.h"
-#endif
 #include "SkrToolCore/asset/importer.hpp"
+#ifndef __meta__
+#include "SkrAnimTool/animation_asset.generated.h"
+#endif
+
 namespace skd sreflect
 {
 namespace asset sreflect
 {
-    class SKR_ANIMTOOL_API SAnimGltfImporter : public skd::asset::SImporter
-    {
-        virtual ~SAnimGltfImporter() = default;
-        virtual void* Import(skr::io::RAMService*, SCookContext* context) override;
-        virtual void Destroy(void*) override;
-        static uint32_t Version() { return kDevelopmentVersion; }
-    };
+sreflect_struct("guid" : "37d07586-0901-480a-8dcd-1f1f8220569c")
+SKR_ANIMTOOL_API SAnimGltfImporter : public skd::asset::SImporter
+{
+    virtual ~SAnimGltfImporter() = default;
+    virtual void* Import(skr::io::RAMService*, SCookContext* context) override;
+    virtual void Destroy(void*) override;
+    static uint32_t Version() { return kDevelopmentVersion; }
+};
 }
 }
