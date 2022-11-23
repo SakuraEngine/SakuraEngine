9df471f4749a16abc404f1f0de813fe57269fba0
BenzzZX <672619410@qq.com>
2022-11-23 23:06:49 +0800

    static dependency

--- tools/animation_tool/src/gltf/animation_asset.cpp
+++ tools/animation_tool/src/gltf/animation_asset.cpp
@@ -12,7 +12,7 @@ void* SAnimGltfImporter::Import(skr::io::RAMService*, SCookContext* context)
 {
     GltfImporter impl;
     ozz::animation::offline::OzzImporter& impoter = impl;
-    auto& skeletonResource = context->GetStaticDependencies()[0];
+    auto& skeletonResource = context->GetStaticDependency(0);
     ozz::animation::Skeleton& skeleton = *(ozz::animation::Skeleton*)skeletonResource.get_ptr();
     RawAnimation* rawAnimation = SkrNew<RawAnimation>();
     impoter.Import(animationName.c_str(), skeleton,
