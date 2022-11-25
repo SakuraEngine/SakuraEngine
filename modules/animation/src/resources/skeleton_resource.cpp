d653637a6f79ca10e8dec9ac33bf735ab6fac128
gaymenhub <672619410@qq.com>
2022-11-25 16:49:44 +0800

    adapt ozz serde

--- modules/animation/src/resources/skeleton_resource.cpp
+++ modules/animation/src/resources/skeleton_resource.cpp
@@ -1,1 +1,13 @@
 #include "SkrAnim/resources/skeleton_resource.h"
+#include "SkrAnim/ozz/base/io/archive.h"
+
+namespace skr::binary
+{
+    int ReadHelper<skr_skeleton_resource_t>::Read(skr_binary_reader_t *reader, skr_skeleton_resource_t &value)
+    {
+        ozz::io::SkrStream stream(reader, nullptr);
+        ozz::io::IArchive archive(&stream);
+        archive >> value.skeleton;
+        return 0;
+    }
+}
