1465221061f254db3ef8d91f6cb45290fa7cd520
BenzzZX <672619410@qq.com>
2022-11-27 13:26:05 +0800

    bin -> blob_view

--- modules/animation/src/components/skin_component.cpp
+++ modules/animation/src/components/skin_component.cpp
@@ -6,13 +6,13 @@ void skr_initialize_skin_component(skr_skin_component_t *component, skr_skeleton
     auto skin = component->skin.get_resolved();
     auto skeleton = skelComponent->skeleton.get_resolved();
     SKR_ASSERT(skin && skeleton);
-    auto size = skin->bin.joint_remaps.size();
+    auto size = skin->blob.joint_remaps.size();
     component->joint_remaps.resize(size);
     for (size_t i = 0; i < size; ++i)
     {   
         for(size_t j = 0; j < skeleton->skeleton.num_joints(); ++j)
         {
-            if (std::strcmp(skeleton->skeleton.joint_names()[j], skin->bin.joint_remaps[i].data()) == 0)
+            if (std::strcmp(skeleton->skeleton.joint_names()[j], skin->blob.joint_remaps[i].data()) == 0)
             {
                 component->joint_remaps[i] = j;
                 break;
--- tools/animation_tool/src/skin_asset.cpp
+++ tools/animation_tool/src/skin_asset.cpp
@@ -15,24 +15,24 @@ bool SSkinCooker::Cook(SCookContext* ctx)
     {
         return false;
     }
-    auto bin = skr::make_blob_builder<skr_skin_bin_t>();
-    bin.joint_remaps.reserve(rawSkin->joints_count);
+    auto blob = skr::make_blob_builder<skr_skin_blob_view_t>();
+    blob.joint_remaps.reserve(rawSkin->joints_count);
     for (auto i = 0; i < rawSkin->joints_count; ++i)
-        bin.joint_remaps.push_back(rawSkin->joints[i]->name);
-    bin.inverse_bind_poses.reserve(rawSkin->joints_count);
+        blob.joint_remaps.push_back(rawSkin->joints[i]->name);
+    blob.inverse_bind_poses.reserve(rawSkin->joints_count);
     auto matrix = (cgltf_float*)rawSkin->inverse_bind_matrices->buffer_view->data;
     auto components = cgltf_num_components(cgltf_type_mat4);
     SKR_ASSERT(components == 16);
     for (auto i = 0; i < rawSkin->joints_count; ++i)
     {
-        bin.inverse_bind_poses.push_back(skr_float4x4_t{ { { matrix[0], matrix[1], matrix[2], matrix[3] },
+        blob.inverse_bind_poses.push_back(skr_float4x4_t{ { { matrix[0], matrix[1], matrix[2], matrix[3] },
         { matrix[4], matrix[5], matrix[6], matrix[7] },
         { matrix[8], matrix[9], matrix[10], matrix[11] },
         { matrix[12], matrix[13], matrix[14], matrix[15] } } });
         matrix += components;
     }
     skr_blob_arena_builder_t builder(32);
-    skr::binary::BlobHelper<skr_skin_bin_t>::BuildArena(builder, resource.bin, bin);
+    skr::binary::BlobHelper<skr_skin_blob_view_t>::BuildArena(builder, resource.blob, blob);
     resource.arena = builder.build();
     return true;
 }
