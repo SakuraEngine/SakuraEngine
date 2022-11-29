8ad3d483bfef24c237130c10759424a7db1d33e7
SaeruHikari <39457738+SaeruHikari@users.noreply.github.com>
2022-11-29 10:05:31 +0800

    分离 ozz 目标并默认开优化以提升性能

--- modules/animation/xmake.lua
+++ modules/animation/xmake.lua
@@ -1,3 +1,14 @@
+shared_module("SkrOzz", "SKR_OZZ", engine_version)
+    set_group("01.modules")
+    set_exceptions("no-cxx")
+    add_rules("skr.static_module", {api = "SKR_DEPENDENCY_GRAPH"})
+    set_optimize("fastest")
+    add_includedirs("ozz", {public=true})
+    add_includedirs("ozz/src", {public=false})
+    add_files("ozz/src/**.cpp", "ozz/src/**.cc")
+    add_rules("c++.unity_build", {batchsize = default_unity_batch_size})
+    public_dependency("SkrRT", engine_version)
+    
 shared_module("SkrAnim", "SKR_ANIM", engine_version)
     set_group("01.modules")
     add_rules("c++.codegen", {
@@ -5,10 +16,9 @@ shared_module("SkrAnim", "SKR_ANIM", engine_version)
         rootdir = "include/SkrAnim",
         api = "SKR_ANIM"
     })
-    public_dependency("SkrRT", engine_version)
+    public_dependency("SkrOzz", engine_version)
     public_dependency("SkrRenderer", engine_version)
     add_includedirs("include", {public=true})
     add_includedirs("ozz", {public=true})
-    add_includedirs("src", {public=false})
-    add_files("src/**.cpp", "src/**.cc")
+    add_files("src/**.cpp")
     add_rules("c++.unity_build", {batchsize = default_unity_batch_size})
