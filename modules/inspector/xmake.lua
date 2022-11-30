704130f9d8724847a1ccff79081f315589069701
gaymenhub <672619410@qq.com>
2022-11-30 20:51:27 +0800

    inspector 新建文件夹

---
+++ modules/inspector/xmake.lua
@@ -0,0 +1,10 @@
+ shared_module("SkrInspector", "SKR_INSPECT", engine_version)
+    set_group("01.modules")
+    -- add_rules("c++.codegen", {
+    --     files = {"include/**.h", "include/**.hpp"},
+    --     rootdir = "include/SkrInspector",
+    --     api = "SKR_INSPECT"
+    -- })
+    public_dependency("SkrImGui", engine_version)
+    add_includedirs("include", {public=true})
+    add_files("src/**.c", "src/**.cpp")
