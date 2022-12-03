b49c3344a829b63a1ffc8b2889e4a3dae96dc28d
BenzzZX <672619410@qq.com>
2022-12-04 03:38:05 +0800

    imgui lua great

--- modules/runtime/include/lua/skr_lua.h
+++ modules/runtime/include/lua/skr_lua.h
@@ -3,6 +3,7 @@
 #include "lua/lua.hpp"
 #include "utils/types.h"
 
-RUNTIME_EXTERN_C RUNTIME_API lua_State *skr_lua_newstate();
+RUNTIME_EXTERN_C RUNTIME_API lua_State *skr_lua_newstate(struct skr_vfs_t* vfs);
+RUNTIME_EXTERN_C RUNTIME_API void skr_lua_close(lua_State* L);
 
 
