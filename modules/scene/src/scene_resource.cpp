d1601d2409f88848da85d17552fc65b7063651f4
gaymenhub <672619410@qq.com>
2022-12-08 21:47:59 +0800

    lua hierachy

--- modules/scene/src/scene_resource.cpp
+++ modules/scene/src/scene_resource.cpp
@@ -4,7 +4,7 @@ namespace skr::resource
 {
     skr_type_id_t SSceneFactory::GetResourceType()
     {
-        return {0x2DEFA834, 0x5CF3, 0x45A5, {0x86, 0x7E, 0x8E, 0x0E, 0xD0, 0xF7, 0xD6, 0xAE}};
+        return {0xEFBA637E, 0xE7E5, 0x4B64, {0xBA, 0x26, 0x90, 0xAE, 0xEE, 0x9E, 0x3E, 0x1A}};
     }
 }
 
@@ -13,6 +13,7 @@ namespace skr::binary
 int ReadTrait<skr_scene_resource_t>::Read(skr_binary_reader_t* reader, skr_scene_resource_t& value)
 {
     //TODO: error code?
+    value.storage = dualS_create();
     dualS_deserialize(value.storage, reader);
     return 0;
 }
