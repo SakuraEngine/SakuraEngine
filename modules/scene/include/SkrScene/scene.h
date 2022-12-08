e6928563949dd298c970a60f3c05954ceebe2235
gaymenhub <672619410@qq.com>
2022-12-08 16:52:36 +0800

    rename comps

--- modules/scene/include/SkrScene/scene.h
+++ modules/scene/include/SkrScene/scene.h
@@ -9,39 +9,30 @@
 // scene hierarchy
 
 struct sreflect sattr(
-    "guid" : "EFBA637E-E7E5-4B64-BA26-90AEEE9E3E1A"
-)
-skr_scene_t
-{
-    struct dual_storage_t* world;
-};
-typedef struct skr_scene_t game_scene_t;
-
-struct sreflect sattr(
     "guid" : "82CDDC11-3D94-4552-8FD4-237A053F35C0",
     "component" : 
     {
         "buffer" : 4
     }
-) skr_child_t
+) skr_child_comp_t
 {
     dual_entity_t entity;
 };
-typedef struct skr_child_t skr_child_t;
+typedef struct skr_child_comp_t skr_child_comp_t;
 
 #ifdef __cplusplus
-using skr_children_t = dual::array_comp_T<skr_child_t, 4>;
+using skr_children_t = dual::array_comp_T<skr_child_comp_t, 4>;
 #endif
 
 struct sreflect sattr(
     "guid" : "2CAA41D2-54A4-46FB-BE43-68B545F313BF",
     "component" : true
 )
-skr_parent_t
+skr_parent_comp_t
 {
     dual_entity_t entity;
 };
-typedef struct skr_parent_t skr_parent_t;
+typedef struct skr_parent_comp_t skr_parent_comp_t;
 
 #define SKR_SCENE_MAX_NAME_LENGTH 32
 
@@ -71,7 +62,7 @@ struct sreflect sattr(
     "guid" : "AE2C7477-8A44-4339-BE5D-64D05D7E05B1",
     "component" : true //, "serialize" : "USD"
 )
-SKR_ALIGNAS(16) skr_l2w_t
+SKR_ALIGNAS(16) skr_l2w_comp_t
 {
     skr_float4x4_t matrix;
 };
@@ -80,7 +71,7 @@ struct sreflect sattr(
     "guid" : "869F46D3-992A-4C18-9538-BDC48F4BED1D",
     "component" : true
 )
-SKR_ALIGNAS(16) skr_l2r_t
+SKR_ALIGNAS(16) skr_l2r_comp_t
 {
     skr_float4x4_t matrix;
 };
@@ -89,7 +80,7 @@ struct sreflect sattr(
     "guid" : "78DD218B-87DE-4250-A7E8-A6B4553B47BF",
     "component" : true
 )
-skr_rotation_t
+skr_rotation_comp_t
 {
     skr_rotator_t euler;
 };
@@ -98,7 +89,7 @@ struct sreflect sattr(
     "guid" : "A059A2A1-CC3B-43B0-88B6-ADA7822BA25D",
     "component" : true
 )
-skr_translation_t
+skr_translation_comp_t
 {
     skr_float3_t value;
 };
@@ -107,7 +98,7 @@ struct sreflect sattr(
     "guid" : "D045D755-FBD1-44C2-8BF0-C86F2D8485FF",
     "component" : true
 )
-skr_scale_t
+skr_scale_comp_t
 {
     skr_float3_t value;
 };
@@ -116,7 +107,7 @@ struct sreflect sattr(
     "guid" : "4fa24729-2c66-45a2-9417-3497ebc18771",
     "component" : true
 )
-skr_movement_t
+skr_movement_comp_t
 {
     skr_float3_t value;
 };
@@ -125,20 +116,20 @@ struct sreflect sattr(
     "guid" : "d33c74c5-2763-4ba4-b58e-dc44a627ebf4",
     "component" : true
 )
-skr_camera_t
+skr_camera_comp_t
 {
     uint32_t viewport_width;
     uint32_t viewport_height;
 };
 
-struct skr_transform_system {
+struct skr_transform_system_t {
     dual_query_t* localToWorld;
     dual_query_t* localToRelative;
     dual_query_t* relativeToWorld;
 };
 
-SKR_SCENE_EXTERN_C SKR_SCENE_API void skr_transform_setup(dual_storage_t* world, skr_transform_system* system);
-SKR_SCENE_EXTERN_C SKR_SCENE_API void skr_transform_update(skr_transform_system* query);
+SKR_SCENE_EXTERN_C SKR_SCENE_API void skr_transform_setup(dual_storage_t* world, skr_transform_system_t* system);
+SKR_SCENE_EXTERN_C SKR_SCENE_API void skr_transform_update(skr_transform_system_t* query);
 
 #ifdef __cplusplus
 #include "lua/bind.hpp"
--- modules/scene/src/transform.cpp
+++ modules/scene/src/transform.cpp
@@ -6,17 +6,17 @@
 #include "SkrScene/scene.h"
 
 /*
-static_assert(alignof(decltype(skr_l2r_t::matrix)) == 16, "Alignment of matrix must be 16");
-static_assert(sizeof(skr_l2w_t::matrix) == sizeof(skr_float4x4_t), "Size of matrix must equal to skr_float4x4_t");
+static_assert(alignof(decltype(skr_l2r_comp_t::matrix)) == 16, "Alignment of matrix must be 16");
+static_assert(sizeof(skr_l2w_comp_t::matrix) == sizeof(skr_float4x4_t), "Size of matrix must equal to skr_float4x4_t");
 
-static_assert(alignof(decltype(skr_rotation_t::euler)) == alignof(skr::math::Rotator), "Alignment of euler must equal to skr::math::Rotator");
-static_assert(sizeof(skr_rotation_t::euler) == sizeof(skr::math::Rotator), "Size of euler must equal to skr::math::Rotator");
+static_assert(alignof(decltype(skr_rotation_comp_t::euler)) == alignof(skr::math::Rotator), "Alignment of euler must equal to skr::math::Rotator");
+static_assert(sizeof(skr_rotation_comp_t::euler) == sizeof(skr::math::Rotator), "Size of euler must equal to skr::math::Rotator");
 
-static_assert(sizeof(skr_translation_t::value) == sizeof(skr::math::Vector3f), "Size of translation must equal to skr::math::Vector3f");
-static_assert(alignof(decltype(skr_translation_t::value)) == alignof(skr::math::Vector3f), "Alignment of translation must equal to skr::math::Vector3f");
+static_assert(sizeof(skr_translation_comp_t::value) == sizeof(skr::math::Vector3f), "Size of translation must equal to skr::math::Vector3f");
+static_assert(alignof(decltype(skr_translation_comp_t::value)) == alignof(skr::math::Vector3f), "Alignment of translation must equal to skr::math::Vector3f");
 
-static_assert(sizeof(skr_scale_t::value) == sizeof(skr::math::Vector3f), "Size of translation must equal to skr::math::Vector3f");
-static_assert(alignof(decltype(skr_scale_t::value)) == alignof(skr::math::Vector3f), "Alignment of scale must equal to skr::math::Vector3f");
+static_assert(sizeof(skr_scale_comp_t::value) == sizeof(skr::math::Vector3f), "Size of translation must equal to skr::math::Vector3f");
+static_assert(alignof(decltype(skr_scale_comp_t::value)) == alignof(skr::math::Vector3f), "Alignment of scale must equal to skr::math::Vector3f");
 
 template <class T>
 static void skr_local_to_x(void* u, dual_storage_t* storage, dual_chunk_view_t* view, dual_type_index_t* localTypes, EIndex entityIndex)
@@ -52,24 +52,24 @@ static void skr_local_to_x(void* u, dual_storage_t* storage, dual_chunk_view_t* 
             *transform = make_transform(default_translation, default_scale, quaternion_from_rotator(rotation[i]));
 }
 
-static void skr_relative_to_world_children(skr_children_t* children, skr_l2w_t* parent, dual_storage_t* storage)
+static void skr_relative_to_world_children(skr_children_t* children, skr_l2w_comp_t* parent, dual_storage_t* storage)
 {
-    auto process = [&](skr_child_t child) {
+    auto process = [&](skr_child_comp_t child) {
         dual_chunk_view_t view;
         //TODO: consider dualS_batch?
         dualS_access(storage, child.entity, &view);
-        auto relative = (skr_float4x4_t*)dualV_get_owned_ro(&view, dual_id_of<skr_l2r_t>::get());
+        auto relative = (skr_float4x4_t*)dualV_get_owned_ro(&view, dual_id_of<skr_l2r_comp_t>::get());
         if (!relative) 
             return;
-        auto transform = (skr_float4x4_t*)dualV_get_owned_ro(&view, dual_id_of<skr_l2w_t>::get());
+        auto transform = (skr_float4x4_t*)dualV_get_owned_ro(&view, dual_id_of<skr_l2w_comp_t>::get());
         if (!transform) 
             return;
 
         *transform = multiply(*relative, *(skr_float4x4_t*)&parent->matrix);
-        auto children = (skr_children_t*)dualV_get_owned_ro(&view, dual_id_of<skr_child_t>::get());
+        auto children = (skr_children_t*)dualV_get_owned_ro(&view, dual_id_of<skr_child_comp_t>::get());
         if (!children) 
             return;
-        skr_relative_to_world_children(children, (skr_l2w_t*)transform, storage);
+        skr_relative_to_world_children(children, (skr_l2w_comp_t*)transform, storage);
     };
     if (children->size() > 256) // dispatch recursively
     {
@@ -88,29 +88,29 @@ static void skr_relative_to_world_children(skr_children_t* children, skr_l2w_t* 
 static void skr_relative_to_world_root(void* u, dual_storage_t* storage, dual_chunk_view_t* view, dual_type_index_t* localTypes, EIndex entityIndex)
 {
     using namespace skr::math;
-    auto transform = (skr_l2w_t*)dualV_get_owned_ro_local(view, localTypes[0]);
+    auto transform = (skr_l2w_comp_t*)dualV_get_owned_ro_local(view, localTypes[0]);
     auto children = (skr_children_t*)dualV_get_owned_ro_local(view, localTypes[1]);
 
     forloop (i, 0, view->count)
         skr_relative_to_world_children(&children[i], &transform[i], storage);
 }
 
-void skr_transform_setup(dual_storage_t* world, skr_transform_system* system)
+void skr_transform_setup(dual_storage_t* world, skr_transform_system_t* system)
 {
     // for root entities, calculate local to world
-    system->localToWorld = dualQ_from_literal(world, "[in]|skr_translation_t,[in]|skr_rotation_t, [in]|skr_scale_t,[out]skr_l2w_t,!skr_parent_t");
+    system->localToWorld = dualQ_from_literal(world, "[in]|skr_translation_comp_t,[in]|skr_rotation_comp_t, [in]|skr_scale_comp_t,[out]skr_l2w_comp_t,!skr_parent_comp_t");
 
     // for node entities, calculate local to parent
-    system->localToRelative = dualQ_from_literal(world, "[in]|skr_translation_t,[in]|skr_rotation_t,[in]|skr_scale_t,[out]skr_l2r_t,[has]skr_parent_t");
+    system->localToRelative = dualQ_from_literal(world, "[in]|skr_translation_comp_t,[in]|skr_rotation_comp_t,[in]|skr_scale_comp_t,[out]skr_l2r_comp_t,[has]skr_parent_comp_t");
 
     // then recursively calculate local to world for node entities
-    system->relativeToWorld = dualQ_from_literal(world, "[inout][rand]skr_l2w_t,[in][rand]skr_child_t,[in][rand]?skr_l2r_t,!skr_parent_t");
+    system->relativeToWorld = dualQ_from_literal(world, "[inout][rand]skr_l2w_comp_t,[in][rand]skr_child_comp_t,[in][rand]?skr_l2r_comp_t,!skr_parent_comp_t");
 }
 
-void skr_transform_update(skr_transform_system* query)
+void skr_transform_update(skr_transform_system_t* query)
 {
-    dualJ_schedule_ecs(query->localToWorld, 256, &skr_local_to_x<skr_l2w_t>, nullptr, nullptr, nullptr, nullptr, nullptr);
-    dualJ_schedule_ecs(query->localToRelative, 256, &skr_local_to_x<skr_l2r_t>, nullptr, nullptr, nullptr, nullptr, nullptr);
+    dualJ_schedule_ecs(query->localToWorld, 256, &skr_local_to_x<skr_l2w_comp_t>, nullptr, nullptr, nullptr, nullptr, nullptr);
+    dualJ_schedule_ecs(query->localToRelative, 256, &skr_local_to_x<skr_l2r_comp_t>, nullptr, nullptr, nullptr, nullptr, nullptr);
     dualJ_schedule_ecs(query->relativeToWorld, 128, &skr_relative_to_world_root, nullptr, nullptr, nullptr, nullptr, nullptr);
 }
 */
