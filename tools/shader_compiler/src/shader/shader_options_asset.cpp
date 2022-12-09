48330f1a8de6f9f4e47c57b8e6145f4534ed1caf
SaeruHikari <39457738+SaeruHikari@users.noreply.github.com>
2022-12-09 16:24:59 +0800

    shader options 保存改用 ctx->Save

--- tools/shader_compiler/src/shader/shader_options_asset.cpp
+++ tools/shader_compiler/src/shader/shader_options_asset.cpp
@@ -48,28 +48,16 @@ void SShaderOptionsImporter::Destroy(void *resource)
 bool SShaderOptionsCooker::Cook(SCookContext *ctx)
 {
     const auto outputPath = ctx->GetOutputPath();
-    const auto assetRecord = ctx->GetAssetRecord();
     //-----load config
     // no cook config for config, skipping
+    
     //-----import resource object
     auto options = ctx->Import<skr_shader_options_resource_t>();
     if(!options) return false;
     SKR_DEFER({ ctx->Destroy(options); });
-    //------write resource object
-    eastl::vector<uint8_t> buffer;
-    skr::binary::VectorWriter writer{&buffer};
-    skr_binary_writer_t archive(writer);
-    skr::binary::Archive(&archive, *options);
-    //------save resource to disk
-    auto file = fopen(outputPath.u8string().c_str(), "wb");
-    if (!file)
-    {
-        SKR_LOG_FMT_ERROR("[SShaderOptionsCooker::Cook] failed to write cooked file for resource {}! path: {}", 
-            assetRecord->guid, assetRecord->path.u8string());
-        return false;
-    }
-    SKR_DEFER({ fclose(file); });
-    fwrite(buffer.data(), 1, buffer.size(), file);
+
+    //------write resource object to disk
+    ctx->Save(*options);
     return true;
 }
 
