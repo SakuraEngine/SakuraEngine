caab9aea4970dd8c2be4b1174cc68fbba2dd5b77
SaeruHikari <39457738+SaeruHikari@users.noreply.github.com>
2022-12-10 02:58:01 +0800

    修复 Project RAM Service 线程没有正确退出的问题

--- tools/tool_core/src/project/project.cpp
+++ tools/tool_core/src/project/project.cpp
@@ -1,10 +1,12 @@
 #include "SkrToolCore/project/project.hpp"
 #include "platform/vfs.h"
+#include "utils/io.h"
 
 namespace skd
 {
 SProject::~SProject() noexcept
 {
+    if(ram_service) skr_io_ram_service_t::destroy(ram_service);
     if (vfs) skr_free_vfs(vfs);
 }
 }
