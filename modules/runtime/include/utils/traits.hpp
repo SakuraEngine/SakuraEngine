fa2379951e20032eb582d8b2185add588fb252fd
BenzzZX <672619410@qq.com>
2022-12-15 01:03:17 +0800

    小 concept

--- modules/runtime/include/utils/traits.hpp
+++ modules/runtime/include/utils/traits.hpp
@@ -3,15 +3,31 @@
 
 namespace skr
 {
+    // type as value
     template<class T>
     struct type_t{ using type = T; };
+    // typelist via function type
+    #define SKR_TYPELIST(...) type_t<void(__VA_ARGS__)>{}
 
-    template<class T, class = void>
-    struct is_complete : std::false_type {};
+    template<class F> struct validator
+    {
+        template<class T> constexpr validator(T&&) noexcept {}
+        // by variable
+        template<class ...Args> constexpr bool operator()(Args&&...) const noexcept
+        {
+            return std::is_invocable_v<F&&, Args&&...>;
+        }
+        // by type
+        template<class ...Args> constexpr bool operator()(type_t<void(Args...)>) const noexcept
+        {
+            return std::is_invocable_v<F&&, Args&&...>;
+        }
+    };
+    template<class F> validator(F&&)->validator<F&&>;
+    #define SKR_VALIDATOR(def, ...) ::skr::validator{[]def -> decltype(__VA_ARGS__) { return __VA_ARGS__; }}
 
+    // use sizeof to check if a type is complete
+    constexpr static auto is_complete = SKR_VALIDATOR((auto t), sizeof(t));
     template<class T>
-    struct is_complete<T, std::enable_if_t<sizeof(T)>> : std::true_type {};
-
-    template<class T>
-    constexpr bool is_complete_v = is_complete<T>::value;
-};
+    constexpr bool is_complete_v = is_complete(SKR_TYPELIST(T));
+}
