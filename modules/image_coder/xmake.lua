6814d4f89e5eea4dda49c04ae7d7813b9ac2030a
SaeruHikari <39457738+SaeruHikari@users.noreply.github.com>
2022-12-20 00:00:39 +0800

    fix compile error

--- modules/image_coder/xmake.lua
+++ modules/image_coder/xmake.lua
@@ -11,21 +11,21 @@ shared_module("SkrImageCoder", "SKR_IMAGE_CODER", engine_version)
         add_linkdirs("lib/windows/x64", {public=true})
 
         add_includedirs("include", {public=true})
-        add_includedirs("libpng/1.5.2", {public=false})
+        add_includedirs("libpng/1.5.2", {public=true})
         if (is_mode("asan")) then
-            add_links("libpng15_static", {public=false})
+            add_links("libpng15_static", {public=true})
         elseif (is_mode("release")) then
-            add_links("libpng15_static", {public=false})
+            add_links("libpng15_static", {public=true})
         else
-            add_links("libpng15_staticd", {public=false})
+            add_links("libpng15_staticd", {public=true})
         end
-        add_links("turbojpeg_static", {public=false})
+        add_links("turbojpeg_static", {public=true})
     end
     -- png
     if (is_os("macosx")) then 
-        add_linkdirs("lib/macos/x86_64", {public=false})
+        add_linkdirs("lib/macos/x86_64", {public=true})
 
-        add_includedirs("include", "libpng/1.5.27", {public=false})
-        add_links("png", {public=false})
-        add_links("turbojpeg", {public=false})
+        add_includedirs("include", "libpng/1.5.27", {public=true})
+        add_links("png", {public=true})
+        add_links("turbojpeg", {public=true})
     end
