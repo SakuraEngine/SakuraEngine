a4136ee308582034416fc90f2f15930fb45bfbd8
SaeruHikari <39457738+SaeruHikari@users.noreply.github.com>
2022-12-21 02:08:10 +0800

    parallel_for 添加 inplace (non-parallel & pinned) 执行的功能

--- modules/runtime/include/utils/parallel_for.hpp
+++ modules/runtime/include/utils/parallel_for.hpp
@@ -6,26 +6,42 @@
 namespace skr
 {
 template <class F, class Iter>
-void parallel_for(Iter begin, Iter end, size_t batch, F f)
+void parallel_for(Iter begin, Iter end, size_t batch, F f, uint32_t inplace_batch_threahold = 1u)
 {
     typename std::iterator_traits<Iter>::difference_type n = std::distance(begin, end);
     size_t batchCount = (n + batch - 1) / batch;
-    task::counter_t counter;
-    counter.add((uint32_t)batchCount);
-    for (size_t i = 0; i < batchCount; ++i)
+    if (batchCount < inplace_batch_threahold)
     {
-        auto toAdvance = std::min((size_t)n, batch);
-        auto l = begin;
-        auto r = begin;
-        std::advance(r, toAdvance);
-        n -= toAdvance;
-        skr::task::schedule([counter, f, l, r] () mutable
-            {
-                SKR_DEFER({counter.decrement();});
-                f(l, r);
-            }, nullptr);
-        begin = r;
+        for (size_t i = 0; i < batchCount; ++i)
+        {
+            auto toAdvance = std::min((size_t)n, batch);
+            auto l = begin;
+            auto r = begin;
+            std::advance(r, toAdvance);
+            n -= toAdvance;
+            f(l, r);
+            begin = r;
+        }
     }
-    counter.wait(true);
+    else
+    {
+        task::counter_t counter;
+        counter.add((uint32_t)batchCount);
+        for (size_t i = 0; i < batchCount; ++i)
+        {
+            auto toAdvance = std::min((size_t)n, batch);
+            auto l = begin;
+            auto r = begin;
+            std::advance(r, toAdvance);
+            n -= toAdvance;
+            skr::task::schedule([counter, f, l, r] () mutable
+                {
+                    SKR_DEFER({counter.decrement();});
+                    f(l, r);
+                }, nullptr);
+            begin = r;
+        }
+        counter.wait(true);
+    }
 }
 } // namespace skr
