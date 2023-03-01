f0ea186573ff9b67d9611c74c1d72cdd09ff8418
SaeruHikari <39457738+SaeruHikari@users.noreply.github.com>
2023-03-01 21:29:52 +0800

    add bind_tables LUT in fwd pass

--- samples/application/game/assets/shaders/emissive_ps.hlsl
+++ samples/application/game/assets/shaders/emissive_ps.hlsl
@@ -14,10 +14,14 @@ struct VSOut
 Texture2D color_texture : register(t0, space1);
 [[vk::binding(0, 2)]]
 SamplerState color_sampler : register(s0, space2);
+[[vk::binding(1, 1)]]
+Texture2D emissive_texture : register(t1, space1);
+[[vk::binding(1, 2)]]
+SamplerState emissive_sampler : register(s1, space2);
 
 void main(VSOut psIn,     
     out float4 o_color : SV_Target0) : SV_TARGET
 {
     float2 uv = psIn.uv;
-    o_color = color_texture.Sample(color_sampler, uv);
+    o_color = color_texture.Sample(color_sampler, uv) + emissive_texture.Sample(emissive_sampler, uv);
 }
