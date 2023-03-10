84982da5010fbfbbe307d6232d44b0844c5881b0
SaeruHikari <39457738+SaeruHikari@users.noreply.github.com>
2023-03-10 15:25:06 +0800

    fix compile error

--- modules/gui/gui/include/SkrGui/render_elements/render_text.hpp
+++ modules/gui/gui/include/SkrGui/render_elements/render_text.hpp
@@ -12,7 +12,7 @@ public:
     virtual ~RenderText();
 
     virtual void layout(struct Constraints* constraints, bool needSize = false) override;
-    virtual void draw(gdi::SGDIViewport* canvas) override;
+    virtual void draw(gdi::SGDICanvas* canvas) override;
 
     virtual skr_float2_t get_size() const;
     virtual void set_size(const skr_float2_t& size);
