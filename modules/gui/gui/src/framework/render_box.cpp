e71dfc697d7c749d797f2406ee2fa3540d5a2124
BenzzZX <672619410@qq.com>
2023-04-06 22:58:29 +0800

    hit test

--- modules/gui/gui/src/framework/render_box.cpp
+++ modules/gui/gui/src/framework/render_box.cpp
@@ -1,6 +1,8 @@
 #include "SkrGui/framework/render_box.hpp"
 #include "SkrGui/gdi/gdi.hpp"
 #include <algorithm>
+#include "math/matrix4x4f.h"
+#include "math/vector.h"
 
 namespace skr {
 namespace gui {
@@ -61,6 +63,46 @@ void RenderBox::before_draw(const DrawParams* params)
 void RenderBox::draw(const DrawParams* params)
 {
     RenderObject::draw(params);
+}
+
+bool RenderBox::hit_test(const Ray& point, HitTestRecord* record) const
+{
+    //convert to local space
+    auto origin = skr::math::load(point.origin);
+    auto direction = skr::math::load(point.direction);
+    rtm::matrix4x4f matrix = skr::math::load(render_matrix);
+    auto inv_matrix = rtm::matrix_inverse(matrix);
+    origin = rtm::matrix_mul_vector(origin, inv_matrix);
+    direction = rtm::matrix_mul_vector(direction, inv_matrix);
+    //check hit
+    auto min = skr::math::load(pos);
+    auto max = rtm::vector_add(skr::math::load(pos), skr::math::load(size));
+    auto o = origin;
+    auto d = direction;
+    auto tmin = (min - o) / d;
+    auto tmax = (max - o) / d;
+    auto tmin_max = rtm::vector_max(tmin, tmax);
+    auto tmax_min = rtm::vector_min(tmin, tmax);
+    auto tmin_max_min = rtm::vector_min(tmin_max, tmax_min);
+    auto tmin_max_max = rtm::vector_max(tmin_max, tmax_min);
+    auto tmin_max_min_max = rtm::vector_max(tmin_max_min, tmin_max_max);
+    auto tmin_max_min_max_x = rtm::vector_get_x(tmin_max_min_max);
+    auto tmin_max_min_max_y = rtm::vector_get_y(tmin_max_min_max);
+    auto tmin_max_min_max_z = rtm::vector_get_z(tmin_max_min_max);
+    auto tmin_max_min_max_w = rtm::vector_get_w(tmin_max_min_max);
+    auto tmin_max_min_max_min = std::min<float>(tmin_max_min_max_x, std::min<float>(tmin_max_min_max_y, std::min<float>(tmin_max_min_max_z, tmin_max_min_max_w)));
+    auto tmin_max_min_max_max = std::max<float>(tmin_max_min_max_x, std::max<float>(tmin_max_min_max_y, std::max<float>(tmin_max_min_max_z, tmin_max_min_max_w)));
+    if (tmin_max_min_max_min > 0.f && tmin_max_min_max_min < tmin_max_min_max_max)
+    {
+        // record->hit = true;
+        // record->distance = tmin_max_min_max_min;
+        // record->hit_point = skr::math::store(rtm::vector_mul_add(d, rtm::vector_set(tmin_max_min_max_min, tmin_max_min_max_min, tmin_max_min_max_min, tmin_max_min_max_min), o));
+        // record->hit_normal = skr::math::store(rtm::vector_normalize(rtm::vector_mul_add(d
+        //     , rtm::vector_set(tmin_max_min_max_max, tmin_max_min_max_max, tmin_max_min_max_max, tmin_max_min_max_max)
+        //     , rtm::vector_mul_add(d, rtm::vector_set(tmin_max_min_max_min, tmin_max_min_max_min, tmin_max_min_max_min, tmin_max_min_max_min), o))));
+        return true;
+    }
+    return false;
 }
 
 RenderBoxSizeType RenderBox::get_size() const
