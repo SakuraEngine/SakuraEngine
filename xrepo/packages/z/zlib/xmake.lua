aabdbdbe5ced25bbb8b6566ba9ee23145193ea36
SaeruHikari <39457738+SaeruHikari@users.noreply.github.com>
2023-04-18 22:03:26 +0800

    remove zlib copy in thirdparty dir

--- xrepo/packages/z/zlib/xmake.lua
+++ xrepo/packages/z/zlib/xmake.lua
@@ -5,18 +5,27 @@ package("zlib")
     
     add_versions("1.2.8-skr", "e3ed8a269df53e9a8530057f4949f48885f746ae0fe9d72e5b4c616833494dc2")
 
-    add_includedirs("zlib/include", {public=true})
-    if is_plat("windows")then
-        add_links("zlib/lib/windows/x64/zlibstatic", {public=true})
-    elseif is_plat("macosx")then
-        add_links("zlib/lib/macos/x86_64/z", {public=true})
-    else
-        print("not supported platform")
+    --[[
+    if (is_plat("windows")) then 
+        add_links("zlibstatic")
     end
+    if (is_plat("macosx")) then 
+        add_links("z")
+    end
+    ]]--
 
     on_install(function (package)
-        local zlib_dir = package:installdir(".")
-        os.cp(path.join(package:scriptdir(), "port", "zlib"), zlib_dir)
-        -- local configs = {}
-        -- import("package.tools.xmake").install(package, configs)
+        os.mkdir(package:installdir())
+        os.cp(path.join(package:scriptdir(), "port", "zlib"), ".")
+        os.cp(path.join(package:scriptdir(), "port", "xmake.lua"), "xmake.lua")
+
+        if (is_plat("windows")) then 
+            os.trycp("zlib/lib/windows/x64/*", package:installdir("lib"))
+        end
+        if (is_plat("macosx")) then 
+            os.trycp("zlib/lib/macos/x86_64/*", package:installdir("lib"))
+        end
+
+        local configs = {}
+        import("package.tools.xmake").install(package, configs)
     end)
