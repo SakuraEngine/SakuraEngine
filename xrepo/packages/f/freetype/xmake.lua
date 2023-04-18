ff0ddbb6204b168ed92008f6ff465b1e59efbf53
SaeruHikari <39457738+SaeruHikari@users.noreply.github.com>
2023-04-18 21:31:36 +0800

    update version string of xrepo libs

--- xrepo/packages/f/freetype/xmake.lua
+++ xrepo/packages/f/freetype/xmake.lua
@@ -3,7 +3,7 @@ package("freetype")
     set_description("FreeType is a freely available software library to render fonts.")
     set_license("The FreeType License (FTL) is a BSD-style license with a credit clause and thus compatible with the GNU Public License (GPL) version 3, but not with the GPL version 2.")
     
-    add_versions("skr", "0986ddbdaaac65da525716e7134038edcc7d8d1ca965629717231a89f32b839f")
+    add_versions("2.13.0-skr", "0986ddbdaaac65da525716e7134038edcc7d8d1ca965629717231a89f32b839f")
 
     on_install(function (package)
         os.mkdir(package:installdir())
