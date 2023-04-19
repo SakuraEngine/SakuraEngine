91960a21687198c9b3ed7e276dacc1ddd1501e77
gaymenhub <672619410@qq.com>
2023-04-19 12:38:02 +0800

    gg

--- modules/runtime/include/utils/bits.hpp
+++ modules/runtime/include/utils/bits.hpp
@@ -14,6 +14,15 @@ inline int CountLeadingZeros64Slow(uint64_t n) {
     return "\4\3\2\2\1\1\1\1\0\0\0\0\0\0\0"[n] + zeroes;
 }
 
+inline int CountTrailingZeros64Slow(uint64_t n) {
+    int zeroes = 0;
+    if ((n & 0xffffffff) == 0) zeroes += 32, n >>= 32;
+    if ((n & 0xffff) == 0) zeroes += 16, n >>= 16;
+    if ((n & 0xff) == 0) zeroes += 8, n >>= 8;
+    if ((n & 0xf) == 0) zeroes += 4, n >>= 4;
+    return "\0\1\0\2\0\1\0\3\0\1\0\2\0\1\0"[n] + zeroes;
+}
+
 inline int CountLeadingZeros64(uint64_t n)
 {
 #if defined(_MSC_VER) && defined(_M_X64)
@@ -52,6 +61,47 @@ inline int CountLeadingZeros64(uint64_t n)
     return __builtin_clzll(n);
 #else
     return CountLeadingZeros64Slow(n);
+#endif
+}
+
+inline int CountTrailingZeros64(uint64_t n)
+{
+#if defined(_MSC_VER) && defined(_M_X64)
+    // MSVC does not have __buitin_ctzll. Use _BitScanForward64.
+    unsigned long result = 0; // NOLINT(runtime/int)
+    if (_BitScanForward64(&result, n))
+    {
+        return (int)result;
+    }
+    return 64;
+#elif defined(_MSC_VER) && !defined(__clang__)
+    // MSVC does not have __buitin_ctzll. Compose two calls to _BitScanForward
+    unsigned long result = 0; // NOLINT(runtime/int)
+    if ((n & 0xffffffff) == 0 && _BitScanForward(&result, (unsigned long)(n >> 32)))
+    {
+        return 32 + result;
+    }
+    if (_BitScanForward(&result, (unsigned long)n))
+    {
+        return result;
+    }
+    return 0;
+#elif defined(__GNUC__) || defined(__clang__)
+    // Use __builtin_ctzll, which uses the following instructions:
+    //  x86: bsf
+    //  ARM64: rbit, clz
+    //  PPC: cntlzd
+    static_assert(sizeof(unsigned long long) == sizeof(n), // NOLINT(runtime/int)
+    "__builtin_ctzll does not take 64-bit arg");
+
+    // Handle 0 as a special case because __builtin_ctzll(0) is undefined.
+    if (n == 0)
+    {
+        return 0;
+    }
+    return __builtin_ctzll(n);
+#else
+    return CountTrailingZeros64Slow(n);
 #endif
 }
 } // namespace dual
