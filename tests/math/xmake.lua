151ec331680f13658e474ee16007f2c9d1307e23
SaeruHikari <39457738+SaeruHikari@users.noreply.github.com>
2023-04-24 18:27:51 +0800

    fix macosx compile error

--- tests/math/xmake.lua
+++ tests/math/xmake.lua
@@ -6,10 +6,12 @@ target("BaseTest")
     add_files("base/base.cpp")
     add_packages("gtest")
     add_packages("lua", {public = true, inherit = true})
-    add_syslinks("advapi32", "Shcore", "user32", "shell32", "Ole32", {public = true})
     add_deps("SkrDependencyGraph", {public = false})
-    add_links("$(buildir)/$(os)/$(arch)/$(mode)/nvapi_x64", {public = true})
-    add_links("$(buildir)/$(os)/$(arch)/$(mode)/WinPixEventRuntime", {public = true})
+    if is_plat("windows") then
+        add_syslinks("advapi32", "Shcore", "user32", "shell32", "Ole32", {public = true})
+        add_links("$(buildir)/$(os)/$(arch)/$(mode)/nvapi_x64", {public = true})
+        add_links("$(buildir)/$(os)/$(arch)/$(mode)/WinPixEventRuntime", {public = true})
+    end
     add_links(links_list, {public = true})
     add_packages(packages_list, {public = true})
 
