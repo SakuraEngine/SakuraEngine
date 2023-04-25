556899ddb1f4b9fd279bbd799016bc584829b90b
SaeruHikari <39457738+SaeruHikari@users.noreply.github.com>
2023-04-25 10:57:30 +0800

    try fix macosx compile error

--- xrepo/packages/d/daScriptTool/xmake.lua
+++ xrepo/packages/d/daScriptTool/xmake.lua
@@ -18,9 +18,10 @@ package("daScriptTool")
         os.cp(path.join(package:scriptdir(), "port", "xmake.lua"), "xmake.lua")
         os.cp(path.join(package:scriptdir(), "port", "da.daS"), package:installdir())
 
+        os.mkdir("bin")
         local configs = {}
         configs.plat = os.host()
-        -- configs.arch = os.arch()
+        configs.arch = os.arch()
         import("package.tools.xmake").install(package, configs)
 
         if is_host("windows") then
