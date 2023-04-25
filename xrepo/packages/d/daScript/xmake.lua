87b69d74b41a588689279f28244dc1920f39c9a3
SaeruHikari <39457738+SaeruHikari@users.noreply.github.com>
2023-04-25 18:32:26 +0800

    daScript 添加多种目标类型

--- xrepo/packages/d/daScript/xmake.lua
+++ xrepo/packages/d/daScript/xmake.lua
@@ -2,10 +2,10 @@ package("daScript")
     set_homepage("https://dascript.org/")
     set_description("daScript - high-performance statically strong typed scripting language")
 
-    add_versions("2023.4.25-skr.9", "64bf3c12081a1324ac29a7cb104598e3e0fe628b574f30143c1038f66e26f21c")
+    add_versions("2023.4.25-skr.10", "64bf3c12081a1324ac29a7cb104598e3e0fe628b574f30143c1038f66e26f21c")
 
-    add_deps("daScriptCore 2023.4.25-skr.9")
-    add_deps("daScriptTool 2023.4.25-skr.9")
+    add_deps("daScriptCore 2023.4.25-skr.10")
+    add_deps("daScriptTool 2023.4.25-skr.10")
     on_install(function (package)
         os.mkdir(package:installdir())
 
@@ -31,15 +31,17 @@ package("daScript")
             )
         end
 
-        os.mkdir(package:installdir("rules"))
+        os.cp(path.join(package:scriptdir(), "rules"), ".")
+        os.cp(path.join(package:scriptdir(), "modules"), ".")
         os.cp(path.join(package:scriptdir(), "port", "daScript", "daslib"), ".")
         os.cp(path.join(package:scriptdir(), "port", "daScript", "dastest"), ".")
-        os.cp(path.join(package:scriptdir(), "port", "xmake.lua"), "xmake.lua")
-        os.cp(path.join(package:scriptdir(), "port", "da.daS"), package:installdir())
-        os.cp(path.join(package:scriptdir(), "rules", "AOT.lua"), "rules/AOT.lua")
-
+        
+        os.cp(path.join(package:scriptdir(), "modules"), package:installdir())
         os.cp(path.join(package:scriptdir(), "port", "daScript", "daslib"), package:installdir())
         os.cp(path.join(package:scriptdir(), "port", "daScript", "dastest"), package:installdir())
+
+        os.cp(path.join(package:scriptdir(), "port", "xmake.lua"), "xmake.lua")
+        os.cp(path.join(package:scriptdir(), "port", "da.daS"), package:installdir())
 
         local configs = {}
         import("package.tools.xmake").install(package, configs)
