dbba294ae26974a1e90a1bf07abb2e6a9adb2e97
SaeruHikari <3045349520@qq.com>
2023-05-05 22:44:06 +0800

    one triangle optimize & bug fixes for RG deferred demo

--- samples/render_graph/rg-deferred/shaders/screen_vs.vs_6_0.hlsl
+++ samples/render_graph/rg-deferred/shaders/screen_vs.vs_6_0.hlsl
@@ -8,24 +8,24 @@ struct VSOut
     float2 uv : TEXCOORD0;
 };
 
-static const float2 uvs[6] = {
-    float2(1.0, 1.0),
-    float2(0.0, 0.0),
-    float2(1.0, 0.0),
-
-    float2(1.0, 1.0),
-    float2(0.0, 1.0),
-    float2(0.0, 0.0)
+static const float2 uvs[3] = {
+    float2(2.f, 1.f),
+    float2(0.f, -1.f),
+    float2(0.f, 1.f)
 };
 
+static const float2 poses[3] = {
+    float2(3.f, -1.f),
+    float2(-1.f, 3.f),
+    float2(-1.f, -1.f)
+};
 
 VSOut main(in uint VertexIndex : SV_VertexID, out float4 position : SV_POSITION)
 {
     VSOut vout;
 	vout.uv = uvs[VertexIndex];
-    vout.uv.y = 1.f - vout.uv.y;
 
-	position = float4(vout.uv.x * 2.f - 1.f, vout.uv.y * 2.f - 1.f, QUAD_Z, 1.f);
+	position = float4(poses[VertexIndex], QUAD_Z, 1.f);
 
     return vout;
 }
