b948bee2d6e8dda1962c39443b56ed3f5a285bd3
SaeruHikari <3045349520@qq.com>
2023-05-28 18:30:29 +0800

    Revert "add mimalloc to xrepo"
    This reverts commit 79ed4f6d9ba34d080887182ecac336a3ae29a879.

--- xmake/thirdparty/mimalloc.lua
+++ xmake/thirdparty/mimalloc.lua
@@ -1,11 +1,12 @@
+mimalloc_includes_dir = "$(projectdir)/thirdparty/mimalloc"
+
 target("mimalloc")
     -- version("0.9.2alpha")
     set_group("00.thirdparty")
     set_kind("static")
     set_optimize("fastest")
-    add_files("mimalloc/build.mimalloc.c")
-    add_headerfiles("mimalloc/*.h")
-    add_includedirs("mimalloc", {public = true})
+    add_files("$(projectdir)/thirdparty/mimalloc/build.mimalloc.c")
+    add_includedirs(mimalloc_includes_dir, {public = true})
     if (is_os("windows")) then 
         add_syslinks("advapi32", "user32", {public = true})
     end
