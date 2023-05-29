41f1a516da300986afe045fc83387516226acb42
zhurong <211103376@qq.com>
2023-05-29 15:00:32 +0800

    Element API 整理

--- modules/gui/gui/include/SkrGui/framework/element.hpp
+++ modules/gui/gui/include/SkrGui/framework/element.hpp
@@ -32,31 +32,32 @@ struct SKR_GUI_API Element : public BuildContext
 {
     Element(skr_gui_widget_id widget) SKR_NOEXCEPT;
 
-    // element interfaces
-    
+    // life circle
     virtual void activate() SKR_NOEXCEPT;
     virtual void deactivate() SKR_NOEXCEPT;
 
+    // element tree modify
     virtual void mount(Element* parent, Slot* slot) SKR_NOEXCEPT;
     virtual void unmount() SKR_NOEXCEPT;
 
+    // render object tree modify
     virtual void attach_render_object(Slot* new_slot) SKR_NOEXCEPT;
     virtual void detach_render_object() SKR_NOEXCEPT;
 
+    // widget update
     virtual not_null<Element*> inflate_widget(not_null<Widget*> widget, Slot* new_slot) SKR_NOEXCEPT;
-
     virtual void update(Widget* new_widget) SKR_NOEXCEPT;
     virtual void update_slot_for_child(Element* child, Slot* new_slot) SKR_NOEXCEPT;
     virtual Element* update_child(Element* child, Widget* new_widget, Slot* new_slot) SKR_NOEXCEPT;
+    virtual void perform_rebuild() SKR_NOEXCEPT;
+    virtual void rebuild(bool force = false) SKR_NOEXCEPT;
+    void _update_slot(Slot* new_slot) SKR_NOEXCEPT;
+
+    // element tree query
     virtual void visit_children(skr::function_ref<void(Element*)> visitor) SKR_NOEXCEPT;
     virtual void visit_child_elements(skr::function_ref<void(Element*)> visitor) SKR_NOEXCEPT;
     virtual void forget_child(Element* child) SKR_NOEXCEPT;
     virtual void deactivate_child(Element* child) SKR_NOEXCEPT;
-
-    virtual void perform_rebuild() SKR_NOEXCEPT;
-    virtual void rebuild(bool force = false) SKR_NOEXCEPT;
-
-    void _update_slot(Slot* new_slot) SKR_NOEXCEPT;
 
     // build context interfaces
     virtual bool mounted() SKR_NOEXCEPT override;
@@ -64,6 +65,8 @@ struct SKR_GUI_API Element : public BuildContext
     virtual BoxSizeType get_size() SKR_NOEXCEPT override;
     virtual RenderObject* find_render_object() SKR_NOEXCEPT override;
 
+private:
+    // help functions
     Element* _retake_inactive_element(Key& key, not_null<Widget*> widget) SKR_NOEXCEPT;
     void _active_with_parent(Element* parent, Slot* slot) SKR_NOEXCEPT;
     static void _active_recursively(Element* element) SKR_NOEXCEPT;
@@ -71,7 +74,7 @@ struct SKR_GUI_API Element : public BuildContext
     static int _compare_depth(Element* a, Element* b) SKR_NOEXCEPT;
     bool _debug_is_in_scope(Element* ancestor) SKR_NOEXCEPT;
 
-
+private:
     uint32_t _depth = 0;
     bool _dirty = true;
     bool _in_dirty_list = false;
