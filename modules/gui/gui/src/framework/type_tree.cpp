28d8b24bce403fe2fe3ef027fe8be038a20ce59b
BenzzZX <672619410@qq.com>
2023-06-05 23:01:42 +0800

    fix OGUI RObjectDemo

--- modules/gui/gui/src/framework/type_tree.cpp
+++ modules/gui/gui/src/framework/type_tree.cpp
@@ -17,17 +17,11 @@ TypeTreeNode::~TypeTreeNode() SKR_NOEXCEPT
 
 void TypeTreeNode::create_dynamic_type(skr_guid_t id, skr_record_type_id parent_type, const char8_t* name) SKR_NOEXCEPT
 {
-    // if (parent_type)
-    // {
-    //     skr_guid_t parent_id = {};
-    //     skr_get_type_id((skr_type_t*)parent_type, &parent_id);
-    //     type = skr_create_record_type(&id, size, align, &parent_id);
-    // }
-    // else
-    // {
-    //     type = skr_create_record_type(&id, size, align, nullptr);
-    // }
-    // if (name) skr_record_type_set_name(type, name);
+    type = skr::type::GetTypeRegistry()->register_record(id);
+    new (type) skr::type::RecordType{
+        size, align, name, id, true, 
+        parent_type, {}, {}, {}
+    };
 }
 
 struct TypeTreeImpl : public TypeTree
--- modules/gui/gui/src/objects/render_stack.cpp
+++ modules/gui/gui/src/objects/render_stack.cpp
@@ -1,4 +1,5 @@
 #include "SkrGui/render_objects/render_stack.hpp"
+#include "misc/log.h"
 
 namespace skr
 {
@@ -27,6 +28,10 @@ void RenderStack::layout(BoxConstraint constraints, bool needSize)
         childConstraints.max_size = skr_float2_t{ std::numeric_limits<float>::infinity(), std::numeric_limits<float>::infinity()};
         if (positional.left && positional.right)
         {
+            if(positional.width)
+            {
+                SKR_LOG_WARN("Both left and right are set, width will be ignored");
+            }
             childConstraints.min_size.x = childConstraints.max_size.x =
             width - positional.left.get_value(width) - positional.right.get_value(width);
         }
@@ -36,6 +41,10 @@ void RenderStack::layout(BoxConstraint constraints, bool needSize)
         }
         if (positional.top && positional.bottom)
         {
+            if (positional.height)
+            {
+                SKR_LOG_WARN("Both top and bottom are set, height will be ignored");
+            }
             childConstraints.min_size.y = childConstraints.max_size.y =
             height - positional.top.get_value(height) - positional.bottom.get_value(height);
         }
@@ -58,6 +67,7 @@ void RenderStack::layout(BoxConstraint constraints, bool needSize)
         }
         else
         {
+            SKR_LOG_WARN("Both left and right are not set, default to left 0");
             childPosition.x = - child->get_size().x * positional.pivot.x;
         }
         if (positional.top)
@@ -72,6 +82,7 @@ void RenderStack::layout(BoxConstraint constraints, bool needSize)
         }
         else
         {
+            SKR_LOG_WARN("Both top and bottom are not set, default to top 0");
             childPosition.y = -child->get_size().y * positional.pivot.y;
         }
         child->set_position(childPosition);
