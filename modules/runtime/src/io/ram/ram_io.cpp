2e78ca3ec2fea15f7de602c6f3eade83a635cd99
SaeruHikari <39457738+SaeruHikari@users.noreply.github.com>
2023-06-05 23:16:36 +0800

    Update ram_io.cpp

--- modules/runtime/src/io/ram/ram_io.cpp
+++ modules/runtime/src/io/ram/ram_io.cpp
@@ -227,7 +227,6 @@ skr::AsyncResult RAMService::Runner::serve() SKR_NOEXCEPT
     SKR_DEFER( { ZoneScopedNC("Finish", tracy::Color::Tan1); finish(); recycle(); } );
 
     uint64_t cnt;
-    const auto queued = getQueuedBatchCount();
     {
         ZoneScopedNC("Resolve", tracy::Color::Orchid1);
 
@@ -243,7 +242,7 @@ skr::AsyncResult RAMService::Runner::serve() SKR_NOEXCEPT
         uncompress();
         return ASYNC_RESULT_OK;
     }
-    else if (!queued)
+    else if (!getQueuedBatchCount())
     {
         setServiceStatus(SKR_ASYNC_SERVICE_STATUS_SLEEPING);
         sleep();
