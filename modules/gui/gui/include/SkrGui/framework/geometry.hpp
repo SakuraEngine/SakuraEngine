d673ceb38084637455a5ec5d4dc06ec99f437047
zhurong <211103376@qq.com>
2023-06-06 20:03:38 +0800

    新建文件夹

---
+++ modules/gui/gui/include/SkrGui/framework/geometry.hpp
@@ -0,0 +1,14 @@
+#pragma once
+#include "misc/types.h"
+#include <cinttypes>
+#include <cstddef>
+#include <limits>
+
+namespace skr::gui
+{
+// TODO. Offset class
+using Offset = skr_float2_t;
+// TODO. Size class
+using Size = skr_float2_t;
+// TODO. Rect class
+}
