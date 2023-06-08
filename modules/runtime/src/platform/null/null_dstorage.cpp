b21d2daf6fe1cecf4057175161da5ae6b05f3815
SaeruHikari <3045349520@qq.com>
2023-06-09 01:10:39 +0800

    fix ostring allocation error (across DLLs) & macos linking error

--- modules/runtime/src/platform/null/null_dstorage.cpp
+++ modules/runtime/src/platform/null/null_dstorage.cpp
@@ -1,49 +1,54 @@
 #include "platform/dstorage.h"
 
-RUNTIME_API SkrDStorageInstanceId skr_create_dstorage_instance(SkrDStorageConfig* config)
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+SkrDStorageInstanceId skr_create_dstorage_instance(SkrDStorageConfig* config)
 {
     return nullptr;
 }
 
-RUNTIME_API SkrDStorageInstanceId skr_get_dstorage_instnace()
+SkrDStorageInstanceId skr_get_dstorage_instnace()
 {
     return nullptr;
 }
 
-RUNTIME_API ESkrDStorageAvailability skr_query_dstorage_availability()
+ESkrDStorageAvailability skr_query_dstorage_availability()
 {
     return SKR_DSTORAGE_AVAILABILITY_NONE;
 }
 
-RUNTIME_API void skr_free_dstorage_instance(SkrDStorageInstanceId inst)
+void skr_free_dstorage_instance(SkrDStorageInstanceId inst)
 {
 
 }
 
-RUNTIME_API SkrDStorageQueueId skr_create_dstorage_queue(const SkrDStorageQueueDescriptor* desc)
-{
-    return nullptr;
-}
-
-RUNTIME_API void skr_free_dstorage_queue(SkrDStorageQueueId queue)
-{
-
-}
-
-RUNTIME_API SkrDStorageFileHandle skr_dstorage_open_file(SkrDStorageQueueId queue, const char* abs_path)
+SkrDStorageQueueId skr_create_dstorage_queue(const SkrDStorageQueueDescriptor* desc)
 {
     return nullptr;
 }
 
-RUNTIME_API void skr_dstorage_query_file_info(SkrDStorageQueueId queue, SkrDStorageFileHandle file, SkrDStorageFileInfo* info)
+void skr_free_dstorage_queue(SkrDStorageQueueId queue)
 {
 
 }
 
-RUNTIME_API void skr_dstorage_close_file(SkrDStorageQueueId queue, SkrDStorageFileHandle file)
+SkrDStorageFileHandle skr_dstorage_open_file(SkrDStorageInstanceId instance, const char* abs_path)
+{
+    return nullptr;
+}
+
+void skr_dstorage_query_file_info(SkrDStorageInstanceId instance, SkrDStorageFileHandle file, SkrDStorageFileInfo* info)
 {
 
 }
+
+void skr_dstorage_close_file(SkrDStorageInstanceId instance, SkrDStorageFileHandle file)
+{
+
+}
+
 SkrDStorageEventId skr_dstorage_queue_create_event(SkrDStorageQueueId queue)
 {
     return nullptr;
@@ -74,4 +79,8 @@ void skr_dstorage_queue_trace_submit(SkrDStorageQueueId queue)
 {
 
 }
+#endif
+
+#ifdef __cplusplus
+} // extern "C"
 #endif
