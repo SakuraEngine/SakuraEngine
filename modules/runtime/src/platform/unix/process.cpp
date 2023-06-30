20e16a037955f7be2d208ddad3493fbf253d308f
SaeruHikari <39457738+SaeruHikari@users.noreply.github.com>
2023-06-30 17:34:40 +0800

    unix pid

--- modules/runtime/src/platform/unix/process.cpp
+++ modules/runtime/src/platform/unix/process.cpp
@@ -1,18 +1,78 @@
 #include "platform/debug.h"
+#include "platform/memory.h"
 #include "platform/process.h"
-#include <EASTL/string.h>
+
+#include "containers/string.hpp"
+#include <EASTL/vector.h>
 #include <unistd.h> 
+
+typedef struct SProcess
+{
+    pid_t pid;
+} SProcess;
 
 SProcessHandle skr_run_process(const char8_t* command, const char8_t** arguments, uint32_t arg_count, const char8_t* stdout_file)
 {
-    SKR_UNIMPLEMENTED_FUNCTION();
-    return nullptr;
+	eastl::vector<const char8_t*> argPtrs;
+	skr::string Args;
+	for (size_t i = 0; i < arg_count; ++i)
+	{
+		Args += skr::string(arguments[i]);
+        Args += u8" ";
+	}
+
+	// int errcode = system(cmd.c_str()); (void)errcode;
+	extern char ** environ;	// provided by libc
+	pid_t ChildPid = -1;
+
+	posix_spawnattr_t SpawnAttr;
+	posix_spawnattr_init(&SpawnAttr);
+	short int SpawnFlags = 0;
+
+	// unmask all signals and set realtime signals to default for children
+	// the latter is particularly important for mono, which otherwise will crash attempting to find usable signals
+	// (NOTE: setting all signals to default fails)
+	sigset_t EmptySignalSet;
+	sigemptyset(&EmptySignalSet);
+	posix_spawnattr_setsigmask(&SpawnAttr, &EmptySignalSet);
+	SpawnFlags |= POSIX_SPAWN_SETSIGMASK;
+
+	sigset_t SetToDefaultSignalSet;
+	sigemptyset(&SetToDefaultSignalSet);
+	for (int SigNum = SIGRTMIN; SigNum <= SIGRTMAX; ++SigNum)
+	{
+		sigaddset(&SetToDefaultSignalSet, SigNum);
+	}
+	posix_spawnattr_setsigdefault(&SpawnAttr, &SetToDefaultSignalSet);
+	SpawnFlags |= POSIX_SPAWN_SETSIGDEF;
+
+	// Makes spawned processes have its own unique group id so we can kill the entire group with out killing the parent
+	SpawnFlags |= POSIX_SPAWN_SETPGROUP;
+
+	int PosixSpawnErrNo = -1;
+    {
+		// if we don't have any actions to do, use a faster route that will use vfork() instead.
+		// This is not just faster, it is crucial when spawning a crash reporter to report a crash due to stack overflow in a thread
+		// since otherwise atfork handlers will get called and posix_spawn() will crash (in glibc's __reclaim_stacks()).
+		// However, it has its problems, see:
+		//		http://ewontfix.com/7/
+		//		https://sourceware.org/bugzilla/show_bug.cgi?id=14750
+		//		https://sourceware.org/bugzilla/show_bug.cgi?id=14749
+		SpawnFlags |= POSIX_SPAWN_USEVFORK;
+
+		posix_spawnattr_setflags(&SpawnAttr, SpawnFlags);
+		PosixSpawnErrNo = posix_spawn(&ChildPid, command, nullptr, &SpawnAttr, Args.c_str(), environ);
+	}
+	posix_spawnattr_destroy(&SpawnAttr);
+    
+    SProcessHandle result = SkrNew<SProcess>();
+    result->pid = ChildPid;
+    return result;
 }
 
 SProcessId skr_get_current_process_id()
 {
-    SKR_UNIMPLEMENTED_FUNCTION();
-    return -1;
+    return getpid();
 }
 
 int skr_wait_process(SProcessHandle process)
@@ -23,6 +83,5 @@ int skr_wait_process(SProcessHandle process)
 
 SProcessId skr_get_process_id(SProcessHandle process)
 {
-    SKR_UNIMPLEMENTED_FUNCTION();
-    return -1;
+    return process->pid;
 }
