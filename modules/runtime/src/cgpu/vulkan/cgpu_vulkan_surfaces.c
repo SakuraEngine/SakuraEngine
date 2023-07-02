04b024f8b559b099ecd1e8e8557e3ea697147934
SaeruHikari <3045349520@qq.com>
2023-07-02 21:54:43 +0800

    remove pch.h

--- modules/runtime/src/cgpu/vulkan/cgpu_vulkan_surfaces.c
+++ modules/runtime/src/cgpu/vulkan/cgpu_vulkan_surfaces.c
@@ -1,4 +1,6 @@
+#ifdef _WIN32
 #include "../../platform/windows/winheaders.h"
+#endif
 #include "cgpu/backend/vulkan/cgpu_vulkan_surfaces.h"
 #include "cgpu/backend/vulkan/cgpu_vulkan.h"
 #include "cgpu/extensions/cgpu_vulkan_exts.h"
