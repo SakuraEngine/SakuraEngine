53cfdbf1651924850eb4f276d1ff34368aa3ab40
SaeruHikari <3045349520@qq.com>
2023-07-03 01:44:39 +0800

    add tids_ & improve query_flushing

--- modules/runtime/src/misc/log/log_queue.hpp
+++ modules/runtime/src/misc/log/log_queue.hpp
@@ -6,6 +6,7 @@
 #include "misc/log/log_formatter.hpp"
 
 #include "containers/concurrent_queue.h"
+#include "containers/vector.hpp"
 #include <EASTL/unique_ptr.h>
 
 namespace skr {
@@ -23,17 +24,18 @@ enum EFlushStatus
 struct ThreadToken
 {
     ThreadToken(LogQueue& q) SKR_NOEXCEPT;
-
+    int64_t query_cnt() const SKR_NOEXCEPT;
+    EFlushStatus query_status() const SKR_NOEXCEPT;
+protected:
+    friend struct LogWorker;
+    friend struct LogQueue;
     SAtomic64 tls_cnt_ = 0;
-    skr::ProducerToken ptok_;
     SAtomic32 flush_status_ = kNoFlush;
+    skr::ProducerToken ptok_;
 };
 
 struct LogElement
 {
-public:
-    void dec() SKR_NOEXCEPT;
-
 private:
     LogElement(LogEvent ev, ThreadToken* ptok) SKR_NOEXCEPT;
     LogElement() SKR_NOEXCEPT;
@@ -52,6 +54,7 @@ struct LogQueue
 {
 public:
     LogQueue() SKR_NOEXCEPT;
+    ~LogQueue() SKR_NOEXCEPT;
 
     void push(LogEvent ev, const skr::string&& what) SKR_NOEXCEPT;
     void push(LogEvent ev, const skr::string_view format, ArgsList&& args) SKR_NOEXCEPT;
@@ -59,20 +62,21 @@ public:
     
     bool try_dequeue(LogElement& element) SKR_NOEXCEPT;
     bool try_dequeue_from(ThreadToken* tok, LogElement& element) SKR_NOEXCEPT;
+    void finish(const LogElement& element) SKR_NOEXCEPT;
 
     ThreadToken* query_token(SThreadID tid) const SKR_NOEXCEPT;
-    ThreadToken* query_flusing() const SKR_NOEXCEPT;
-    int64_t query_cnt(SThreadID tid) const SKR_NOEXCEPT;
+    ThreadToken* query_flushing() const SKR_NOEXCEPT;
     int64_t query_cnt() const SKR_NOEXCEPT;
 
 private:
     [[nodiscard]] ThreadToken* on_push(const LogEvent& ev) SKR_NOEXCEPT;
 
     // formatter & args
+    template<int BLK_SIZE = 256>
     struct LogQueueTraits : public ConcurrentQueueDefaultTraits
     {
         static const bool RECYCLE_ALLOCATED_BLOCKS = true;
-        static const int BLOCK_SIZE = 256;
+        static const int BLOCK_SIZE = BLK_SIZE;
 
         static inline void* malloc(size_t size) SKR_NOEXCEPT { return sakura_mallocN(size, kLogMemoryName); }
         static inline void free(void* ptr) SKR_NOEXCEPT { return sakura_freeN(ptr, kLogMemoryName); }
@@ -83,7 +87,11 @@ private:
     // MPSC
     skr::ConsumerToken ctok_;
     skr::parallel_flat_hash_map<uint64_t, eastl::unique_ptr<ThreadToken>> thread_id_map_;
-    skr::ConcurrentQueue<LogElement, LogQueueTraits> queue_;
+    skr::ConcurrentQueue<LogElement, LogQueueTraits<256>> queue_;
+
+    skr::vector<uint64_t> tids_;
+    mutable SRWMutex tids_mutex_;
+    mutable skr::vector<uint64_t> tids_cpy_;
 };
 
 } } // namespace skr::log
