6ea6a1deec4feca956233f8495dffbe956a3d9a2
SaeruHikari <3045349520@qq.com>
2023-07-21 12:48:51 +0800

    try fix codegen missing error

--- modules/animation/ozz_src/pch.hpp
+++ modules/animation/ozz_src/pch.hpp
@@ -6,6 +6,7 @@
 #include "SkrRT/containers/sptr.hpp" // IWYU pragma: export
 
 #include "SkrAnim/ozz/geometry/skinning_job.h" // IWYU pragma: export
+#include "SkrAnim/ozz/base/log.h" // IWYU pragma: export
 #include "SkrAnim/ozz/base/span.h" // IWYU pragma: export
 #include "SkrAnim/ozz/base/maths/simd_math.h" // IWYU pragma: export
 
--- modules/animation/src/pch.hpp
+++ modules/animation/src/pch.hpp
@@ -8,6 +8,7 @@
 #include <SkrRT/containers/vector.hpp> // IWYU pragma: export
 
 #include "SkrAnim/ozz/geometry/skinning_job.h" // IWYU pragma: export
+#include "SkrAnim/ozz/base/log.h" // IWYU pragma: export
 #include "SkrAnim/ozz/base/span.h" // IWYU pragma: export
 #include "SkrAnim/ozz/base/maths/simd_math.h" // IWYU pragma: export
 
