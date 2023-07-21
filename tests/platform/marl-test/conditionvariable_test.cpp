4270fdb54e75ec5f74bb3dcb20c36ea49f47822b
SaeruHikari <3045349520@qq.com>
2023-07-22 01:33:06 +0800

    减少 marl 测试用例中夹具初始化次数

--- tests/platform/marl-test/conditionvariable_test.cpp
+++ tests/platform/marl-test/conditionvariable_test.cpp
@@ -63,80 +63,82 @@ TEST_CASE_METHOD(WithoutBoundScheduler, "ConditionVariable") {
 }
 
 TEST_CASE_METHOD(WithBoundScheduler, "ConditionVariable") {
-  bool trigger[3] = {false, false, false};
-  bool signal[3] = {false, false, false};
-  marl::mutex mutex;
-  marl::ConditionVariable cv;
-
-  std::thread thread([&] {
-    for (int i = 0; i < 3; i++) {
-      marl::lock lock(mutex);
-      cv.wait(lock, [&] {
-        EXPECT_TRUE(lock.owns_lock());
-        return trigger[i];
-      });
-      EXPECT_TRUE(lock.owns_lock());
-      signal[i] = true;
-      cv.notify_one();
-    }
-  });
-
-  ASSERT_FALSE(signal[0]);
-  ASSERT_FALSE(signal[1]);
-  ASSERT_FALSE(signal[2]);
-
-  for (int i = 0; i < 3; i++) {
-    {
-      marl::lock lock(mutex);
-      trigger[i] = true;
-      cv.notify_one();
-      cv.wait(lock, [&] {
-        EXPECT_TRUE(lock.owns_lock());
-        return signal[i];
-      });
-      EXPECT_TRUE(lock.owns_lock());
-    }
-
-    ASSERT_EQ(signal[0], 0 <= i);
-    ASSERT_EQ(signal[1], 1 <= i);
-    ASSERT_EQ(signal[2], 2 <= i);
-  }
-
-  thread.join();
-}
-
-// ConditionVariableTimeouts spins up a whole lot of wait_fors(), unblocking
-// some with timeouts and some with a notify, and then let's all the workers
-// go to idle before repeating.
-// This is testing to ensure that the scheduler handles timeouts correctly when
-// they are early-unblocked, along with expected lock state.
-TEST_CASE_METHOD(WithBoundScheduler, "ConditionVariableTimeouts") {
-  for (int i = 0; i < 10; i++) {
+  SECTION("ConditionVariable") {
+    bool trigger[3] = {false, false, false};
+    bool signal[3] = {false, false, false};
     marl::mutex mutex;
     marl::ConditionVariable cv;
-    bool signaled = false;  // guarded by mutex
-    auto wg = marl::WaitGroup(100);
-    for (int j = 0; j < 100; j++) {
-      marl::schedule([=, &mutex, &cv, &signaled] {
-        {
-          marl::lock lock(mutex);
-          cv.wait_for(lock, std::chrono::milliseconds(j), [&] {
-            EXPECT_TRUE(lock.owns_lock());
-            return signaled;
-          });
+
+    std::thread thread([&] {
+      for (int i = 0; i < 3; i++) {
+        marl::lock lock(mutex);
+        cv.wait(lock, [&] {
           EXPECT_TRUE(lock.owns_lock());
-        }
-        // Ensure the mutex unlock happens *before* the wg.done() call,
-        // otherwise the stack pointer may no longer be valid.
-        wg.done();
-      });
+          return trigger[i];
+        });
+        EXPECT_TRUE(lock.owns_lock());
+        signal[i] = true;
+        cv.notify_one();
+      }
+    });
+
+    ASSERT_FALSE(signal[0]);
+    ASSERT_FALSE(signal[1]);
+    ASSERT_FALSE(signal[2]);
+
+    for (int i = 0; i < 3; i++) {
+      {
+        marl::lock lock(mutex);
+        trigger[i] = true;
+        cv.notify_one();
+        cv.wait(lock, [&] {
+          EXPECT_TRUE(lock.owns_lock());
+          return signal[i];
+        });
+        EXPECT_TRUE(lock.owns_lock());
+      }
+
+      ASSERT_EQ(signal[0], 0 <= i);
+      ASSERT_EQ(signal[1], 1 <= i);
+      ASSERT_EQ(signal[2], 2 <= i);
     }
-    std::this_thread::sleep_for(std::chrono::milliseconds(50));
-    {
-      marl::lock lock(mutex);
-      signaled = true;
-      cv.notify_all();
+
+    thread.join();
+  }
+
+  // ConditionVariableTimeouts spins up a whole lot of wait_fors(), unblocking
+  // some with timeouts and some with a notify, and then let's all the workers
+  // go to idle before repeating.
+  // This is testing to ensure that the scheduler handles timeouts correctly when
+  // they are early-unblocked, along with expected lock state.
+  SECTION("ConditionVariableTimeouts") {
+    for (int i = 0; i < 10; i++) {
+      marl::mutex mutex;
+      marl::ConditionVariable cv;
+      bool signaled = false;  // guarded by mutex
+      auto wg = marl::WaitGroup(100);
+      for (int j = 0; j < 100; j++) {
+        marl::schedule([=, &mutex, &cv, &signaled] {
+          {
+            marl::lock lock(mutex);
+            cv.wait_for(lock, std::chrono::milliseconds(j), [&] {
+              EXPECT_TRUE(lock.owns_lock());
+              return signaled;
+            });
+            EXPECT_TRUE(lock.owns_lock());
+          }
+          // Ensure the mutex unlock happens *before* the wg.done() call,
+          // otherwise the stack pointer may no longer be valid.
+          wg.done();
+        });
+      }
+      std::this_thread::sleep_for(std::chrono::milliseconds(50));
+      {
+        marl::lock lock(mutex);
+        signaled = true;
+        cv.notify_all();
+      }
+      wg.wait();
     }
-    wg.wait();
   }
 }
