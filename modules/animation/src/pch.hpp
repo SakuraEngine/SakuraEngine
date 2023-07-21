2db3f5256d7c62c4abdc689c083497223754d897
SaeruHikari <3045349520@qq.com>
2023-07-21 12:44:15 +0800

    fix compile error

--- modules/animation/src/pch.hpp
+++ modules/animation/src/pch.hpp
@@ -4,11 +4,11 @@
 #include "SkrRT/ecs/dual.h" // IWYU pragma: export
 #include "SkrRT/resource/resource_factory.h" // IWYU pragma: export
 #include "SkrRT/containers/sptr.hpp" // IWYU pragma: export
+#include <SkrRT/containers/string.hpp> // IWYU pragma: export
+#include <SkrRT/containers/vector.hpp> // IWYU pragma: export
 
 #include "SkrAnim/ozz/geometry/skinning_job.h" // IWYU pragma: export
 #include "SkrAnim/ozz/base/span.h" // IWYU pragma: export
 #include "SkrAnim/ozz/base/maths/simd_math.h" // IWYU pragma: export
-
-#include "SkrRenderer/render_mesh.h" // IWYU pragma: export
 
 #include "tracy/Tracy.hpp" // IWYU pragma: export
