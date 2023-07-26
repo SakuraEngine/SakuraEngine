d4931bca1b84a8ad0878c4f362e3ae8b5c03e9ad
SaeruHikari <39457738+SaeruHikari@users.noreply.github.com>
2023-07-26 20:56:16 +0800

    升级 tinygltf 和 json

--- tools/animation_tool/src/gltf/tiny_gltf.h
+++ tools/animation_tool/src/gltf/tiny_gltf.h
@@ -3,3 +3,27 @@
 #define TINYGLTF_NO_STB_IMAGE_WRITE
 #define TINYGLTF_NO_EXTERNAL_IMAGE
 #include "extern/tiny_gltf.h"
+
+namespace tinygltf
+{
+static inline int32_t GetTypeSizeInBytes(uint32_t ty) {
+  if (ty == TINYGLTF_TYPE_SCALAR) {
+    return 1;
+  } else if (ty == TINYGLTF_TYPE_VEC2) {
+    return 2;
+  } else if (ty == TINYGLTF_TYPE_VEC3) {
+    return 3;
+  } else if (ty == TINYGLTF_TYPE_VEC4) {
+    return 4;
+  } else if (ty == TINYGLTF_TYPE_MAT2) {
+    return 4;
+  } else if (ty == TINYGLTF_TYPE_MAT3) {
+    return 9;
+  } else if (ty == TINYGLTF_TYPE_MAT4) {
+    return 16;
+  } else {
+    // Unknown componenty type
+    return -1;
+  }
+}
+}
