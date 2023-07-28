a0497f5fc9e860210d3c0ee0f844f4d97a36ce2e
SaeruHikari <39457738+SaeruHikari@users.noreply.github.com>
2023-07-28 17:00:50 +0800

    独立 SkrBase 目标

---
+++ modules/base/include/SkrBase/tools.h
@@ -0,0 +1,10 @@
+#pragma once
+#include "SkrBase/tools/assert.h"
+#include "SkrBase/tools/debugbreak.h"
+
+#ifdef __cplusplus
+#include "SkrBase/tools/bit.hpp"
+#include "SkrBase/tools/integer_tools.hpp"
+#endif
+
+#define SKR_DEBUG_BREAK() debug_break()
