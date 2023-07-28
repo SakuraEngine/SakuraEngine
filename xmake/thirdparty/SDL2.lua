f89a234d90227f252738d07a4c8506df0688c995
SaeruHikari <39457738+SaeruHikari@users.noreply.github.com>
2023-07-28 17:17:41 +0800

    移除构建脚本中的一些全局列表

--- xmake/thirdparty/SDL2.lua
+++ xmake/thirdparty/SDL2.lua
@@ -4,10 +4,12 @@ target("SkrRoot")
     add_includedirs(sdl2_includes_dir, {public = true})
 
 if (is_os("windows")) then 
-    table.insert(links_list, "SDL2")
+    target("SkrRT")
+        add_links("SDL2", {public = true})
 elseif (is_os("macosx") or is_os("linux")) then
     add_requires("libsdl", {configs = {shared = true}})
-    table.insert(packages_list, "libsdl")
+    target("SkrRT")
+        add_packages("libsdl", {public = true})
 else
 
 end
