f423b60eb02d9ff04e907b05ba74218199309498
zhurong <211103376@qq.com>
2023-08-09 13:42:39 +0800

    bit algo test

---
+++ tests/base/xmake.lua
@@ -0,0 +1,6 @@
+target("AlgoTest")
+    set_kind("binary")
+    set_group("05.tests/base")
+    public_dependency("SkrBase", engine_version)
+    add_deps("SkrTestFramework", {public = false})
+    add_files("algo/*.cpp")
