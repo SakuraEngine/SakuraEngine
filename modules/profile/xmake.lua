ceba41c6fa6a963a8f37d6c89853f829fb6520af
SaeruHikari <39457738+SaeruHikari@users.noreply.github.com>
2023-08-11 11:41:26 +0800

    构建脚本优化

--- modules/profile/xmake.lua
+++ modules/profile/xmake.lua
@@ -1,17 +1,12 @@
-target("SkrCompileFlags")
-    add_defines("TRACY_IMPORTS", "TRACY_ON_DEMAND", "TRACY_FIBERS", {public = true})
+target("SkrProfile")
+    set_group("01.modules")
+    set_kind("shared")
+    add_deps("SkrCompileFlags", {public = true})
+    add_includedirs("include", {public = true})
+    add_files("internal/tracy/TracyClient.cpp") -- version("0.9.2alpha")
     if (is_config("use_profile", "enable")) then
         add_defines("SKR_PROFILE_OVERRIDE_ENABLE", {public = true})
     end
     if (is_config("use_profile", "disable")) then
         add_defines("SKR_PROFILE_OVERRIDE_DISABLE", {public = true})
     end
-
-target("SkrProfile")
-    set_group("00.thirdparty")
-    set_kind("shared")
-    -- version("0.9.2alpha")
-    add_deps("SkrCompileFlags", {public = true})
-    add_files("internal/tracy/TracyClient.cpp")
-    add_includedirs("include", {public = true})
-    add_defines("TRACY_EXPORTS", {public = false})
