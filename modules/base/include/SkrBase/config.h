a4fc4aea39b4cd88b54fcb46b1a134e062ba69ca
zhurong <211103376@qq.com>
2023-09-18 17:20:06 +0800

    optimize config & rttr register marco

--- modules/base/include/SkrBase/config.h
+++ modules/base/include/SkrBase/config.h
@@ -4,12 +4,4 @@
 #include "SkrBase/config/compiler.h"
 
 // keywords
-// #include "SkrBase/config/key_words.h"
-
-// TODO. remove this
-// INLINE
-#if defined(__cplusplus)
-    #define SKR_INLINE inline
-#else
-    #define SKR_INLINE
-#endif
+#include "SkrBase/config/key_words.h"
