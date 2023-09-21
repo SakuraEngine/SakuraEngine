c594f24058722876ce96d1a957aded218a9fd4f0
SaeruHikari <39457738+SaeruHikari@users.noreply.github.com>
2023-09-21 11:49:56 +0800

    #106 构建报错 (#110)
    * Update setup.lua

--- setup.lua
+++ setup.lua
@@ -32,7 +32,7 @@ if (os.host() == "windows") then
     find_sdk.tool_from_github("dxc", "dxc-windows-x64.zip")
     find_sdk.tool_from_github("wasm-clang", "wasm-clang-windows-x64.zip")
     find_sdk.tool_from_github("ispc", "ispc-windows-x64.zip")
-    find_sdk.tool_from_github("reflector", "reflector-windows-x64.zip")
+    find_sdk.tool_from_github("meta-v1.0.0-llvm_17.0.1", "meta-v1.0.0-llvm_17.0.1-windows-x64.zip")
     find_sdk.tool_from_github("tracy-gui-0.9.2a", "tracy-gui-0.9.2a-windows-x64.zip")
     -- network
     find_sdk.lib_from_github("gns", "gns-windows-x64.zip")
@@ -43,7 +43,7 @@ if (os.host() == "macosx") then
     if (os.arch() == "x86_64") then
         --
         find_sdk.tool_from_github("dxc", "dxc-macosx-x86_64.zip")
-        find_sdk.tool_from_github("reflector", "reflector-macosx-x86_64.zip")
+        find_sdk.tool_from_github("meta-v0.0.1-llvm_15.0.3", "meta-v0.0.1-llvm_15.0.3-macosx-x86_64.zip")
         find_sdk.tool_from_github("tracy-gui-0.9.2a", "tracy-gui-0.9.2a-macosx-x86_64.zip")
         -- network
         find_sdk.lib_from_github("gns", "gns-macosx-x86_64.zip")
