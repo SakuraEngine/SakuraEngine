db9e8d9c46e2acfc7fbcf94952eb3241860e07cc
zhurong <211103376@qq.com>
2023-10-07 16:03:56 +0800

    fix ci

--- modules/runtime/include/SkrRT/containers_new/array.hpp
+++ modules/runtime/include/SkrRT/containers_new/array.hpp
@@ -7,3 +7,13 @@ namespace skr
 template <typename T>
 using Array = container::Array<T, SkrAllocator>;
 }
+
+// serde
+#include "SkrRT/serde/binary/serde.h"
+#include "SkrRT/serde/binary/reader_fwd.h"
+#include "SkrRT/serde/binary/writer_fwd.h"
+
+// binary reader
+namespace skr::binary
+{
+} // namespace skr
