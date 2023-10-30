78bc2459188f1fbe2ce6cd3f9be85281b9568245
SaeruHikari <39457738+SaeruHikari@users.noreply.github.com>
2023-10-30 16:59:36 +0800

    read/write/del

--- modules/lightning_storage/src/lightning_storage/mdb.cpp
+++ modules/lightning_storage/src/lightning_storage/mdb.cpp
@@ -2,7 +2,7 @@
 #include <SkrRT/platform/filesystem.hpp>
 #include "SkrRT/misc/log.h"
 #include "SkrRT/containers/string.hpp"
-#include "SkrLightningStorage/storage.h"
+#include "SkrLightningStorage/mdb.h"
 #include "lmdb/lmdb.h"
 
 SLightningEnvironmentId skr_lightning_storage_create_environment(const char8_t* name)
@@ -73,4 +73,73 @@ void skr_close_lightning_storage(SLightningStorageId storage)
     MDB_dbi dbi = static_cast<MDB_dbi>(storage->mdbi);
     mdb_dbi_close(storage->env->env, dbi);
     sakura_free(storage);
+}
+
+SLightningTXNId skr_open_lightning_transaction(SLightningEnvironmentId env, SLightningTXNId parent, ELightningTransactionOpenFlags flags)
+{
+    MDB_txn* _txn = nullptr;
+    MDB_txn* _parent = (MDB_txn*)parent;
+    if (auto rc = mdb_txn_begin(env->env, _parent, flags, &_txn))
+    {
+        skr::string err = (const char8_t*)mdb_strerror(rc);
+        SKR_LOG_ERROR(u8"mdb_txn_begin failed: %d, %s", rc, err.c_str());
+    }
+    return (SLightningTXNId)_txn;
+}
+
+bool skr_lightning_storage_read(SLightningTXNId txn, SLightningStorageId storage, const struct SLightningStorageValue* key, struct SLightningStorageValue* value)
+{
+    MDB_val _key = { key->size, (void*)key->data };
+    MDB_val _value;
+    auto dbi = (MDB_dbi)storage->mdbi;
+    if (auto rc = mdb_get((MDB_txn*)txn, dbi, &_key, &_value))
+    {
+        if (rc != MDB_NOTFOUND)
+        {
+            skr::string err = (const char8_t*)mdb_strerror(rc);
+            SKR_LOG_ERROR(u8"e failed: %d, %s", rc, err.c_str());
+        }
+        return false;
+    }
+    value->data = _value.mv_data;
+    value->size = _value.mv_size;
+    return true;
+}
+
+bool skr_lightning_storage_write(SLightningTXNId txn, SLightningStorageId storage, const struct SLightningStorageValue* key, const struct SLightningStorageValue* value)
+{
+    MDB_val _key = { key->size, (void*)key->data };
+    MDB_val _value = { value->size, (void*)value->data };
+    auto dbi = (MDB_dbi)storage->mdbi;
+    if (auto rc = mdb_put((MDB_txn*)txn, dbi, &_key, &_value, 0))
+    {
+        skr::string err = (const char8_t*)mdb_strerror(rc);
+        SKR_LOG_ERROR(u8"e failed: %d, %s", rc, err.c_str());
+        return false;
+    }
+    return true;
+}
+
+bool skr_lightning_storage_del(SLightningTXNId txn, SLightningStorageId storage, const struct SLightningStorageValue* key)
+{
+    MDB_val _key = { key->size, (void*)key->data };
+    auto dbi = (MDB_dbi)storage->mdbi;
+    if (auto rc = mdb_del((MDB_txn*)txn, dbi, &_key, nullptr))
+    {
+        skr::string err = (const char8_t*)mdb_strerror(rc);
+        SKR_LOG_ERROR(u8"e failed: %d, %s", rc, err.c_str());
+        return false;
+    }
+    return true;
+}
+
+bool skr_commit_lightning_transaction(SLightningTXNId txn)
+{
+    if (auto rc = mdb_txn_commit((MDB_txn*)txn))
+    {
+        skr::string err = (const char8_t*)mdb_strerror(rc);
+        SKR_LOG_ERROR(u8"mdb_txn_commit failed: %d, %s", rc, err.c_str());
+        return false;
+    }
+    return true;
 }
--- tests/runtime/mdb/main.cpp
+++ tests/runtime/mdb/main.cpp
@@ -1,4 +1,5 @@
-#include "SkrLightningStorage/storage.h"
+#include "SkrRT/misc/log.h"
+#include "SkrLightningStorage/mdb.h"
 
 #include "SkrTestFramework/framework.hpp"
 
@@ -27,4 +28,75 @@ TEST_CASE_METHOD(MDBTests, "Storage")
     
     skr_close_lightning_storage(storage);
     skr_lightning_storage_free_environment(env);
+}
+
+TEST_CASE_METHOD(MDBTests, "TXN")
+{
+    auto env = skr_lightning_storage_create_environment(u8"./test_mdb");
+    EXPECT_NE(env, nullptr);
+
+    SLightningStorageOpenDescriptor desc = {};
+    desc.flags = LIGHTNING_STORAGE_OPEN_CREATE;
+    desc.name = u8"test_storage";
+    auto storage = skr_open_lightning_storage(env, &desc);
+    EXPECT_NE(storage, nullptr);
+    
+    if (auto txn = skr_open_lightning_transaction(env, nullptr, 0))
+    {
+        skr::string key_string = u8"hello";
+        skr::string value_string = u8"world";
+
+        SLightningStorageValue key = { key_string.size() + 1, key_string.c_str() };
+        SLightningStorageValue value = { value_string.size() + 1, value_string.c_str() };
+
+        EXPECT_TRUE(skr_lightning_storage_write(txn, storage, &key, &value));
+
+        SLightningStorageValue readed_value;
+        EXPECT_TRUE(skr_lightning_storage_read(txn, storage, &key, &readed_value));
+
+        skr::string readed_string = (const char8_t*)readed_value.data;
+        EXPECT_EQ(readed_string, value_string);
+
+        EXPECT_TRUE(skr_lightning_storage_del(txn, storage, &key));
+        EXPECT_FALSE(skr_lightning_storage_read(txn, storage, &key, &readed_value));
+
+        EXPECT_TRUE(skr_commit_lightning_transaction(txn));
+    }
+
+    skr_close_lightning_storage(storage);
+    skr_lightning_storage_free_environment(env);
+}
+
+TEST_CASE_METHOD(MDBTests, "TXN2")
+{
+    auto env = SLightningEnvironment::Open(u8"./test_mdb");
+    EXPECT_NE(env, nullptr);
+
+    SLightningStorageOpenDescriptor desc = {};
+    desc.flags = LIGHTNING_STORAGE_OPEN_CREATE;
+    desc.name = u8"test_storage";
+    auto storage = env->open_storage(&desc);
+    EXPECT_NE(storage, nullptr);
+    
+    if (auto txn = env->open_transaction(nullptr, 0))
+    {
+        skr::string key_string = u8"hello";
+        skr::string value_string = u8"world";
+
+        SLightningStorageValue key = { key_string.size() + 1, key_string.c_str() };
+        SLightningStorageValue value = { value_string.size() + 1, value_string.c_str() };
+
+        EXPECT_TRUE(storage->write(txn, key, value));
+
+        SLightningStorageValue readed_value = storage->read(txn, key);
+        skr::string readed_string = (const char8_t*)readed_value.data;
+        EXPECT_EQ(readed_string, value_string);
+
+        EXPECT_TRUE(storage->del(txn, key));
+
+        EXPECT_TRUE(env->commit_transaction(txn));
+    }
+
+    env->close_storage(storage);
+    SLightningEnvironment::Free(env);
 }
