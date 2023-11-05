158915f447a1194711b9cd2817813faf6a18068b
BenzzZX <672619410@qq.com>
2023-11-05 11:49:06 +0800

    Trait object (#126)
    * trait object initial implementation
    
    * add parameter to trait
    
    * handle noexcept
    
    * trait property

---
+++ modules/runtime/include/SkrRT/rttr/trait.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#ifdef __meta__
+#define TRAIT_MIXIN(type) void* self; template<class T> type(T&& t) : self(&t) {}
+#else
+#define TRAIT_MIXIN(type) \
+    void* self = nullptr; \
+    const __VTABLE_##type##_t* vtable = nullptr; \
+    template<class T> \
+    type(T& t) noexcept \
+        : self(&t), vtable(&__VTABLE_##type##_impl<T>::vtable) {}
+#endif
