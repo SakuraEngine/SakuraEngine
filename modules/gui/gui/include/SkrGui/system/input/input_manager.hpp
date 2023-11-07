766950cfc7d43d901678e9b6b16b2d9a6f062794
zhurong <211103376@qq.com>
2023-11-07 18:29:50 +0800

    update roadmap

--- modules/gui/gui/include/SkrGui/system/input/input_manager.hpp
+++ modules/gui/gui/include/SkrGui/system/input/input_manager.hpp
@@ -9,11 +9,12 @@ namespace gui
 // TODO. roadmap
 // //  1. hit test
 // //  2. pointer events
-//  3. pointer events bind
-//  4. focus (Pointer)
-//  5. keyboard events
-//  6. keyboard events bind
-//  7. focus (keyboard)         // ! 可 TODO
+// //  3. pointer events bind
+//  4. 实现 MouseRegion（同时补全 state 和 增量更新的机制）
+//  5. focus (Pointer)
+//  6. keyboard events
+//  7. keyboard events bind
+//  8. focus & navigation (keyboard)         // ! 可 TODO
 struct InputManager {
 };
 } // namespace gui
