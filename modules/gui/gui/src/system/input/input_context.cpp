a710eb0b0aa615e6fb6d525dad85e31b725f3166
zhurong <211103376@qq.com>
2023-11-10 11:57:42 +0800

    input context & input manager

--- modules/gui/gui/src/system/input/input_context.cpp
+++ modules/gui/gui/src/system/input/input_context.cpp
@@ -1,10 +1,18 @@
 #include "SkrGui/system/input/input_context.hpp"
+#include "SkrGui/framework/render_object/render_box.hpp"
 
 namespace skr::gui
 {
-InputContext::InputContext(NotNull<RenderBox*> widget)
-    : widget(widget)
+InputContext::InputContext(NotNull<InputManager*> manager, NotNull<RenderBox*> widget)
+    : _owner(manager)
+    , _widget(widget)
 {
+}
+
+// hit test
+bool InputContext::hit_test(HitTestResult* result, Offsetf global_position)
+{
+    return false;
 }
 
 } // namespace skr::gui
