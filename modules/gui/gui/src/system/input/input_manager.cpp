3e8f1f695f014dba1594fbc9827244d1e7e7d1b7
zhurong <211103376@qq.com>
2023-11-14 15:23:53 +0800

    SNewChild => children += SNewWidget

--- modules/gui/gui/src/system/input/input_manager.cpp
+++ modules/gui/gui/src/system/input/input_manager.cpp
@@ -9,7 +9,6 @@ bool InputManager::hit_test(HitTestResult* result, Offsetf system_location)
     for (const auto& context : _contexts)
     {
         Offsetf local_position = context->system_to_local(system_location);
-        // SKR_LOG_INFO(u8"system: (%f, %f) local:(%f, %f)", system_location.x, system_location.y, local_position.x, local_position.y);
         if (context->hit_test(result, local_position))
         {
             return true;
