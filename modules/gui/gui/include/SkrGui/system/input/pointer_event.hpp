85d46da72b38ed204f6d9bf7200f094d82834c2f
zhu-rong <211103376@qq.com>
2023-11-22 17:01:10 +0800

    pointer events

--- modules/gui/gui/include/SkrGui/system/input/pointer_event.hpp
+++ modules/gui/gui/include/SkrGui/system/input/pointer_event.hpp
@@ -8,9 +8,7 @@ namespace skr sreflect
 {
 namespace gui sreflect
 {
-sreflect_enum_class(
-    "guid": "7223961b-5309-4fac-8207-8476bf7f3b05"
-)
+sreflect_enum_class("guid": "7223961b-5309-4fac-8207-8476bf7f3b05")
 EPointerDeviceType : int32_t
 {
     Unknown = 0,
@@ -20,9 +18,7 @@ EPointerDeviceType : int32_t
     // Stylus,
     // InvertedStylus,
 };
-sreflect_enum_class(
-    "guid": "3f6c7998-a93e-424e-9d95-c75800309a16"
-)
+sreflect_enum_class("guid": "3f6c7998-a93e-424e-9d95-c75800309a16")
 EPointerButton : int32_t
 {
     Unknown = 0,
@@ -34,70 +30,74 @@ EPointerButton : int32_t
     X3B,
     X4B,
     X5B,
+    __COUNT,
 };
 
-sreflect_struct(
-    "guid": "fa4706c4-5982-4056-a5b5-b12098c0963a"
-)
+// TODO. Touch Event, 与 PointerEvent 分开实现
+sreflect_struct("guid": "fa4706c4-5982-4056-a5b5-b12098c0963a")
 PointerEvent : public Event {
     SKR_RTTR_GENERATE_BODY()
 
-    // TODO. 对于移动端可能不需要，但是对于桌面端，需要同时得到左右 Ctrl/Alt/Shift 等 Modifier 的信息
     EPointerDeviceType device_type     = EPointerDeviceType::Unknown; // 来源设备
     Offsetf            global_position = {};                          // 位置，全局空间的逻辑坐标
     Offsetf            global_delta    = {};                          // 距离上一次触发的位置变化量
     EPointerButton     button          = EPointerButton::Unknown;     // 按键 id
     Matrix4            transform       = {};                          // global->local 转换矩阵
+
+    // TODO. get mouse button states
+    // TODO. get modifier key states
 };
 
-sreflect_struct(
-    "guid": "91343bb8-4b20-4734-b491-23362b76aa17"
-)
+// Down/Up/Move
+sreflect_struct("guid": "91343bb8-4b20-4734-b491-23362b76aa17")
 PointerDownEvent : public PointerEvent {
     SKR_RTTR_GENERATE_BODY()
 };
 
-sreflect_struct(
-    "guid": "8817dd2b-7e0d-47f5-8ee5-72790bbf3f09"
-)
+sreflect_struct("guid": "8817dd2b-7e0d-47f5-8ee5-72790bbf3f09")
 PointerUpEvent : public PointerEvent {
     SKR_RTTR_GENERATE_BODY()
 };
 
-sreflect_struct(
-    "guid": "453f7052-9740-4136-9831-55e8188827d2"
-)
+sreflect_struct("guid": "453f7052-9740-4136-9831-55e8188827d2")
 PointerMoveEvent : public PointerEvent {
     SKR_RTTR_GENERATE_BODY()
 };
 
-sreflect_struct(
-    "guid": "6cddd04b-749c-4a5e-99b0-27396ef84d50"
-)
+// Enter/Exit
+sreflect_struct("guid": "6cddd04b-749c-4a5e-99b0-27396ef84d50")
 PointerEnterEvent : public PointerEvent {
     SKR_RTTR_GENERATE_BODY()
 };
 
-sreflect_struct(
-    "guid": "f41e2f74-b813-411d-b065-5df10f5edaeb"
-)
+sreflect_struct("guid": "f41e2f74-b813-411d-b065-5df10f5edaeb")
 PointerExitEvent : public PointerEvent {
     SKR_RTTR_GENERATE_BODY()
 };
 
-sreflect_struct(
-    "guid": "51778097-47ab-4eb3-9193-3cecffedf8a0"
-)
-PointerSignalEvent : public PointerEvent {
-    SKR_RTTR_GENERATE_BODY()
-};
-
-sreflect_struct(
-    "guid": "1da2a830-544a-44c4-8ba7-2f313194bced"
-)
+// Scroll/Scale
+sreflect_struct("guid": "1da2a830-544a-44c4-8ba7-2f313194bced")
 PointerScrollEvent : public PointerEvent {
     SKR_RTTR_GENERATE_BODY()
     Offsetf scroll_delta = {};
+};
+sreflect_struct("guid": "51778097-47ab-4eb3-9193-3cecffedf8a0")
+PointerScaleEvent : public PointerEvent {
+    SKR_RTTR_GENERATE_BODY()
+};
+
+// Pan/Zoom
+sreflect_struct("guid": "5d3aeff7-4cd5-41d2-93ba-0d0cdd14b9b0")
+PointerPanZoomStartEvent : public PointerEvent {
+    SKR_RTTR_GENERATE_BODY()
+};
+sreflect_struct("guid": "f25e3d34-4fae-4be8-8f17-385b960a95f8")
+PointerPanZoomUpdateEvent : public PointerEvent {
+    SKR_RTTR_GENERATE_BODY()
+};
+sreflect_struct("guid": "d8ee52fd-2f4e-4a62-a922-d3cb2467883b")
+PointerPanZoomEndEvent : public PointerEvent {
+    SKR_RTTR_GENERATE_BODY()
 };
 
 } // namespace gui sreflect
