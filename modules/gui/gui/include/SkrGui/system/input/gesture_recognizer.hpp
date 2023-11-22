fc2a8f9b52c755f342c88dd1310f78c7d7a93543
zhu-rong <211103376@qq.com>
2023-11-22 17:29:23 +0800

    GestureRecognizer

---
+++ modules/gui/gui/include/SkrGui/system/input/gesture_recognizer.hpp
@@ -0,0 +1,31 @@
+#pragma once
+#include "SkrGui/fwd_config.hpp"
+#ifndef __meta__
+    #include "SkrGui/system/input/gesture_recognizer.generated.h"
+#endif
+
+namespace skr sreflect
+{
+namespace gui sreflect
+{
+
+// 手势区分为：
+//  1. Pointer 手势，用于 desktop，通常只有 Click、Double Click、Drag-Drop 等，可以实现一些特殊操作，比如震动
+//  2. Touch 手势，相比于 Pointer 手势更复杂，用于移动端，不一一展开
+sreflect_struct("guid": "8fb085fd-9412-4a1b-bc95-a518e32746f2")
+SKR_GUI_API GestureRecognizer : public skr::rttr::IObject {
+    SKR_RTTR_GENERATE_BODY()
+};
+
+sreflect_struct("guid": "7ac4136e-e753-475c-98f2-9b7e44964000")
+SKR_GUI_API PointerGestureRecognizer : public GestureRecognizer {
+    SKR_RTTR_GENERATE_BODY()
+};
+
+sreflect_struct("guid": "8bf39324-45f2-4341-afef-96f276526e88")
+SKR_GUI_API TouchGestureRecognizer : public GestureRecognizer {
+    SKR_RTTR_GENERATE_BODY()
+};
+
+} // namespace gui sreflect
+} // namespace skr sreflect
