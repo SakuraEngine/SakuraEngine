0d9e249e74ac3cf529c053d8ca308cc8d07cdabf
zhu-rong <211103376@qq.com>
2023-11-29 18:21:50 +0800

    finish arena API

--- modules/gui/gui/include/SkrGui/system/input/gesture/gesture_arena_manager.hpp
+++ modules/gui/gui/include/SkrGui/system/input/gesture/gesture_arena_manager.hpp
@@ -10,9 +10,16 @@ namespace gui sreflect
 {
 sreflect_struct("guid": "6b1d8469-6958-4800-affa-bf1e12cd5197")
 GestureArenaManager {
+    ~GestureArenaManager();
+
+    GestureArena* open_if_resolved(CombinePointerId pointer);
+    GestureArena* add_gesture(CombinePointerId pointer, GestureRecognizer* gesture);
+
+    GestureArena* find_arena(CombinePointerId pointer);
+    GestureArena* find_arena_or_add(CombinePointerId pointer);
 
 private:
-    UMap<CombinePointerId, GestureArena> _arenas = {};
+    UMap<CombinePointerId, GestureArena*> _arenas = {};
 };
 } // namespace gui sreflect
 } // namespace skr sreflect
--- modules/gui/gui/src/system/input/gesture/gesture_arena.cpp
+++ modules/gui/gui/src/system/input/gesture/gesture_arena.cpp
@@ -2,22 +2,43 @@
 
 namespace skr::gui
 {
+GestureArena::GestureArena(CombinePointerId pointer)
+    : _pointer(pointer)
+{
+}
+
+void GestureArena::open()
+{
+    if (_state == GestureArenaState::Closed)
+    {
+        SKR_LOG_ERROR(u8"open a closed arena is not allowed");
+    }
+    _state = GestureArenaState::Opened;
+}
 void GestureArena::add(GestureRecognizer* gesture)
 {
-    SKR_ASSERT(_is_open && "add gesture when arena is closed is not allowed");
+    SKR_ASSERT(_state == GestureArenaState::Opened && "add gesture when arena is closed is not allowed");
     _members.add(gesture);
 }
-bool GestureArena::close()
+void GestureArena::close()
 {
-    _is_open = false;
-    return _try_to_resolve_arena();
+    if (_state != GestureArenaState::Opened)
+    {
+        SKR_LOG_ERROR(u8"close a non-opened arena is not allowed");
+    }
+    _state = GestureArenaState::Closed;
+    _try_to_resolve_arena();
 }
-bool GestureArena::sweep(CombinePointerId pointer)
+void GestureArena::sweep(CombinePointerId pointer)
 {
-    if (_is_held) // held, mark need sweep first
+    if (_state != GestureArenaState::Closed)
+    {
+        SKR_LOG_ERROR(u8"sweep a non-closed arena is not allowed");
+    }
+
+    if (_is_held) // 延迟 Sweep 状态到 release
     {
         _has_pending_sweep = true;
-        return false;
     }
     else
     {
@@ -30,7 +51,8 @@ bool GestureArena::sweep(CombinePointerId pointer)
                 _members[i]->reject_gesture(pointer);
             }
         }
-        return true;
+
+        _state = GestureArenaState::Resolved;
     }
 }
 void GestureArena::hold()
@@ -45,48 +67,58 @@ void GestureArena::release(CombinePointerId pointer)
         sweep(pointer);
     }
 }
-
-// helper functions
-void GestureArena::_resolve(GestureRecognizer* member, bool accept)
+void GestureArena::accept_gesture(GestureRecognizer* member)
 {
-    if (accept)
+    if (_state == GestureArenaState::Opened)
     {
-        if (_is_open)
-        {
-            eager_winner = member;
-        }
-        else
-        {
-            _resolve_in_favor_of(member);
-        }
+        eager_winner = member;
     }
-    else // reject
+    else if (_state == GestureArenaState::Closed)
     {
-        _members.remove(member);
-        member->reject_gesture(_pointer);
-        if (!_is_open)
-        {
-            _try_to_resolve_arena();
-        }
+        _resolve_in_favor_of(member);
+    }
+    else
+    {
+        SKR_LOG_ERROR(u8"accept gesture in a resolved arena is not allowed");
     }
 }
-bool GestureArena::_try_to_resolve_arena()
+void GestureArena::reject_gesture(GestureRecognizer* member)
 {
+    if (_state == GestureArenaState::Resolved)
+    {
+        SKR_LOG_ERROR(u8"reject gesture in a resolved arena is not allowed");
+    }
+
+    _members.remove(member);
+    member->reject_gesture(_pointer);
+    if (_state == GestureArenaState::Closed)
+    {
+        _try_to_resolve_arena();
+    }
+}
+
+// helper functions
+void GestureArena::_try_to_resolve_arena()
+{
+    if (_state != GestureArenaState::Closed)
+    {
+        SKR_LOG_ERROR(u8"try to resolve a non-closed arena is not allowed");
+    }
+
     if (_members.empty()) // empty arena, just remove
     {
-        return true;
+        _state = GestureArenaState::Resolved;
     }
     else if (_members.size() == 1) // 只有一个手势，直接接受
     {
         _members[0]->accept_gesture(_pointer);
-        return true;
+        _state = GestureArenaState::Resolved;
     }
     else if (eager_winner) // 多个手势，接受希望胜利的 winner，如果没有这个 winner 代表暂时无法决议
     {
         _resolve_in_favor_of(eager_winner);
-        return true;
+        _state = GestureArenaState::Resolved;
     }
-    return false;
 }
 void GestureArena::_resolve_in_favor_of(GestureRecognizer* member)
 {
