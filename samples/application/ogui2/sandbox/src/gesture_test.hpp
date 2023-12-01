7e243e7154b84a583b90fe6cfb9c10de27805aa7
zhu-rong <211103376@qq.com>
2023-12-01 14:23:33 +0800

    gesture test

---
+++ samples/application/ogui2/sandbox/src/gesture_test.hpp
@@ -0,0 +1,18 @@
+#pragma once
+#include "SkrGui/system/input/gesture/click_gesture_recognizer.hpp"
+#include "SkrGui/framework/widget/stateless_widget.hpp"
+#ifndef __meta__
+    #include "OGUI_Sandbox/gesture_test.generated.h"
+#endif
+
+namespace skr sreflect
+{
+namespace gui sreflect
+{
+sreflect_struct("guid": "e52ccdac-d8bc-4abf-af1a-85248f615477")
+GestureTest : public StatelessWidget {
+    SKR_RTTR_GENERATE_BODY()
+};
+
+} // namespace gui sreflect
+} // namespace skr sreflect
