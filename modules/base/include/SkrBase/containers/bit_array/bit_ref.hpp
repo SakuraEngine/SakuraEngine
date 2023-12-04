299cf9a693f4194ee263e532e58562cdea62dda5
zhu-rong <211103376@qq.com>
2023-12-04 17:07:54 +0800

    optimize bit array result

--- modules/base/include/SkrBase/containers/bit_array/bit_ref.hpp
+++ modules/base/include/SkrBase/containers/bit_array/bit_ref.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include "SkrBase/config.h"
+#include "SkrRT/containers/optional.hpp"
 #include <type_traits>
 
 namespace skr::container
@@ -13,7 +14,7 @@ struct BitRef {
         , _mask(mask)
     {
     }
-    SKR_INLINE operator bool() const { return (_data & _mask); }
+    SKR_INLINE         operator bool() const { return (_data & _mask); }
     SKR_INLINE BitRef& operator=(bool v)
     {
         _data = v ? _data | _mask : _data & ~_mask;
@@ -28,5 +29,25 @@ struct BitRef {
 private:
     TBlock& _data;
     TBlock  _mask;
+};
+
+template <typename TBlock, typename TS>
+struct BitDataRef {
+    Optional<BitRef<TBlock>> data  = {};
+    TS                       index = npos_of<TS>;
+
+    inline BitDataRef() = default;
+    inline BitDataRef(TS index)
+        : index(index)
+    {
+    }
+    inline BitDataRef(BitRef<TBlock> data, TS index)
+        : data(data)
+        , index(index)
+    {
+    }
+
+    inline                 operator bool() const { return data.has_value() || index != npos_of<TS>; }
+    inline BitRef<TBlock>& operator*() const { return *data; }
 };
 } // namespace skr::container
