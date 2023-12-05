a2abb35e6a192b65fec4ba3ba383d1f3f0cd876f
zhu-rong <211103376@qq.com>
2023-12-05 10:44:49 +0800

    use pointer bit ref

--- modules/base/include/SkrBase/containers/bit_array/bit_ref.hpp
+++ modules/base/include/SkrBase/containers/bit_array/bit_ref.hpp
@@ -13,18 +13,25 @@ struct BitRef {
 
     SKR_INLINE static BitRef<TBlock> At(TBlock* data, TBlock idx)
     {
-        return { data[idx >> Algo::PerBlockSizeLog2], TBlock(1) << (idx & Algo::PerBlockSizeMask) };
+        return { data + (idx >> Algo::PerBlockSizeLog2), TBlock(1) << (idx & Algo::PerBlockSizeMask) };
     }
 
-    SKR_INLINE BitRef(TBlock& data, TBlock mask)
+    SKR_INLINE BitRef() = default;
+
+    SKR_INLINE BitRef(TBlock* data, TBlock mask)
         : _data(data)
         , _mask(mask)
     {
     }
-    SKR_INLINE         operator bool() const { return (_data & _mask); }
+    SKR_INLINE operator bool() const
+    {
+        SKR_ASSERT(_data != nullptr && "BitRef is invalid");
+        return ((*_data) & _mask);
+    }
     SKR_INLINE BitRef& operator=(bool v)
     {
-        _data = v ? _data | _mask : _data & ~_mask;
+        SKR_ASSERT(_data != nullptr && "BitRef is invalid");
+        (*_data) = v ? (*_data) | _mask : (*_data) & ~_mask;
         return *this;
     }
     SKR_INLINE BitRef& operator=(const BitRef& rhs)
@@ -33,15 +40,17 @@ struct BitRef {
         return *this;
     }
 
+    SKR_INLINE bool is_valid() const { return _data != nullptr; }
+
 private:
-    TBlock& _data;
-    TBlock  _mask;
+    TBlock* _data = nullptr;
+    TBlock  _mask = Algo::EmptyMask;
 };
 
 template <typename TBlock, typename TS>
 struct BitDataRef {
-    Optional<BitRef<TBlock>> data  = {};
-    TS                       index = npos_of<TS>;
+    BitRef<TBlock> data  = {};
+    TS             index = npos_of<TS>;
 
     inline BitDataRef() = default;
     inline BitDataRef(TS index)
@@ -54,7 +63,7 @@ struct BitDataRef {
     {
     }
 
-    inline                 operator bool() const { return data.has_value() || index != npos_of<TS>; }
+    inline                 operator bool() const { return data.is_valid() || index != npos_of<TS>; }
     inline BitRef<TBlock>& operator*() const { return *data; }
 };
 } // namespace skr::container
