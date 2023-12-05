684d85845407f405288f812d3ebb4b0074bf06c5
zhu-rong <211103376@qq.com>
2023-12-05 10:44:49 +0800

    use pointer bit ref

---
+++ modules/base/include/SkrBase/containers/bit_array/bit_set.hpp
@@ -0,0 +1,10 @@
+#pragma once
+#include "SkrBase/containers/bit_array/bit_ref.hpp"
+#include "SkrBase/algo/bit_algo.hpp"
+
+namespace skr::container
+{
+template <size_t N, typename TBlock>
+struct BitSet {
+};
+} // namespace skr::container
