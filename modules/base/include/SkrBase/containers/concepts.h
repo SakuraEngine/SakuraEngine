58e10a1c750c453373e47c71d272f3caf61f9a61
Maxwell Geng <33229754+MaxwellGengYF@users.noreply.github.com>
2023-12-13 10:56:21 +0800

    algorithm concept if needed.

--- modules/base/include/SkrBase/containers/concepts.h
+++ modules/base/include/SkrBase/containers/concepts.h
@@ -2,6 +2,13 @@
 #include <type_traits>
 namespace skr::container::concepts
 {
+namespace detail
+{
+template <typename T>
+std::remove_reference_t<T>& decl_lval(T&& t) {}
+template <typename T>
+std::remove_reference_t<T>&& decl_rval(T&& t) {}
+} // namespace detail
 template <typename T, typename... Args>
 concept Constructible = requires(Args&&... args) {
     T{ std::forward<Args>(args)... };
@@ -11,6 +18,15 @@ template <typename T>
 concept Iterable = requires(T v) {
     v.begin();
     v.end();
+};
+
+template <typename T>
+concept LinearIterable = requires(T v) {
+    v.end();
+    detail::decl_lval(v.begin())++;
+    detail::decl_lval(v.begin())--;
+    detail::decl_lval(v.begin()) += 1;
+    detail::decl_lval(v.begin()) -= 1;
 };
 
 template <typename T>
