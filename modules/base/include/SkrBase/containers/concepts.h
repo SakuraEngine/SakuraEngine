51eb7d900d99876c726baf1f40badd51d03dfd34
Maxwell Geng <33229754+MaxwellGengYF@users.noreply.github.com>
2023-12-18 17:20:47 +0800

    more concept

--- modules/base/include/SkrBase/containers/concepts.h
+++ modules/base/include/SkrBase/containers/concepts.h
@@ -15,6 +15,27 @@ concept Constructible = requires(Args&&... args) {
 };
 
 template <typename T>
+concept DefaultConstructible = requires() {
+    T{};
+};
+
+template <typename T>
+concept CopyConstructible = requires(T const& t) {
+    T{t};
+};
+
+template <typename T>
+concept MoveConstructible = requires() {
+    T{std::declval<T>()};
+};
+
+template <typename T>
+concept Comparable = requires(T const& a) {
+    a.operator==(a);
+    a.operator!=(a);
+};
+
+template <typename T>
 concept Iterable = requires(T v) {
     v.begin();
     v.end();
@@ -22,9 +43,7 @@ concept Iterable = requires(T v) {
 
 template <typename T>
 concept LinearIterable = requires(T v) {
-    v.end();
-    detail::decl_lval(v.begin())++;
-    detail::decl_lval(v.begin())--;
+    v.end() - v.begin();
     detail::decl_lval(v.begin()) += 1;
     detail::decl_lval(v.begin()) -= 1;
 };
