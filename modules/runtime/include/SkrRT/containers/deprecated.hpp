41ca22bc108e1f8688cd69bdfe0ed35ecb868ae3
zhu-rong <211103376@qq.com>
2023-12-19 13:57:59 +0800

    remove eastl in deprecated

--- modules/runtime/include/SkrRT/containers/deprecated.hpp
+++ modules/runtime/include/SkrRT/containers/deprecated.hpp
@@ -1,35 +1,19 @@
 #pragma once
 #include "SkrRT/config.h"
-#include "SkrRT/serde/binary/writer_fwd.h"
-#include "SkrRT/serde/binary/reader_fwd.h"
-// TODO: REMOVE EASTL
-#include <EASTL/bitset.h>
-#include <EASTL/fixed_vector.h>
-#include <EASTL/fixed_set.h>
-#include <EASTL/fixed_string.h>
-#include <EASTL/fixed_map.h>
-#include <EASTL/shared_ptr.h>
+#include <memory>
 
 // SMART POINTER
 namespace skr
 {
 template <typename T>
-using shared_ptr = eastl::shared_ptr<T>;
+using shared_ptr = std::shared_ptr<T>;
 
 template <typename T>
-using weak_ptr = eastl::weak_ptr<T>;
+using weak_ptr = std::weak_ptr<T>;
 
 template <typename T>
-using unique_ptr = eastl::unique_ptr<T>;
+using unique_ptr = std::unique_ptr<T>;
 
-using eastl::make_shared;
-using eastl::make_unique;
+using std::make_shared;
+using std::make_unique;
 } // namespace skr
-
-// FIXED CONTAINERS
-namespace skr
-{
-template <typename CharT, size_t N>
-using FixedString = eastl::fixed_string<CharT, N>;
-} // namespace skr
-// FIXED CONTAINERS
