8836281f7c98ed424f1477c86a57c94c9de44d5b
SaeruHikari <39457738+SaeruHikari@users.noreply.github.com>
2024-01-13 14:56:09 +0800

    fix macos build

--- modules/graphics/src/pch.hpp
+++ modules/graphics/src/pch.hpp
@@ -1,4 +1,6 @@
 #include "SkrGraphics/config.h" // IWYU pragma: export
 #include "SkrGraphics/containers.hpp" // IWYU pragma: export
 
+#ifdef _WIN32
 #include "winheaders.h" // IWYU pragma: export
+#endif
