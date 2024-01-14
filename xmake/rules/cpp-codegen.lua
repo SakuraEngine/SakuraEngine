96719d349390cb4b253c92093d3d82920679d557
SaeruHikari <39457738+SaeruHikari@users.noreply.github.com>
2024-01-15 05:18:56 +0800

    fix compile

--- xmake/rules/cpp-codegen.lua
+++ xmake/rules/cpp-codegen.lua
@@ -7,30 +7,12 @@ task("run-codegen-jobs")
 rule("c++.codegen")
     on_load(function (target, opt)
         local gendir = path.join(target:autogendir({root = true}), target:plat(), "codegen")
-        local target_gendir = path.join(gendir, target:name())
-
-        if (not os.exists(target_gendir)) then
-            os.mkdir(target_gendir)
+        local sourcefile = path.join(gendir, target:name(), "/generated.cpp")
+        if (not os.exists(sourcefile)) then
+            local genfile = io.open(sourcefile, "w")                
+            genfile:print("static_assert(false, \"codegen of module "..target:name().." is not completed!\")")
+            genfile:close()
         end
-
-        target:data_set("meta.codegen.dir", gendir)
+        target:add("files", sourcefile)
         target:add("includedirs", gendir, {public = true})
-    end)
-    on_config(function(target)
-        local gendir = target:data("meta.codegen.dir")
-        local sourcebatches = target:sourcebatches()
-        if sourcebatches then
-            local sourcebatch = sourcebatches["c++.build"]
-            if sourcebatch then
-                local sourcefiles = os.files(path.join(gendir, target:name(), "/generated.cpp"))
-                for _, sourcefile in ipairs(sourcefiles) do
-                    -- add source file to this batch
-                    table.insert(sourcebatch.sourcefiles, sourcefile)
-                    -- insert object files to source batches
-                    local objectfile = target:objectfile(sourcefile, "cxx")
-                    table.insert(sourcebatch.objectfiles, objectfile)
-                    table.insert(sourcebatch.dependfiles, target:dependfile(objectfile))
-                end
-            end
-        end
     end)
