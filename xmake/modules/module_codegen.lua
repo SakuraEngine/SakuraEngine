c487335392c8d8df4147703340a05c9049f8baca
SaeruHikari <39457738+SaeruHikari@users.noreply.github.com>
2024-01-15 02:40:03 +0800

    try fix shipping error

--- xmake/modules/module_codegen.lua
+++ xmake/modules/module_codegen.lua
@@ -57,7 +57,8 @@ function skr_module_gen_json(target, filename, dep_modules)
         local deptarget = project.target(dep)
         assert(deptarget:rule("skr.module"), "public dependency must be a skr.module: "..deptarget:name())
         local depversion = target:values(dep..".version")
-        json_content = json_content.."\t\t{ \"name\":\""..dep.."\", \"version\": \""..depversion.."\" },\n"
+        local kind = target:get("kind") == "shared" and "shared" or "static"
+        json_content = json_content.."\t\t{ \"name\":\""..dep.."\", \"version\": \""..depversion.."\", \"kind\": \""..kind.."\" },\n"
     end
     json_content = json_content.sub(json_content, 1, -3)
     json_content = json_content.."\n\t]\n}\n}"
@@ -105,7 +106,8 @@ function skr_module_gen_cpp(target, filename, dep_modules)
         local deptarget = project.target(dep)
         assert(deptarget:rule("skr.module"), "public dependency must be a skr.module: "..deptarget:name())
         local depversion = target:values(dep..".version")
-        cpp_content = cpp_content.."\t\t{ \"name\":\""..dep.."\", \"version\": \""..depversion.."\" },\n"
+        local kind = target:get("kind") == "shared" and "shared" or "static"
+        cpp_content = cpp_content.."\t\t{ \"name\":\""..dep.."\", \"version\": \""..depversion.."\", \"kind\": \""..kind.."\" },\n"
     end
     cpp_content = cpp_content.sub(cpp_content, 1, -3)
     cpp_content = cpp_content.."\n\t]\n}\n"..delim2.."\", "..target:name()..")"
