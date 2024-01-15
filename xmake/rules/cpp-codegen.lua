966102c7d3deaf067e4f2df4e374200b7b42ab6e
SaeruHikari <39457738+SaeruHikari@users.noreply.github.com>
2024-01-15 13:02:43 +0800

    Policy 和 Framework 分组

--- xmake/rules/cpp-codegen.lua
+++ xmake/rules/cpp-codegen.lua
@@ -1,11 +1,16 @@
-task("run-codegen-jobs")
-    on_run(function (targetname)
+target("SkrCodegenPolicy")
+    set_kind("headeronly")
+    set_group("00.utilities")
+    -- dispatch codegen task
+    before_build(function(target)
         import("meta_codegen")
-        meta_codegen(target)
+        meta_codegen()
     end)
 
 rule("c++.codegen")
     on_load(function (target, opt)
+        target:add("deps", "SkrCodegenPolicy")
+
         local gendir = path.join(target:autogendir({root = true}), target:plat(), "codegen")
         local sourcefile = path.join(gendir, target:name(), "/generated.cpp")
         if (not os.exists(sourcefile)) then
