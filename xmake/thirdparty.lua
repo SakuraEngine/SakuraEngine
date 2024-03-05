d72a7918360e48ef85e0e3bcc2d2b6bba9985b3f
SaeruHikari <39457738+SaeruHikari@users.noreply.github.com>
2024-03-05 16:47:52 +0800

    remove SDL2 script

--- xmake/thirdparty.lua
+++ xmake/thirdparty.lua
@@ -2,7 +2,6 @@ sdk_libs_dir = "$(projectdir)/SDKs/libs/"
 
 -- this must be on the top because other thirdparty libs may depend on it
 includes("thirdparty/mimalloc.lua")
-includes("thirdparty/SDL2.lua")
 includes("thirdparty/platform_sdks.lua")
 includes("thirdparty/FiberTaskingLib.lua")
 includes("thirdparty/gamenetworkingsockets.lua")
