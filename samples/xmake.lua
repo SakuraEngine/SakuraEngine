a93b6cd64eb5cc07dc62e31896fa1cb5511bfb5d
SaeruHikari <39457738+SaeruHikari@users.noreply.github.com>
2024-03-05 21:28:47 +0800

    build all samples for active parts

--- samples/xmake.lua
+++ samples/xmake.lua
@@ -1,14 +1,10 @@
 includes("application/xmake.lua")
-includes("hotfix/xmake.lua")
 
-if build_part("samples.cgpu") then
+if build_part("engine") then
     includes("cgpu/xmake.lua")
+    includes("hotfix/xmake.lua")
 end
 
-if build_part("samples.render_graph") then
+if build_part("render") then
     includes("render_graph/xmake.lua")
-end
-
-if build_part("samples.AAA") then
-    includes("AAA/xmake.lua")
 end
