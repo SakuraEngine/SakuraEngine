519f7f1e002435d9b3c4c779e6edf463167afe4a
SaeruHikari <39457738+SaeruHikari@users.noreply.github.com>
2024-03-05 17:06:29 +0800

    remove FTL script (#192)
    * fix missing parts
    
    * remove SDL2 script
    
    * remove FTL script

--- xmake/thirdparty.lua
+++ xmake/thirdparty.lua
@@ -3,5 +3,4 @@ sdk_libs_dir = "$(projectdir)/SDKs/libs/"
 -- this must be on the top because other thirdparty libs may depend on it
 includes("thirdparty/mimalloc.lua")
 includes("thirdparty/platform_sdks.lua")
-includes("thirdparty/FiberTaskingLib.lua")
 includes("thirdparty/gamenetworkingsockets.lua")
