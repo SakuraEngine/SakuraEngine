64a13006188a500b0c1aba80a728972b8034ec85
zhu-rong <211103376@qq.com>
2024-04-07 18:41:01 +0800

    update v8 package

--- xrepo/packages/v/v8/xmake.lua
+++ xrepo/packages/v/v8/xmake.lua
@@ -1,13 +1,31 @@
 package("v8")
     set_homepage("https://chromium.googlesource.com/v8/v8.git")
     set_description("V8 JavaScript Engine")
-    set_urls("https://github.com/SakuraEngine/v8-compile/release/douwnload/v8_$(version)/",
+    set_urls("https://github.com/SakuraEngine/v8-compile/releases/download/v8_$(version)/",
     {
         version = function (version) 
             return version:gsub("(%S*)-skr", "%1")
         end
     })
-    add_versions("12.4-skr", "a28d420d2755bef7c0b049e6f233a22facc9f674141df631ce2ca21adbb7b6f5")
+    add_versions("12.4-skr", "be9e9cbd9f6874e455a537bdefdcc24f895bc4d2b2646f5e25217436dcf6c081")
+
+    -- v8 sys env
+    if is_plat("linux", "bsd") then
+        add_syslinks("pthread", "dl")
+    elseif is_plat("windows") then
+        add_syslinks("user32", "winmm", "advapi32", "dbghelp", "shlwapi")
+    end
+
+    -- v8 env
+    add_links(
+        "v8.dll",
+        "v8_libbase.dll",
+        "v8_libplatform.dll",
+        "third_party_zlib.dll")
+
+    -- v8 def
+    add_defines("USING_V8_PLATFORM_SHARED", { public = true })
+    add_defines("USING_V8_SHARED", { public = true })
 
     on_download(function (package, opt) 
         import("net.http")
@@ -19,7 +37,7 @@ package("v8")
         local arch = package:arch()
 
         -- combine url
-        local url = format("%s/v8-%s-%s.tgz", raw_url, plat, arch)
+        local url = format("%sv8-%s-%s.tgz", raw_url, plat, arch)
 
         -- get download config
         local sourcedir = opt.sourcedir
@@ -60,10 +78,19 @@ package("v8")
     on_install("windows|x64", function (package) 
         os.mkdir(package:installdir())
         os.cp("include", package:installdir())
-        os.cp("lib", package:installdir())
-        os.cp("bin", package:installdir())
+        os.cp("lib/*.lib", package:installdir("lib"))
+        os.cp("bin/*.dll", package:installdir("bin"))
+        os.cp("bin/*.pdb", package:installdir("bin"))
     end)
 
     on_test(function (package) 
-        assert(package:has_cxxfuncs("v8::V8::InitializePlatform(0)", {configs = {languages = "c++17"}, includes = "v8.h"}))
+        assert(package:has_cxxfuncs("v8::V8::InitializePlatform(v8::platform::NewDefaultPlatform().get())", {
+            configs = {
+                languages = "c++17"
+            }, 
+            includes = { 
+                "v8.h", 
+                "libplatform/libplatform.h" 
+            }
+        }))
     end)
