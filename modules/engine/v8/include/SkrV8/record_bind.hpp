9555903777e4512a1200f2513ee85a23d9a1da3c
zhu-rong <211103376@qq.com>
2024-04-15 16:29:34 +0800

    basic bind

---
+++ modules/engine/v8/include/SkrV8/record_bind.hpp
@@ -0,0 +1,36 @@
+#pragma once
+#include "SkrContainers/vector.hpp"
+#include "SkrRT/rttr/guid.hpp"
+
+namespace skr::v8_bind
+{
+struct ClassBindInfo;
+struct MethodBindInfo;
+struct FieldInfo;
+struct StaticMethodInfo;
+struct StaticFieldInfo;
+
+struct ClassBindInfo {
+    // class basic
+    GUID type_id;
+    GUID super_id;
+
+    // method & fields
+    Vector<MethodBindInfo> methods;
+    Vector<FieldInfo>      fields;
+
+    // static fields & static methods
+    Vector<StaticMethodInfo> static_methods;
+    Vector<StaticFieldInfo>  static_fields;
+};
+
+template <typename T>
+struct Class_ {
+    template <typename Ctor>
+    void ctor();
+
+private:
+    ClassBindInfo _bind_info;
+};
+
+} // namespace skr::v8_bind
