765ed09a513de60909589cc98848685ca8cc86ba
zhu-rong <211103376@qq.com>
2024-07-09 11:36:48 +0800

    reflect function

--- modules/core/base/include/SkrBase/meta.h
+++ modules/core/base/include/SkrBase/meta.h
@@ -8,12 +8,14 @@
     #define sreflect_interface(...) struct [[clang::annotate("__reflect__")]] sattr(__VA_ARGS__)
     #define sreflect_enum(...) enum [[clang::annotate("__reflect__")]] sattr(__VA_ARGS__)
     #define sreflect_enum_class(...) enum class [[clang::annotate("__reflect__")]] sattr(__VA_ARGS__)
+    #define sreflect_function(...) [[clang::annotate("__reflect__")]] sattr(__VA_ARGS__)
 #else
     #define sattr(...)
     #define sreflect_struct(...) struct
     #define sreflect_interface(...) struct
     #define sreflect_enum(...) enum
     #define sreflect_enum_class(...) enum class
+    #define sreflect_function(...)
 #endif
 
 // meta tools
