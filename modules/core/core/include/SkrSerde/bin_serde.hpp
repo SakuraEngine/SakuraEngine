d88fddaa51b37bfebf3609e375f58eb2596b534b
zhu-rong <211103376@qq.com>
2024-07-12 19:04:30 +0800

    update serde impl

--- modules/core/core/include/SkrSerde/bin_serde.hpp
+++ modules/core/core/include/SkrSerde/bin_serde.hpp
@@ -2,8 +2,10 @@
 #include "SkrBase/misc/traits.hpp"
 #include "SkrBase/template/concepts.hpp"
 #include "SkrCore/log.h"
+#include "SkrBase/types.h"
 
 // writer & reader
+// TODO. 搬到 archive 里面去
 // TODO. use proxy instead
 struct SBinaryWriter {
     using WriteFunc     = bool(void* user_data, const void* data, size_t size);
@@ -78,17 +80,30 @@ namespace skr
 template <typename T>
 struct BinSerde;
 
-// helper
-template <typename T>
-bool bin_read(SBinaryReader* r, T&& v) { return BinSerde<T>::read(r, v); }
-template <typename T>
-bool bin_write(SBinaryWriter* w, const T& v) { return BinSerde<T>::write(w, v); }
-
 // concept
 template <typename T>
 concept HasBinRead = requires(SBinaryReader* r, T& t) { BinSerde<T>::read(r, t); };
 template <typename T>
 concept HasBinWrite = requires(SBinaryWriter* w, const T& t) { BinSerde<T>::write(w, t); };
+
+// helper
+template <HasBinRead T>
+inline bool bin_read(SBinaryReader* r, T&& v) { return BinSerde<T>::read(r, v); }
+template <HasBinWrite T>
+inline bool bin_write(SBinaryWriter* w, const T& v) { return BinSerde<T>::write(w, v); }
+
+// POD writer
+template <typename T>
+struct BinSerdePOD {
+    inline static bool read(SBinaryReader* r, T& v)
+    {
+        return r->read(&v, sizeof(v));
+    }
+    inline static bool write(SBinaryWriter* w, const T& v)
+    {
+        return w->write(&v, sizeof(v));
+    }
+};
 } // namespace skr
 
 // primitive types
@@ -97,29 +112,42 @@ concept HasBinWrite = requires(SBinaryWriter* w, const T& t) { BinSerde<T>::writ
 //  float/double
 namespace skr
 {
-#define BIN_SERDE_PRIMITIVE(__TYPE)                           \
-    template <>                                               \
-    struct SKR_STATIC_API BinSerde<__TYPE> {                  \
-        static bool read(SBinaryReader* r, __TYPE& v);        \
-        static bool write(SBinaryWriter* w, const __TYPE& v); \
-    };
+template <>
+struct BinSerde<bool> : BinSerdePOD<bool> {
+};
 
-BIN_SERDE_PRIMITIVE(bool)
+template <>
+struct BinSerde<int8_t> : BinSerdePOD<int8_t> {
+};
+template <>
+struct BinSerde<int16_t> : BinSerdePOD<int16_t> {
+};
+template <>
+struct BinSerde<int32_t> : BinSerdePOD<int32_t> {
+};
+template <>
+struct BinSerde<int64_t> : BinSerdePOD<int64_t> {
+};
 
-BIN_SERDE_PRIMITIVE(int8_t)
-BIN_SERDE_PRIMITIVE(int16_t)
-BIN_SERDE_PRIMITIVE(int32_t)
-BIN_SERDE_PRIMITIVE(int64_t)
+template <>
+struct BinSerde<uint8_t> : BinSerdePOD<uint8_t> {
+};
+template <>
+struct BinSerde<uint16_t> : BinSerdePOD<uint16_t> {
+};
+template <>
+struct BinSerde<uint32_t> : BinSerdePOD<uint32_t> {
+};
+template <>
+struct BinSerde<uint64_t> : BinSerdePOD<uint64_t> {
+};
 
-BIN_SERDE_PRIMITIVE(uint8_t)
-BIN_SERDE_PRIMITIVE(uint16_t)
-BIN_SERDE_PRIMITIVE(uint32_t)
-BIN_SERDE_PRIMITIVE(uint64_t)
-
-BIN_SERDE_PRIMITIVE(float)
-BIN_SERDE_PRIMITIVE(double)
-
-#undef BIN_SERDE_PRIMITIVE
+template <>
+struct BinSerde<float> : BinSerdePOD<float> {
+};
+template <>
+struct BinSerde<double> : BinSerdePOD<double> {
+};
 } // namespace skr
 
 // enum & array
@@ -171,8 +199,172 @@ struct BinSerde<T[N]> {
 
 // skr types, 主要指 skr base 中的类型，遵循模块依赖的规则
 //  float2/float3/float4
-//  float4x4
-//  rotator
-//  quaternion
-//  guid
-//  md5
+//  float4x4/rotator/quaternion
+//  guid/md5
+namespace skr
+{
+template <>
+struct BinSerde<skr_float2_t> : BinSerdePOD<skr_float2_t> {
+};
+template <>
+struct BinSerde<skr_float3_t> : BinSerdePOD<skr_float3_t> {
+};
+template <>
+struct BinSerde<skr_float4_t> : BinSerdePOD<skr_float4_t> {
+};
+
+template <>
+struct BinSerde<skr_float4x4_t> : BinSerdePOD<skr_float4x4_t> {
+};
+template <>
+struct BinSerde<skr_rotator_t> : BinSerdePOD<skr_rotator_t> {
+};
+template <>
+struct BinSerde<skr_quaternion_t> : BinSerdePOD<skr_quaternion_t> {
+};
+
+template <>
+struct BinSerde<skr_guid_t> : BinSerdePOD<skr_guid_t> {
+};
+template <>
+struct BinSerde<skr_md5_t> : BinSerdePOD<skr_md5_t> {
+};
+} // namespace skr
+
+// 解环
+//  string/vector
+#include "SkrContainers/string.hpp"
+#include "SkrContainers/vector.hpp"
+namespace skr
+{
+template <>
+struct BinSerde<String> {
+    inline static bool read(SBinaryReader* r, String& v)
+    {
+        uint64_t size;
+        if (!BinSerde<uint64_t>::read(r, size))
+        {
+            SKR_LOG_FATAL(u8"failed to read string buffer size!");
+            return false;
+        }
+        skr::InlineVector<char8_t, 64> temp;
+        temp.resize_default(size);
+        if (!r->read((void*)temp.data(), temp.size()))
+        {
+            SKR_LOG_FATAL(u8"failed to read string buffer size!");
+            return false;
+        }
+        v = skr::String(skr::StringView((const char8_t*)temp.data(), (int32_t)temp.size()));
+        return true;
+    }
+    inline static bool write(SBinaryWriter* w, const String& v)
+    {
+        if (!BinSerde<uint64_t>::write(w, v.size()))
+            return false;
+        return w->write(v.u8_str(), v.size());
+    }
+};
+template <typename V>
+struct BinSerde<Vector<V>> {
+    inline static bool read(SBinaryReader* r, Vector<V>& v)
+    {
+
+        // read size
+        uint64_t size;
+        if (!BinSerde<uint64_t>::read(r, size))
+            return false;
+
+        // read elements
+        Vector<V> temp;
+        temp.reserve(size);
+        for (uint32_t i = 0; i < size; ++i)
+        {
+            V value;
+            if (!BinSerde<V>::read(r, value))
+                return false;
+            temp.add(std::move(value));
+        }
+
+        // result
+        v = std::move(temp);
+        return true;
+    }
+    inline static bool write(SBinaryWriter* w, const Vector<V>& v)
+    {
+        // write size
+        if (!BinSerde<uint64_t>::write(w, v.size()))
+            return false;
+
+        // write elements
+        for (const auto& value : v)
+        {
+            if (!BinSerde<V>::write(w, value))
+                return false;
+        }
+        return true;
+    }
+};
+} // namespace skr
+
+// vector writer
+namespace skr
+{
+struct VectorBinWriter {
+    Vector<uint8_t>* buffer;
+
+    bool write(const void* data, size_t size)
+    {
+        buffer->append((uint8_t*)data, size);
+        return true;
+    }
+};
+
+struct VectorBinWriterBitpacked {
+    Vector<uint8_t>* buffer;
+    uint8_t          bitOffset = 0;
+    bool             write(const void* data, size_t size)
+    {
+        return write_bits(data, size * 8);
+    }
+    bool write_bits(const void* data, size_t bitSize)
+    {
+        uint8_t* dataPtr = (uint8_t*)data;
+        if (bitOffset == 0)
+        {
+            buffer->append(dataPtr, (bitSize + 7) / 8);
+            bitOffset = bitSize % 8;
+            if (bitOffset != 0)
+                buffer->last() &= (1 << bitOffset) - 1;
+        }
+        else
+        {
+            buffer->last() |= dataPtr[0] << bitOffset;
+            int i = 1;
+            while (bitSize > 8)
+            {
+                buffer->add((dataPtr[i - 1] >> (8 - bitOffset)) | (dataPtr[i] << bitOffset));
+                ++i;
+                bitSize -= 8;
+            }
+            if (bitSize > 0)
+            {
+                auto newBitOffset = bitOffset + bitSize;
+                if (newBitOffset == 8)
+                {
+                    bitOffset = 0;
+                    return true;
+                }
+                else if (newBitOffset > 8)
+                {
+                    buffer->add(dataPtr[i - 1] >> (8 - bitOffset));
+                    newBitOffset = newBitOffset - 8;
+                }
+                buffer->last() &= (1 << newBitOffset) - 1;
+                SKR_ASSERT(newBitOffset <= UINT8_MAX);
+                bitOffset = (uint8_t)newBitOffset;
+            }
+        }
+        return true;
+    }
+};
+} // namespace skr
---
+++ modules/core/core/include/SkrSerde/json_serde.hpp
@@ -0,0 +1,205 @@
+#pragma once
+#include "SkrArchive/json/writer.h"
+#include "SkrArchive/json/reader.h"
+#include "SkrBase/template/concepts.hpp"
+
+// traits
+namespace skr
+{
+template <typename T>
+struct JsonSerde;
+
+// concept
+template <typename T>
+concept HasJsonRead = requires(skr::archive::JsonReader* r, T& t) { JsonSerde<T>::read(r, t); };
+template <typename T>
+concept HasJsonWrite = requires(skr::archive::JsonWriter* w, const T& t) { JsonSerde<T>::write(w, t); };
+
+// helper
+template <HasJsonRead T>
+inline bool json_read(skr::archive::JsonReader* r, T& v) { return JsonSerde<T>::read(r, v); }
+template <HasJsonWrite T>
+inline bool json_write(skr::archive::JsonWriter* w, const T& v) { return JsonSerde<T>::write(w, v); }
+} // namespace skr
+
+// primitive types
+//  bool
+//  int/uint 8/16/32/64
+//  float/double
+namespace skr
+{
+template <>
+struct JsonSerde<bool> {
+    inline static bool read(skr::archive::JsonReader* r, bool& v)
+    {
+        return r->Bool(v).has_value();
+    }
+    inline static bool write(skr::archive::JsonWriter* w, const bool& v)
+    {
+        return w->Bool(v).has_value();
+    }
+};
+
+template <>
+struct JsonSerde<int8_t> {
+    inline static bool read(skr::archive::JsonReader* r, int8_t& v)
+    {
+        int32_t _v;
+        if (r->Int32(_v).has_value())
+        {
+            v = static_cast<int8_t>(_v);
+            return true;
+        }
+        return false;
+    }
+    inline static bool write(skr::archive::JsonWriter* w, const int8_t& v)
+    {
+        return w->Int32(v).has_value();
+    }
+};
+template <>
+struct JsonSerde<int16_t> {
+    inline static bool read(skr::archive::JsonReader* r, int16_t& v)
+    {
+        int32_t _v;
+        if (r->Int32(_v).has_value())
+        {
+            v = static_cast<int16_t>(_v);
+            return true;
+        }
+        return false;
+    }
+    inline static bool write(skr::archive::JsonWriter* w, const int16_t& v)
+    {
+        return w->Int32(v).has_value();
+    }
+};
+template <>
+struct JsonSerde<int32_t> {
+    inline static bool read(skr::archive::JsonReader* r, int32_t& v)
+    {
+        return r->Int32(v).has_value();
+    }
+    inline static bool write(skr::archive::JsonWriter* w, const int32_t& v)
+    {
+        return w->Int32(v).has_value();
+    }
+};
+template <>
+struct JsonSerde<int64_t> {
+    inline static bool read(skr::archive::JsonReader* r, int64_t& v)
+    {
+        return r->Int64(v).has_value();
+    }
+    inline static bool write(skr::archive::JsonWriter* w, const int64_t& v)
+    {
+        return w->Int64(v).has_value();
+    }
+};
+
+template <>
+struct JsonSerde<uint8_t> {
+    inline static bool read(skr::archive::JsonReader* r, uint8_t& v)
+    {
+        uint32_t _v;
+        if (r->UInt32(_v).has_value())
+        {
+            v = static_cast<uint8_t>(_v);
+            return true;
+        }
+        return false;
+    }
+    inline static bool write(skr::archive::JsonWriter* w, const uint8_t& v)
+    {
+        return w->UInt32(v).has_value();
+    }
+};
+template <>
+struct JsonSerde<uint16_t> {
+    inline static bool read(skr::archive::JsonReader* r, uint16_t& v)
+    {
+        uint32_t _v;
+        if (r->UInt32(_v).has_value())
+        {
+            v = static_cast<uint16_t>(_v);
+            return true;
+        }
+        return false;
+    }
+    inline static bool write(skr::archive::JsonWriter* w, const uint16_t& v)
+    {
+        return w->UInt32(v).has_value();
+    }
+};
+template <>
+struct JsonSerde<uint32_t> {
+    inline static bool read(skr::archive::JsonReader* r, uint32_t& v)
+    {
+        return r->UInt32(v).has_value();
+    }
+    inline static bool write(skr::archive::JsonWriter* w, const uint32_t& v)
+    {
+        return w->UInt32(v).has_value();
+    }
+};
+template <>
+struct JsonSerde<uint64_t> {
+    inline static bool read(skr::archive::JsonReader* r, uint64_t& v)
+    {
+        return r->UInt64(v).has_value();
+    }
+    inline static bool write(skr::archive::JsonWriter* w, const uint64_t& v)
+    {
+        return w->UInt64(v).has_value();
+    }
+};
+
+template <>
+struct JsonSerde<float> {
+    inline static bool read(skr::archive::JsonReader* r, float& v)
+    {
+        return r->Float(v).has_value();
+    }
+    inline static bool write(skr::archive::JsonWriter* w, const float& v)
+    {
+        return w->Float(v).has_value();
+    }
+};
+template <>
+struct JsonSerde<double> {
+    inline static bool read(skr::archive::JsonReader* r, double& v)
+    {
+        return r->Double(v).has_value();
+    }
+    inline static bool write(skr::archive::JsonWriter* w, const double& v)
+    {
+        return w->Double(v).has_value();
+    }
+};
+} // namespace skr
+
+// enum & array
+namespace skr
+{
+template <concepts::Enum T>
+struct JsonSerde<T> {
+};
+
+template <typename T, size_t N>
+struct JsonSerde<T[N]> {
+};
+} // namespace skr
+
+// skr types, 主要指 skr base 中的类型，遵循模块依赖的规则
+//  float2/float3/float4
+//  float4x4/rotator/quaternion
+//  guid/md5
+namespace skr
+{
+}
+
+// 解环
+//  string/vector
+namespace skr
+{
+}
