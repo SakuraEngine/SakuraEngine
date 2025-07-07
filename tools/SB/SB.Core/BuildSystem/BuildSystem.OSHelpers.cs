618a05c30016e8b50af4d04624545a906342e803
SaeruHikari <3045349520@qq.com>
2025-07-07 12:35:44 +0800

    try fix

--- tools/SB/SB.Core/BuildSystem/BuildSystem.OSHelpers.cs
+++ tools/SB/SB.Core/BuildSystem/BuildSystem.OSHelpers.cs
@@ -97,11 +97,9 @@ namespace SB
                             return -1;
                         }
                     }
-                    else
-                    {
-                        P.WaitForExit();
-                    }
-
+                    // 在 .NET 中，当使用 BeginOutputReadLine() 和 BeginErrorReadLine() 时，输出是异步读取的。进程可能已经退出了，但异步读取线程可能还在处理缓冲区中的数据。
+                    // 需要在 WaitForExit(timeout) 返回 true 后，再调用无参数的 WaitForExit() 来确保所有异步读取操作完成：
+                    P.WaitForExit();
                     Output = localOutput;
                     Error = localError;
                     return P.ExitCode;
